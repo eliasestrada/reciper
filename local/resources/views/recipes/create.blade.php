598d3a368695155f440ee378dad21b974f53ef1f
Serhii Cho <1990serzhil@gmail.com>
2018-02-23 15:18:09 +0200

    fixed css

--- local/resources/views/recipes/create.blade.php
+++ local/resources/views/recipes/create.blade.php
@@ -1,20 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>Добавление рецепта</h1>
-        <div class="form">
-            {!! Form::open(['action' => 'RecipesController@store', 'method' => 'post']) !!}
-                <p>
-                    {{ Form::label('title', 'Название') }}</p>
-                    {{ Form::text('title', '', ['placeholder' => 'Название']) }}
-                </p>
-                <p>
-                    {{ Form::label('intro', 'Краткое описание') }}</p>
-                    {{ Form::textarea('intro', '', ['placeholder' => 'Краткое описание']) }}
-                </p>
-                {{ Form::submit('Сохранить', ['class' => 'button']) }}</p>
-            {!! Form::close() !!}
-        </div>
+    <h1>Добавление рецепта</h1>
+    <div class="form">
+        {!! Form::open(['action' => 'RecipesController@store', 'method' => 'post']) !!}
+            <p>
+                {{ Form::label('title', 'Название') }}</p>
+                {{ Form::text('title', '', ['placeholder' => 'Название']) }}
+            </p>
+            <p>
+                {{ Form::label('intro', 'Краткое описание') }}</p>
+                {{ Form::textarea('intro', '', ['placeholder' => 'Краткое описание']) }}
+            </p>
+            {{ Form::submit('Сохранить', ['class' => 'button']) }}</p>
+        {!! Form::close() !!}
     </div>
 @endsection
