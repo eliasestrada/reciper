b060bdbd3d449ed665e53822dd30234a93b52684
Serhii Cho <1990serzhil@gmail.com>
2018-02-27 14:50:14 +0200

    ..

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 
 <div class="wrapper">
-
     {!! Form::open(['action' => 'RecipesController@store', 'method' => 'post', 'class' => 'form', 'enctype' => 'multipart/form-data']) !!}
 
         <div class="recipe-buttons">
@@ -12,52 +11,95 @@
 
         <h2>Добавление рецепта</h2>
 
-        <div class="form-group">
-            {{ Form::label('название', 'Название') }}
-            {{ Form::text('название', '', ['placeholder' => 'Название']) }}
-        </div>
-
-        <div class="form-group">
-            {{ Form::label('описание', 'Описание') }}
-            {{ Form::textarea('описание', '', ['placeholder' => 'Описание']) }}
-        </div>
-
-        <div class="form-group">
-            {{ Form::label('ингридиенты', 'Ингридиенты') }}
-            {{ Form::textarea('ингридиенты', '', ['placeholder' => 'Ингридиенты']) }}
-        </div>
-
-        <div class="form-group">
-            {{ Form::label('совет', 'Совет') }}
-            {{ Form::textarea('совет', '', ['placeholder' => 'Совет']) }}
-        </div>
-
-        <div class="form-group">
-            {{ Form::label('приготовление', 'Приготовление') }}
-            {{ Form::textarea('приготовление', '', ['placeholder' => 'Приготовление']) }}
+        <button class="accordion" type="button">Название</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('название', 'Название') }}
+                {{ Form::text('название', '', ['placeholder' => 'Название рецепта']) }}
+            </div>
         </div>
         
-        <div class="form-group">
-            {{ Form::label('категория', 'Категория') }}
+        <button class="accordion" type="button">Описание</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('описание', 'Описание') }}
+                {{ Form::textarea('описание', '', ['placeholder' => 'Краткое описание рецепта']) }}
+            </div>
+        </div>
+        
+        <button class="accordion" type="button">Ингридиенты</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('ингридиенты', 'Ингридиенты') }}
+                {{ Form::textarea('ингридиенты', '', ['placeholder' => 'Все ингридиенты рецепта. После каждого ингридиента нажимайте кнопку Ввод (Enter) чтобы разделить их на строки.']) }}
+            </div>
         </div>
 
-        <div class="form-group">
-            <select name="категория">
-                @foreach ($categories as $category)
-                    <option selected value="{{ $category->category }}">{{ $category->category }}</option>
-                @endforeach
-            </select>
+        <button class="accordion" type="button">Совет</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('совет', 'Совет') }}
+                {{ Form::textarea('совет', '', ['placeholder' => 'Это поле не обязательно к заполнению, если у вас есть просьба или совет который может помочь в приготовлении блюда пишите его сюда.']) }}
+            </div>
         </div>
 
-        <div class="form-group">
-            {{ Form::label('время', 'Время приготовления в минутах') }}
-            {{ Form::number('время', '0') }}
+        <button class="accordion" type="button">Приготовление</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('приготовление', 'Приготовление') }}
+                {{ Form::textarea('приготовление', '', ['placeholder' => 'Опишите процесс приготовления по пунктам используя Ввод (Enter) для отделения пунктов друг от друга.']) }}
+            </div>
         </div>
 
-        <div class="form-group">
-            {{ Form::file('изображение') }}
+        <button class="accordion" type="button">Категория</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('категория', 'Категория') }}
+                <select name="категория">
+                    @foreach ($categories as $category)
+                        <option selected value="{{ $category->category }}">{{ $category->category }}</option>
+                    @endforeach
+                </select>
+            </div>
         </div>
+
+        <button class="accordion" type="button">Время приготовления</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('время', 'Время приготовления в минутах') }}
+                {{ Form::number('время', '0') }}
+            </div>
+        </div>
+
+        <button class="accordion" type="button">Изображение</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('изображение', 'Изображение не должно быть высокого разрешения.') }}
+                {{ Form::file('изображение', ['class' => "upload-image-form"]) }}
+                {{ Form::hidden('_method', 'PUT') }}
+            </div>
+        </div>
+
     {!! Form::close() !!}
 </div>
+
+<script>
+// Dropdowns
+var acc = document.getElementsByClassName("accordion")
+var i
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function(){
+        this.classList.toggle("active")
+        var panel = this.nextElementSibling
+
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px"
+        } 
+    })
+}
+</script>
 
 @endsection
--- resources/views/recipes/edit.blade.php
+++ resources/views/recipes/edit.blade.php
@@ -24,46 +24,6 @@
         <h2>Добавление рецепта</h2>
 
         <div class="form-group">
-            {{ Form::label('название', 'Название') }}
-            {{ Form::text('название', $recipe->title, ['placeholder' => 'Название рецепта']) }}
-        </div>
-
-        <div class="form-group">
-            {{ Form::label('описание', 'Описание') }}
-            {{ Form::textarea('описание', $recipe->intro, ['placeholder' => 'Краткое описание рецепта']) }}
-        </div>
-
-        <div class="form-group">
-            {{ Form::label('ингридиенты', 'Ингридиенты') }}
-            {{ Form::textarea('ингридиенты', $recipe->ingredients, ['placeholder' => 'Все ингридиенты рецепта. После каждого ингридиента нажимайте кнопку Ввод (Enter) чтобы разделить их на строки.']) }}
-        </div>
-
-        <div class="form-group">
-            {{ Form::label('совет', 'Совет') }}
-            {{ Form::textarea('совет', $recipe->advice, ['placeholder' => 'Это поле не обязательно к заполнению, если у вас есть просьба или совет который может помочь в приготовлении блюда пишите его сюда.']) }}
-        </div>
-
-        <div class="form-group">
-            {{ Form::label('приготовление', 'Приготовление') }}
-            {{ Form::textarea('приготовление', $recipe->text, ['placeholder' => 'Опишите процесс приготовления по пунктам используя Ввод (Enter) для отделения пунктов друг от друга.']) }}
-        </div>
-
-        <div class="form-group">
-            {{ Form::label('категория', 'Категория') }}
-            <select name="категория">
-                @foreach ($categories as $category)
-                    <option selected value="{{ $category->category }}">{{ $category->category }}</option>
-                @endforeach
-                <option selected value="{{ $recipe->category }}">{{ $recipe->category }}</option>
-            </select>
-        </div>
-
-        <div class="form-group">
-            {{ Form::label('время', 'Время приготовления в минутах') }}
-            {{ Form::number('время', $recipe->time) }}
-        </div>
-
-        <div class="form-group">
             <section class="recipes">
                 <div>
                     <img src="{{ asset('storage/images/'.$recipe->image) }}" alt="{{$recipe->title}}" title="{{$recipe->title}}">
@@ -71,11 +31,104 @@
             </section>
         </div>
 
-        <div class="form-group">
-            {{ Form::file('изображение', ['class' => "upload-image-form"]) }}
-            {{ Form::hidden('_method', 'PUT') }}
+        <button class="accordion" type="button">Название</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('название', 'Название') }}
+                {{ Form::text('название', $recipe->title, ['placeholder' => 'Название рецепта']) }}
+            </div>
         </div>
+
+        <button class="accordion" type="button">Название</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('описание', 'Описание') }}
+                {{ Form::textarea('описание', $recipe->intro, ['placeholder' => 'Краткое описание рецепта']) }}
+            </div>
+        </div>
+
+        <button class="accordion" type="button">Описание</button>
+        <div class="panel">
+            <div class="form-group">
+                    {{ Form::label('описание', 'Описание') }}
+                    {{ Form::textarea('описание', $recipe->intro, ['placeholder' => 'Краткое описание рецепта']) }}
+            </div>
+        </div>
+
+        <button class="accordion" type="button">Ингридиенты</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('ингридиенты', 'Ингридиенты') }}
+                {{ Form::textarea('ингридиенты', $recipe->ingredients, ['placeholder' => 'Все ингридиенты рецепта. После каждого ингридиента нажимайте кнопку Ввод (Enter) чтобы разделить их на строки.']) }}
+            </div>
+        </div>
+
+        <button class="accordion" type="button">Совет</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('совет', 'Совет') }}
+                {{ Form::textarea('совет', $recipe->advice, ['placeholder' => 'Это поле не обязательно к заполнению, если у вас есть просьба или совет который может помочь в приготовлении блюда пишите его сюда.']) }}
+            </div>
+        </div>
+
+        <button class="accordion" type="button">Приготовление</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('приготовление', 'Приготовление') }}
+                {{ Form::textarea('приготовление', $recipe->text, ['placeholder' => 'Опишите процесс приготовления по пунктам используя Ввод (Enter) для отделения пунктов друг от друга.']) }}
+            </div>
+        </div>
+
+        <button class="accordion" type="button">Категория</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('категория', 'Категория') }}
+                <select name="категория">
+                    @foreach ($categories as $category)
+                        <option selected value="{{ $category->category }}">{{ $category->category }}</option>
+                    @endforeach
+                    <option selected value="{{ $recipe->category }}">{{ $recipe->category }}</option>
+                </select>
+            </div>
+        </div>
+
+        <button class="accordion" type="button">Время приготовления</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('время', 'Время приготовления в минутах') }}
+                {{ Form::number('время', $recipe->time) }}
+            </div>
+        </div>
+
+        <button class="accordion" type="button">Изображение</button>
+        <div class="panel">
+            <div class="form-group">
+                {{ Form::label('изображение', 'Изображение не должно быть высокого разрешения.') }}
+                {{ Form::file('изображение', ['class' => "upload-image-form"]) }}
+                {{ Form::hidden('_method', 'PUT') }}
+            </div>
+        </div>
+
     {!! Form::close() !!}
 </div>
+
+<script>
+// Dropdowns
+var acc = document.getElementsByClassName("accordion")
+var i
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function(){
+        this.classList.toggle("active")
+        var panel = this.nextElementSibling
+
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px"
+        } 
+    })
+}
+</script>
 
 @endsection
