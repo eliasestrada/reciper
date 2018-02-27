fae74c3c4c40a17074cb12a50b2423f4dce863cb
Serhii Cho <1990serzhil@gmail.com>
2018-02-27 13:53:25 +0200

    css files

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,8 +5,8 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <!-- Styles -->
-<link rel="stylesheet" href="{{ asset('css/mobile.css') }}">
-<link rel="stylesheet" href="{{ asset('css/desktop.css') }}" media="screen and (min-width: 640px)">
+<link rel="stylesheet" href="{{ asset('css/mobile.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/desktop.min.css') }}" media="screen and (min-width: 640px)">
 
 <!-- Links -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
