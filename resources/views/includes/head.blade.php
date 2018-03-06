c8cde5213512b60401f043b9fb2e3f7e9e81e3b2
Serhii Cho <1990serzhil@gmail.com>
2018-03-06 10:29:01 +0200

    cleaning

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,8 +5,9 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <!-- Styles -->
-<link rel="stylesheet" href="{{ asset('css/mobile.min.css') }}">
-<link rel="stylesheet" href="{{ asset('css/desktop.min.css') }}" media="screen and (min-width: 640px)">
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('css/bootstrap-grid.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/animations.css') }}">
 
 <!-- Links -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
