2efc9587c437c4de563305fb22e5af768e3a39b3
Serhii Cho <1990serzhil@gmail.com>
2018-03-06 10:44:08 +0200

    footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,17 +1,34 @@
 <footer>
-    <ul>
-        <li><h3>Навигация</h3></li>
-        <li><a href="/">Главная</a></li>
-        <li><a href="/recipes">Рецепты</a></li>
-        {{--  TODO: <li><a href="/contact">Связь с нами</a></li>  --}}
-    </ul>
+	<div class="col-3">
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae accusantium corporis enim? Harum, repudiandae aut?</p>
+	</div>
+	
+	<div class="col-1">
+		<ul class="unstyled-list">
+			<li><strong>Навигация</strong></li>
+			<li><a href="/">Главная</a></li>
+			<li><a href="/recipes">Рецепты</a></li>
+			<li><a href="">О нас</a></li>
+		</ul>
+	</div>
 
-    {{--  TODO: <ul class="footer-col" id="column-b">
-        <li><h3>Условия и приватность</h3></li>
-        <li><a href="#">О нас</a></li>
-    </ul>  --}}
+	<div class="col-1">
+		<ul class="unstyled-list">
+			<li><strong>Популярное</strong></li>
+			<li><a href="">Что-то</a></li>
+			<li><a href="">Парапане</a></li>
+			<li><a href="">Лорамас</a></li>
+			<li><a href="">Ронепро</a></li>
+		</ul>
+	</div>
 
-    <div class="copyright">
-        <li>Copyright &copy; {{ date("Y") }}</li>
-    </div>
+	<div class="col-1">
+		<ul class="unstyled-list">
+			<li><strong>Популярное</strong></li>
+			<li><a href="">Чкарамак</a></li>
+			<li><a href="">Простор</a></li>
+			<li><a href="">Купалав</a></li>
+			<li><a href="">Зопарлав</a></li>
+		</ul>
+	</div>
 </footer>
