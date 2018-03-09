45ad38b1fc85c964c1ca22c606be50796dace7a6
Serhii Cho <1990serzhil@gmail.com>
2018-03-09 14:47:03 +0200

    sxgeo done

--- resources/views/statistic.blade.php
+++ resources/views/statistic.blade.php
@@ -13,12 +13,9 @@
 			@foreach ($visitors as $visitor)
 				<?php $geodata = $sxgeo->getCityFull($visitor->ip);?>
 				<div class="notification">
-					<h4 class="notification-title">{{ $geodata['country']['name_ru'] }}</h4>
-					<h4 class="notification-title">{{ $geodata['region']['name_ru'] }}</h4>
-					<h4 class="notification-title">{{ $geodata['city']['name_ru'] }}</h4>
-					<p class="notification-message">{{ $visitor->clicks }}</p>
-					<p class="notification-message">{{ $visitor->ip }}</p>
-					<p class="notification-message">{{ facebookTimeAgo($visitor->created_at) }}</p>
+					<h4 class="notification-title">{{ $geodata['country']['name_ru'] }} / {{ $geodata['city']['name_ru'] }}</h4>
+					<p class="notification-message">Клики: {{ $visitor->clicks }}</p>
+					<p class="notification-message">{{ facebookTimeAgo($visitor->updated_at) }}</p>
 				</div>
 			@endforeach
 		</div>
