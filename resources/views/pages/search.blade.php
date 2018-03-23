aa6d3062a94be75958284ab48ac05d5ab57bca4f
Serhii Cho <1990serzhil@gmail.com>
2018-03-23 13:18:54 +0200

    copied from server

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -15,33 +15,35 @@
 			<input type="submit" value="Поиск" style="display: none;">
 		</div>
 	</form>
-	
 
 	{{--  Results  --}}
 	@if($recipes)
-		<section class="recipes">
-			@foreach($recipes as $recipe)
-				
-					<div>
-						<a href="/recipes/{{ $recipe->id }}">
-							<img src="{{ asset('storage/images/' . $recipe->image) }}" alt="{{ $recipe->title }}" title="{{ $recipe->title }}">
-						</a>
-						<div class="recipes-content">
-							<h3>{{ $recipe->title }}</h3>
-							<p>{{ $recipe->intro }}</p>
-							<a href="/search?for={{ $recipe->category }}" title="{{ $recipe->category }}"><span class="category">{{ $recipe->category }}</span></a>
+		<section class="container recipes">
+			<div class="row">
+
+				@foreach($recipes as $recipe)
+					<div class="recipe-container col-xs-12 col-sm-6 col-md-4 col-lg-3">
+						<div class="recipe">
+							<a href="/recipes/{{ $recipe->id }}">
+								<!-- Image -->
+								<img  src="{{ asset('storage/images/'.$recipe->image) }}" alt="{{ $recipe->title }}" title="{{ $recipe->title }}">
+							</a>
+							<div class="recipes-content">
+								<!-- Title -->
+								<h3>{{$recipe->title}}</h3>
+							</div>
 						</div>
 					</div>
-			@endforeach
-			{{ $recipes->links() }}
+				@endforeach
+				{{ $recipes->links() }}
+
+			</div>
 		</section>
 	@endif
 
 	<div class="content">
 		<h4 class="content center">{{ $message }}</h4>
 	</div>
-
-
 </div>
 
 @endsection
