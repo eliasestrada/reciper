ed02ff3917bd3e6e3f13c42a22ddecd73a61f847
Serhii Cho <1990serzhil@gmail.com>
2018-04-01 18:38:18 +0300

    profile-menu-line again

--- resources/views/includes/profile-menu-line.blade.php
+++ resources/views/includes/profile-menu-line.blade.php
@@ -1,29 +1,35 @@
 <nav class="admin-sidebar">
 	<ul>
 		<li>
-			<a>
-				<i class="fa fa-list fa-2x"></i>
-				<span class="nav-text">
-					Forms
-				</span>
+			<a href="#">
+				<i style="background: url({{ asset('/css/icons/user.png') }});" class="icon-profile-menu-line"></i>
+				<span class="nav-text">Профиль</span>
 			</a>
 		</li>
 		<li>
 			<a href="#">
-				<i class="fa fa-info fa-2x"></i>
-				<span class="nav-text">
-					Documentation
-				</span>
+				<i style="background: url({{ asset('/css/icons/docs.png') }})" class="icon-profile-menu-line"></i>
+				<span class="nav-text">Мои рецепты</span>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<i style="background: url({{ asset('/css/icons/users.png') }});" class="icon-profile-menu-line"></i>
+				<span class="nav-text">Пользователи</span>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<i style="background: url({{ asset('/css/icons/gear.png') }});" class="icon-profile-menu-line"></i>
+				<span class="nav-text">Настройки</span>
 			</a>
 		</li>
 	</ul>
 	<ul class="logout">
 		<li>
 			<a href="#">
-					<i class="fa fa-power-off fa-2x"></i>
-				<span class="nav-text">
-					Logout
-				</span>
+				<i style="background: url({{ asset('/css/icons/user.png') }});" class="icon-profile-menu-line"></i>
+				<span class="nav-text">Выйти</span>
 			</a>
 		</li>  
 	</ul>
