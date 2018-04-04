9e88d0a56c9d7723a68044559aaa7a6868b95e1e
Serhii Cho <1990serzhil@gmail.com>
2018-04-04 15:24:03 +0300

    ..

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,31 +4,30 @@
 
 @section('content')
 
-<div class="wrapper">
-    <h1 class="headline">Войти</h1>
-    <form method="POST" action="{{ route('login') }}" class="form">
-        @csrf
+<h2 class="headline">Войти</h2>
+<form method="POST" action="{{ route('login') }}" class="form">
+	@csrf
 
-        <div class="form-group">
-            {{ Form::label('email', 'Эл. адрес') }}
-            {{ Form::email('email', null, ['placeholder' => 'Эл. адрес']) }}
-        </div>
+	<div class="form-group">
+		{{ Form::label('email', 'Эл. адрес') }}
+		{{ Form::email('email', null, ['placeholder' => 'Эл. адрес']) }}
+	</div>
 
-        <div class="form-group">
-            {{ Form::label('password', 'Пароль') }}
-			{{ Form::password('password', ['placeholder' => 'Пароль']) }}
-		</div>
-		
-		<div class="form-group" style="display:flex;">
-			<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Запомнить меня
-        </div>
+	<div class="form-group">
+		{{ Form::label('password', 'Пароль') }}
+		{{ Form::password('password', ['placeholder' => 'Пароль']) }}
+	</div>
+	
+	<div class="form-group" style="display:flex;">
+		<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Запомнить меня
+	</div>
 
-        <div class="form-group">
-            {{ Form::submit('Войти') }}
-        </div>
+	<div class="form-group">
+		{{ Form::submit('Войти') }}
+	</div>
 
-        <a href="{{ route('password.request') }}">Забыли пароль?</a>
-    </form>
-</div>
+	<a href="{{ route('password.request') }}">Забыли пароль?</a>
+</form>
+<div class="spacer"></div>
 
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,35 +4,34 @@
 
 @section('content')
 
-<div class="wrapper">
-    <h1 class="headline">Регистрация</h1>
-    <form method="POST" action="{{ route('register') }}" class="form">
-        @csrf
+<h2 class="headline">Регистрация</h2>
+<form method="POST" action="{{ route('register') }}" class="form">
+	@csrf
 
-        <div class="form-group">
-            <label for="name">Это имя будет отображаться на всех ваших рецептах</label>
-            <input type="text" id="name" name="name" value="{{ old('name') }}" placeholder="Имя" required autofocus>
-        </div>
+	<div class="form-group">
+		<label for="name">Это имя будет отображаться на всех ваших рецептах</label>
+		<input type="text" id="name" name="name" value="{{ old('name') }}" placeholder="Имя" required autofocus>
+	</div>
 
-        <div class="form-group">
-            <label for="email">Эл. адрес</label>
-            <input type="email" id="email" name="email" value="{{ old('email') }}" placeholder="Эл. адрес" required>
-        </div>
+	<div class="form-group">
+		<label for="email">Эл. адрес</label>
+		<input type="email" id="email" name="email" value="{{ old('email') }}" placeholder="Эл. адрес" required>
+	</div>
 
-        <div class="form-group">
-            <label for="password">Пароль</label>
-            <input type="password" id="password" name="password" placeholder="Пароль" required>
-        </div>
+	<div class="form-group">
+		<label for="password">Пароль</label>
+		<input type="password" id="password" name="password" placeholder="Пароль" required>
+	</div>
 
-        <div class="form-group">
-            <label for="password_confirmation">Повторите пароль</label>
-            <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Повторите пароль" required>
-        </div>
+	<div class="form-group">
+		<label for="password_confirmation">Повторите пароль</label>
+		<input type="password" id="password_confirmation" name="password_confirmation" placeholder="Повторите пароль" required>
+	</div>
 
-        <div class="form-group">
-            <input type="submit" value="Регистрация">
-        </div>
-    </form>
-</div>
+	<div class="form-group">
+		<input type="submit" value="Регистрация">
+	</div>
+</form>
+<div class="spacer"></div>
 
 @endsection
