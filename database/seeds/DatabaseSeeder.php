f7b5023292d1603b38296135f3b91ded90381f9f
Serhii Cho <1990serzhil@gmail.com>
2018-04-07 17:35:20 +0300

    added Recipe seeder with 1 recipe

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(TitlesSeeder::class);
 		$this->call(CategoriesTableSeeder::class);
 		$this->call(UsersSeeder::class);
+		$this->call(RecipesSeeder::class);
     }
 }
