350293287f62f123c13cbc770b8262cbc523ab2f
Serhii Cho <1990serzhil@gmail.com>
2018-04-15 10:36:50 +0300

    done with scss

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,19 +4,19 @@
 
 @section('content')
 
-	<div class="profile-header">
-		<h1>{{ Auth::user()->name }}</h1>
-		<img src="{{ asset('storage/uploads/'.Auth::user()->image) }}" alt="{{ Auth::user()->name }}" />
-	</div>
+<div class="profile-header">
+	<h1>{{ Auth::user()->name }}</h1>
+	<img src="{{ asset('storage/uploads/'.Auth::user()->image) }}" alt="{{ Auth::user()->name }}" />
+</div>
 
-	{{--  Buttons  --}}
-	<div style="animation: appear .5s; padding-bottom: 4em;">
-		<a href="/notifications" title="Оповещения" class="button" {{ $notifications }}>Оповещения</a>
+{{--  Buttons  --}}
+<div style="animation: appear .5s; padding-bottom: 4em;">
+	<a href="/notifications" title="Оповещения" class="button" {{ $notifications }}>Оповещения</a>
+
+	@if (Auth::user()->admin === 1)
+		<a href="/checklist" title="Проверочная" class="button" {{ $allunapproved }}>Проверочная</a>
+		<a href="/feedback" title="Обратная связь" class="button" {{ $allfeedback }}>Обратная связь</a>
+	@endif
+</div>
 	
-		@if (Auth::user()->admin === 1)
-			<a href="/checklist" title="Проверочная" class="button" {{ $allunapproved }}>Проверочная</a>
-			<a href="/feedback" title="Обратная связь" class="button" {{ $allfeedback }}>Обратная связь</a>
-		@endif
-	</div>
-
 @endsection
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,7 +5,7 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <!-- Styles -->
-{!! styleTimestamp('/css/app.css') !!}
+{!! styleTimestamp('/css/app.min.css') !!}
 
 <!-- Links -->
 <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/x-icon">
