18743beeb268212f19ce73ba10bd4d40c4866d3d
Serhii Cho <1990serzhil@gmail.com>
2018-04-15 14:00:08 +0300

    nothing

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,8 +51,8 @@ class RegisterController extends Controller
             'name' => 'required|string|min:3|max:199',
             'email' => 'required|string|email|max:199|unique:users',
             'password' => 'required|string|min:6|confirmed',
-        ]);
-    }
+		]);
+	}
 
     // Create a new user instance after a valid registration.
     protected function create(array $data)
