e24451aec5dc494183f2d28eea310d876a8b5c99
Serhii Cho <1990serzhil@gmail.com>
2018-04-24 17:56:19 +0300

    lang forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,32 +1,33 @@
 @extends('layouts.app')
 
-@section('title', 'Войти')
+@section('title', trans('form.login'))
 
 @section('content')
 
-<h2 class="headline">Войти</h2>
+<h2 class="headline">@lang('form.login')</h2>
 <form method="POST" action="{{ route('login') }}" class="form">
+
 	@csrf
 
 	<div class="form-group simple-group">
-		{{ Form::label('email', 'Эл. адрес') }}
-		{{ Form::email('email', null, ['placeholder' => 'Эл. адрес']) }}
+		{{ Form::label('email', trans('form.email')) }}
+		{{ Form::email('email', null, ['placeholder' => trans('form.email')]) }}
 	</div>
 
 	<div class="form-group simple-group">
-		{{ Form::label('password', 'Пароль') }}
-		{{ Form::password('password', ['placeholder' => 'Пароль']) }}
+		{{ Form::label('password', trans('form.pwd')) }}
+		{{ Form::password('password', ['placeholder' => trans('form.pwd')]) }}
 	</div>
 	
 	<div class="form-group simple-group" style="display:flex;">
-		<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Запомнить меня
+		<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> @lang('form.remember_me')
 	</div>
 
 	<div class="form-group simple-group">
-		{{ Form::submit('Войти') }}
+		{{ Form::submit(trans('form.login')) }}
 	</div>
 
-	{{-- <a href="{{ route('password.request') }}">Забыли пароль?</a> --}}
+	{{-- <a href="{{ route('password.request') }}">@lang('form.forgot_pwd')</a> --}}
 </form>
 <div class="spacer"></div>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,35 +1,35 @@
 @extends('layouts.app')
 
-@section('title', 'Регистрация')
+@section('title', trans('form.register'))
 
 @section('content')
 
-<h2 class="headline">Регистрация</h2>
+<h2 class="headline">@lang('form.register')</h2>
 <form method="POST" action="{{ route('register') }}" class="form">
 	@csrf
 
 	<div class="form-group simple-group">
-		<label for="name">Это имя будет отображаться на всех ваших рецептах</label>
-		<input type="text" id="name" name="name" value="{{ old('name') }}" placeholder="Имя" required autofocus>
+		<label for="name">@lang('form.this_name_will_be_on_every_recipe')</label>
+		<input type="text" id="name" name="name" value="{{ old('name') }}" placeholder="@lang('form.name')" required autofocus>
 	</div>
 
 	<div class="form-group simple-group">
-		<label for="email">Эл. адрес</label>
-		<input type="email" id="email" name="email" value="{{ old('email') }}" placeholder="Эл. адрес" required>
+		<label for="email">@lang('form.email')</label>
+		<input type="email" id="email" name="email" value="{{ old('email') }}" placeholder="@lang('form.email')" required>
 	</div>
 
 	<div class="form-group simple-group">
-		<label for="password">Пароль</label>
-		<input type="password" id="password" name="password" placeholder="Пароль" required>
+		<label for="password">@lang('form.pwd')</label>
+		<input type="password" id="password" name="password" placeholder="@lang('form.pwd')" required>
 	</div>
 
 	<div class="form-group simple-group">
-		<label for="password_confirmation">Повторите пароль</label>
-		<input type="password" id="password_confirmation" name="password_confirmation" placeholder="Повторите пароль" required>
+		<label for="password_confirmation">@lang('form.pwd_confirm')</label>
+		<input type="password" id="password_confirmation" name="password_confirmation" placeholder="@lang('form.pwd_confirm')" required>
 	</div>
 
 	<div class="form-group simple-group">
-		<input type="submit" value="Регистрация">
+		<input type="submit" value="@lang('form.register')">
 	</div>
 </form>
 <div class="spacer"></div>
