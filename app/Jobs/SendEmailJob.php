ebd9044b8a85c1d81a19b07f4b5a688ffb84aeed
Serhii Cho <1990serzhil@gmail.com>
2018-05-18 20:12:10 +0300

    Added Job for sending email

---
+++ app/Jobs/SendEmailJob.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use App\Mail\UserRegisteredMail;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class SendEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle()
+    {
+        \Mail::to('test@gmail.com')->send(new UserRegisteredMail);
+    }
+}
