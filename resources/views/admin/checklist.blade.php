71e596dab745bc3fb8a0119e8664ba902f420454
Serhii Cho <1990serzhil@gmail.com>
2018-05-19 16:28:11 +0300

    Added component to check_list

--- resources/views/admin/checklist.blade.php
+++ resources/views/admin/checklist.blade.php
@@ -4,33 +4,15 @@
 
 @section('content')
 
-<h2 class="content center">@lang('admin.checklist')</h2>
-
-{{--  Unapproved recipes  --}}
 @admin
-	<div class="item-list unstyled-list">
-		<h4>
-			{{ $unapproved->count() > 0 ? trans('admin.unapproved') .' '. $unapproved->count() : '' }}
-		</h4>
-
-		@forelse ($unapproved as $unapprove)
-			<a href="/recipes/{{ $unapprove->id }}" title="{{ $unapprove->title }}">
-				<li>
-					<img src="{{ asset('storage/images/'.$unapprove->image) }}" alt="{{ $unapprove->title }}" />
-					<div class="item-content">
-						<h3 class="project-name">{{ $unapprove->title }}</h3>
-						<p class="project-title">
-							@lang('users.date') {{ facebookTimeAgo($unapprove->created_at) }}
-						</p>
-					</div>
-				</li>
-			</a>
-		@empty
-			<p class="content center">@lang('admin.no_unapproved')</p>
-		@endforelse
-
-		{{ $unapproved->links() }}
-	</div>
+	@component('components.list_of_recipes', ['recipes' => $unapproved])
+		@slot('title')
+			@lang('admin.checklist')
+		@endslot
+		@slot('no_recipes')
+			@lang('admin.no_unapproved')
+		@endslot
+	@endcomponent
 @endadmin
 
 @endsection
