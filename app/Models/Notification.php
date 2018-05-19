ca3164e4327eddf9ba2bc77a8bbb9f5e5945d234
Serhii Cho <1990serzhil@gmail.com>
2018-05-19 16:35:30 +0300

    Created translation for notification class

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -6,9 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-	protected $fillable = [
-		'user_id', 'title', 'message', 'for_admins'
-	];
+	protected $guarded = ['id'];
 
     public function user() {
 		return $this->belongsTo(User::class);
@@ -16,18 +14,17 @@ class Notification extends Model
 
 	public static function recipeHasBeenApproved($title, $user_id) {
 		self::create([
-			'user_id'    => $user_id,
-			'title'      => 'Рецепт опубликован',
-			'message'    => 'Рецепт под названием "' . $title . '" был опубликован.'
+			'user_id' => $user_id,
+			'title' => trans('notifications.recipe_published'),
+			'message' => trans('notifications.recipe_with_title_published', ['title' => $title])
 		]);
 	}
 
 	public static function recipeHasNotBeenCreated($title, $user_id) {
 		self::create([
-			'user_id'    => $user_id,
-			'title'      => 'Рецепт не опубликован',
-			'message'    => 'Рецепт под названием "' . $title . '" не был опубликован 
-							так как администрация венула его вам на переработку.'
+			'user_id' => $user_id,
+			'title' => trans('notifications.recipe_not_published'),
+			'message' => trans('notifications.recipe_with_title_not_published', ['title' => $title])
 		]);
 	}
 }
---
+++ resources/lang/ru/notifications.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+	'recipe_published' => 'Рецепт опубликован',
+	'recipe_with_title_published' => 'Рецепт под названием :title был опубликован.',
+	'recipe_not_published' => 'Рецепт не опубликован',
+	'recipe_with_title_not_published' => 'Рецепт под названием :title не был опубликован так как администрация венула его вам на переработку.',
+
+];
