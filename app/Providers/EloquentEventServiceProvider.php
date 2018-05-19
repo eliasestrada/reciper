148356e90891ba6e8b6ac0b6d11f14538bbbf289
Serhii Cho <1990serzhil@gmail.com>
2018-05-19 13:01:10 +0300

    Changed observer

--- app/Providers/EloquentEventServiceProvider.php
+++ app/Providers/EloquentEventServiceProvider.php
@@ -3,18 +3,12 @@
 namespace App\Providers;
 
 use App\Models\Recipe;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class EloquentEventServiceProvider extends ServiceProvider
 {
-    // Bootstrap services.
     public function boot()
     {
-		if (Schema::hasTable('recipes')) {
-			Recipe::observe(\App\Observers\RecipeObserver::class);
-		} else {
-			\Log::emergency(trans('logs.no_table', ['table' => 'recipes']));
-		}
+		Recipe::observe(\App\Observers\RecipeObserver::class);
     }
 }
