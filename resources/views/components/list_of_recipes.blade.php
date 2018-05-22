a80e98c3eee8d3e65ed15eba6269a00eafb1dd57
Serhii Cho <1990serzhil@gmail.com>
2018-05-22 14:30:36 +0300

    Fixed stuff

--- resources/views/components/list_of_recipes.blade.php
+++ resources/views/components/list_of_recipes.blade.php
@@ -1,36 +1,38 @@
 @author
-	<div class="item-list unstyled-list">
+	<div class="container">
 		@isset($title, $recipes)
 			<h2 class="content center">
 				{{ count($recipes) > 0 ? $title .' '. count($recipes) : '' }}
 			</h2>
 		@endisset
 
-		@isset($recipes)
-			@forelse ($recipes as $recipe)
-				<a href="/recipes/{{ $recipe->id }}" title="{{ $recipe->title }}">
-					<li style="border-left:solid 3px #{{ $recipe->approved() ? '65b56e' : 'ce7777' }};">
-						<img src="{{ asset('storage/images/'.$recipe->image) }}" alt="{{ $recipe->title }}" />
-						<div class="item-content">
-							<h3 class="project-name">{{ $recipe->title }}</h3>
-							<p class="project-title">
-								@lang('users.status'): {{ $recipe->approved() ? trans('users.checked') : trans('users.not_checked') }}
-							</p>
-							<p class="project-title">
-								@lang('users.date') {{ facebookTimeAgo($recipe->updated_at) }}
-							</p>
-						</div>
-					</li>
-				</a>
-			@empty
-				@isset($no_recipes)
-					<p class="content center">{{ $no_recipes }}</p>
-				@endisset
-			@endforelse
-		@endisset
-
-		@isset($recipes)
-			{{ optional($recipes)->links() }}
-		@endisset
+		<div class="item-list unstyled-list row">
+			@isset($recipes)
+				@forelse ($recipes as $recipe)
+					<a href="/recipes/{{ $recipe->id }}" title="{{ $recipe->title }}" class="col-lg-6">
+						<li style="border-left:solid 3px #{{ $recipe->approved() ? '65b56e' : 'ce7777' }};">
+							<img src="{{ asset('storage/images/'.$recipe->image) }}" alt="{{ $recipe->title }}" />
+							<div class="item-content">
+								<h3 class="project-name">{{ $recipe->title }}</h3>
+								<p class="project-title">
+									@lang('users.status'): {{ $recipe->approved() ? trans('users.checked') : trans('users.not_checked') }}
+								</p>
+								<p class="project-title">
+									@lang('users.date') {{ facebookTimeAgo($recipe->updated_at) }}
+								</p>
+							</div>
+						</li>
+					</a>
+				@empty
+					@isset($no_recipes)
+						<p class="content center">{{ $no_recipes }}</p>
+					@endisset
+				@endforelse
+			@endisset
+	
+			@isset($recipes)
+				{{ optional($recipes)->links() }}
+			@endisset
+		</div>
 	</div>
 @endauthor
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,38 +7,36 @@
 <h2 class="content center">@lang('users.all_authors')</h2>
 
 <div class="container">
-	<div class="row">
-		<ul class="item-list unstyled-list">
-			@foreach ($users as $user)
-				<a href="/users/{{ $user->id }}" title="{{ $user->name }}">
-					<li class="col-md-6 col-lg-4">
-						<img src="{{ asset('storage/uploads/'.$user->image) }}" alt="{{ $user->name }}" style="width:3.7em;" />
-						<div class="item-content">
-							<h3 class="project-name">{{ $user->name }}</h3>
-							<p class="project-title">
-								@lang('users.online'): {{ facebookTimeAgo( $user->updated_at ) }}
-							</p>
-							@admin
-								@if (!$user->isAuthor() )
-								<div class="block-message-content">
-									<p>@lang('users.new_user', ['date' => facebookTimeAgo( $user->created_at )])</p>
-									
-									<a href="/member/{{ $user->id }}/add" class="button-add-user" title="@lang('users.click_to_add')" onclick='return confirm("@lang('users.sure_to_add')")'>
-										@lang('users.add_to_team')
-									</a>
-									
-									<a href="/member/{{ $user->id }}/delete" class="button-add-user" title="@lang('users.click_to_delete')" onclick='return confirm("@lang('users.sure_to_delete')")'>
-										@lang('users.delete')
-									</a>
-								</div>
-								@endif
-							@endadmin
-						</div>
-					</li>
-				</a>
-			@endforeach
-		</ul>
-	</div>
+	<ul class="item-list unstyled-list row">
+		@foreach ($users as $user)
+			<a href="/users/{{ $user->id }}" title="{{ $user->name }}" class="col-md-6 col-xl-4">
+				<li>
+					<img src="{{ asset('storage/uploads/'.$user->image) }}" alt="{{ $user->name }}" style="width:3.7em;" />
+					<div class="item-content">
+						<h3 class="project-name">{{ $user->name }}</h3>
+						<p class="project-title">
+							@lang('users.online'): {{ facebookTimeAgo( $user->updated_at ) }}
+						</p>
+						@admin
+							@if (!$user->isAuthor() )
+							<div class="block-message-content">
+								<p>@lang('users.new_user', ['date' => facebookTimeAgo( $user->created_at )])</p>
+								
+								<a href="/member/{{ $user->id }}/add" class="button-add-user" title="@lang('users.click_to_add')" onclick='return confirm("@lang('users.sure_to_add')")'>
+									@lang('users.add_to_team')
+								</a>
+								
+								<a href="/member/{{ $user->id }}/delete" class="button-add-user" title="@lang('users.click_to_delete')" onclick='return confirm("@lang('users.sure_to_delete')")'>
+									@lang('users.delete')
+								</a>
+							</div>
+							@endif
+						@endadmin
+					</div>
+				</li>
+			</a>
+		@endforeach
+	</ul>
 </div>
 
 {{ $users->links() }}
