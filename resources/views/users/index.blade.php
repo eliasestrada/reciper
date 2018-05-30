7d8791f9a84289637a77d376ed4b0195459d7995
Serhii Cho <1990serzhil@gmail.com>
2018-05-30 18:14:43 +0300

    Fixed users index page

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,9 +7,9 @@
 <h2 class="content center">@lang('users.all_authors')</h2>
 
 <div class="container">
-	<ul class="item-list unstyled-list row">
+	<ul class="item-list unstyled-list">
 		@foreach ($users as $user)
-			<a href="/users/{{ $user->id }}" title="{{ $user->name }}" class="col-md-6 col-xl-4">
+			<a href="/users/{{ $user->id }}" title="{{ $user->name }}" >
 				<li>
 					<img src="{{ asset('storage/uploads/'.$user->image) }}" alt="{{ $user->name }}" style="width:3.7em;" />
 					<div class="item-content">
@@ -19,17 +19,17 @@
 						</p>
 						@admin
 							@if (!$user->isAuthor() )
-							<div class="block-message-content">
-								<p>@lang('users.new_user', ['date' => facebookTimeAgo( $user->created_at )])</p>
+								<p>
+									@lang('users.new_user', ['date' => facebookTimeAgo($user->created_at )])
+								</p>
 								
-								<a href="/member/{{ $user->id }}/add" class="button-add-user" title="@lang('users.click_to_add')" onclick='return confirm("@lang('users.sure_to_add')")'>
+								<a href="/member/{{ $user->id }}/add" class="btn" title="@lang('users.click_to_add')" onclick='return confirm("@lang('users.sure_to_add')")'>
 									@lang('users.add_to_team')
 								</a>
 								
-								<a href="/member/{{ $user->id }}/delete" class="button-add-user" title="@lang('users.click_to_delete')" onclick='return confirm("@lang('users.sure_to_delete')")'>
+								<a href="/member/{{ $user->id }}/delete" class="btn" title="@lang('users.click_to_delete')" onclick='return confirm("@lang('users.sure_to_delete')")'>
 									@lang('users.delete')
 								</a>
-							</div>
 							@endif
 						@endadmin
 					</div>
