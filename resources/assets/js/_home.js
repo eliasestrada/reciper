70e99701d84ca66c11c0f5a7c22944b0cd122096
Serhii Cho <1990serzhil@gmail.com>
2018-05-31 16:39:45 +0300

    Cleaned home.js

---
+++ resources/assets/js/_home.js
@@ -0,0 +1,17 @@
+
+// Run function
+if ($('btn-for-banner')) {
+	addClassToElement('banner-form', 'btn-for-banner', 'show-elem')
+}
+
+if ($('btn-for-intro')) {
+	addClassToElement('intro-form', 'btn-for-intro', 'show-elem')
+}
+
+if ($('btn-for-footer')) {
+	addClassToElement('footer-form', 'btn-for-footer', 'show-elem')
+}
+
+if ($('search-form')) {
+	addClassToElement('search-form', 'home-search-btn', 'show-search')
+}
