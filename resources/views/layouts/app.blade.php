a72c3971bac8e0fbd4db159a1242731f68f0df80
Serhii Cho <1990serzhil@gmail.com>
2018-06-02 14:43:54 +0300

    Fixed message animation and styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,11 +14,12 @@
 	@include('includes.user-sidebar')
 
 	<div class="wrapper pb-5" id="app">
+
+		@include('includes.messages')
+
 		<div class="container">
 			<div class="loading" id="loading"></div>
 			<h4 class="loading-title" id="loading-title">@lang('includes.loading') ...</h4>
-	
-			@include('includes.messages')
 	
 			@yield('content')
 		</div>
