f1d32cc70feb440deac14746ab19a732a785c7dd
Serhii Cho <1990serzhil@gmail.com>
2018-06-02 15:27:51 +0300

    Moved contact validation messages to contact lang file

--- resources/lang/ru/my_valid.php
+++ resources/lang/ru/my_valid.php
@@ -1,12 +1,6 @@
 <?php
 
 return [
-	// Contact form validation
-	'contact_name_required'  => 'Поле имя обязательно к заполнению',
-	'contact_email_required'  => 'Поле эл. адрес обязательно к заполнению',
-	'contact_message_required'  => 'Поле сообщение обязательно к заполнению',
-	'contact_email_email'  => 'Эл. адрес должен быть правильным адресом',
-	'contact_message_min'  => 'Сообщение слишком короткое. Должно быть не меньше :min символов',
-	'contact_message_max'  => 'Сообщение слишком длинное. Должно быть не более :max символов',
+	
 
 ];
