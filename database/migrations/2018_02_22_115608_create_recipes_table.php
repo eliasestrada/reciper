7eee4bf4f8dff96b4f27148e7a7927da52db5232
Serhii Cho <1990serzhil@gmail.com>
2018-06-02 17:26:47 +0300

    Added migration with meal table and added field meal_id to recipes table

--- database/migrations/2018_02_22_115608_create_recipes_table.php
+++ database/migrations/2018_02_22_115608_create_recipes_table.php
@@ -13,6 +13,7 @@ class CreateRecipesTable extends Migration
 			$table->increments('id');
 			$table->integer('user_id');
 			$table->integer('category_id');
+			$table->integer('meal_id');
             $table->string('title')->nullable();
             $table->text('intro')->nullable();
             $table->text('ingredients')->nullable();
@@ -24,7 +25,6 @@ class CreateRecipesTable extends Migration
             $table->bigInteger('ready')->default(0);
 			$table->boolean('approved')->default(0);
 			$table->string('image')->default('default.jpg');
-			$table->string('meal_time', 30)->nullable();
             $table->timestamps();
         });
     }
