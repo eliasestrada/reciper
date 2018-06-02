778c7d862e065380a7bd18f629e35e375397159b
Serhii Cho <1990serzhil@gmail.com>
2018-06-02 15:21:06 +0300

    Moved recipes validation messages to recipes file Fixed bag in message Cleaned Requests

--- app/Http/Requests/RecipePublichRequest.php
+++ app/Http/Requests/RecipePublichRequest.php
@@ -20,13 +20,13 @@ class RecipePublichRequest extends FormRequest
     {
         if ($this->ready == 1) {
 			return [
-				'title'        => 'min:5|max:199',
-				'intro'        => 'min:20|max:2000',
-				'ingredients'  => 'min:20|max:5000',
-				'advice'       => 'max:5000',
-				'text'         => 'min:80|max:10000',
-				'time'         => 'numeric|digits_between:0,1000',
-				'image'        => 'image|nullable|max:1999'
+				'title' => 'min:5|max:199',
+				'intro' => 'min:20|max:2000',
+				'ingredients' => 'min:20|max:5000',
+				'advice' => 'max:5000',
+				'text' => 'min:80|max:10000',
+				'time' => 'numeric|digits_between:0,1000',
+				'image' => 'image|nullable|max:1999'
 			];
 		}
 		return [];
@@ -37,19 +37,19 @@ class RecipePublichRequest extends FormRequest
     {
         if ($this->ready == 1) {
 			return [
-				'title.min'             => trans('my_valid.recipe_min'),
-				'title.max'             => trans('my_valid.recipe_title_max'),
-				'intro.min'             => trans('my_valid.recipe_intro_max'),
-				'intro.max'             => trans('my_valid.recipe_intro_max'),
-				'ingredients.min'       => trans('my_valid.recipe_ingredients_min'),
-				'ingredients.max'       => trans('my_valid.recipe_ingredients_max'),
-				'advice.max'            => trans('my_valid.recipe_advice_max'),
-				'text.min'              => trans('my_valid.recipe_text_min'),
-				'text.max'              => trans('my_valid.recipe_text_max'),
-				'time.numeric'          => trans('my_valid.recipe_time_numeric'),
-				'time.digits_between'   => trans('my_valid.recipe_digits_between'),
-				'image.image'           => trans('my_valid.recipe_image_image'),
-				'image.max'             => trans('my_valid.recipe_image_max')
+				'title.min' => trans('recipes.recipe_min'),
+				'title.max' => trans('recipes.recipe_title_max'),
+				'intro.min' => trans('recipes.recipe_intro_min'),
+				'intro.max' => trans('recipes.recipe_intro_max'),
+				'ingredients.min' => trans('recipes.recipe_ingredients_min'),
+				'ingredients.max' => trans('recipes.recipe_ingredients_max'),
+				'advice.max' => trans('recipes.recipe_advice_max'),
+				'text.min' => trans('recipes.recipe_text_min'),
+				'text.max' => trans('recipes.recipe_text_max'),
+				'time.numeric' => trans('recipes.recipe_time_numeric'),
+				'time.digits_between' => trans('recipes.recipe_digits_between'),
+				'image.image' => trans('recipes.recipe_image_image'),
+				'image.max' => trans('recipes.recipe_image_max')
 			];
 		}
 		return [];
--- app/Http/Requests/RecipeSaveRequest.php
+++ app/Http/Requests/RecipeSaveRequest.php
@@ -16,32 +16,32 @@ class RecipeSaveRequest extends FormRequest
     public function rules()
     {
         return [
-            'title'        => 'max:199',
-            'intro'        => 'max:2000',
+            'title' => 'max:199',
+            'intro' => 'max:2000',
             'ingredients'  => 'max:5000',
-            'advice'       => 'max:5000',
-            'text'         => 'max:10000',
-            'time'         => 'numeric|digits_between:0,1999',
-            'image'        => 'image|nullable|max:1999'
+            'advice' => 'max:5000',
+            'text' => 'max:10000',
+            'time' => 'numeric|digits_between:0,1999',
+            'image' => 'image|nullable|max:1999'
         ];
 	}
 
     public function messages()
     {
         return [
-            'title.min'             => trans('my_valid.recipe_min'),
-			'title.max'             => trans('my_valid.recipe_title_max'),
-			'intro.min'             => trans('my_valid.recipe_intro_max'),
-			'intro.max'             => trans('my_valid.recipe_intro_max'),
-			'ingredients.min'       => trans('my_valid.recipe_ingredients_min'),
-			'ingredients.max'       => trans('my_valid.recipe_ingredients_max'),
-			'advice.max'            => trans('my_valid.recipe_advice_max'),
-			'text.min'              => trans('my_valid.recipe_text_min'),
-			'text.max'              => trans('my_valid.recipe_text_max'),
-			'time.numeric'          => trans('my_valid.recipe_time_numeric'),
-			'time.digits_between'   => trans('my_valid.recipe_digits_between'),
-			'image.image'           => trans('my_valid.recipe_image_image'),
-			'image.max'             => trans('my_valid.recipe_image_max')
+            'title.min' => trans('recipes.recipe_min'),
+			'title.max' => trans('recipes.recipe_title_max'),
+			'intro.min' => trans('recipes.recipe_intro_max'),
+			'intro.max' => trans('recipes.recipe_intro_max'),
+			'ingredients.min' => trans('recipes.recipe_ingredients_min'),
+			'ingredients.max' => trans('recipes.recipe_ingredients_max'),
+			'advice.max' => trans('recipes.recipe_advice_max'),
+			'text.min' => trans('recipes.recipe_text_min'),
+			'text.max' => trans('recipes.recipe_text_max'),
+			'time.numeric' => trans('recipes.recipe_time_numeric'),
+			'time.digits_between'   => trans('recipes.recipe_digits_between'),
+			'image.image' => trans('recipes.recipe_image_image'),
+			'image.max' => trans('recipes.recipe_image_max')
         ];
     }
 }
