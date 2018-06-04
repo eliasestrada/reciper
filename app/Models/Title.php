a1bdde36e6b013e4c9c2579c006dc2150679437c
Serhii Cho <1990serzhil@gmail.com>
2018-06-04 18:32:33 +0300

    Figured how set table name in model dipending on... current locale state (language)

--- app/Models/Title.php
+++ app/Models/Title.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Schema;
 use Illuminate\Database\Eloquent\Model;
 
 class Title extends Model
@@ -9,4 +10,14 @@ class Title extends Model
 	protected $table = 'titles_ru';
 	protected $fillable = [ 'name', 'title', 'text' ];
 	public $timestamps  = false;
+
+	/**
+	 * Name of the table depends on locale state
+	 */
+	public function __construct()
+	{
+		if (Schema::hasTable('title_' . language())) {
+			$this->table = 'titles_' . language();
+		}
+	}
 }
