75a52f05dd9736c12852f0a96e9cbf50837b16d4
Serhii Cho <1990serzhil@gmail.com>
2018-06-05 18:32:36 +0300

    Changed Recipes resources to proper way

--- app/Http/Resources/RecipesRandomResource.php
+++ app/Http/Resources/RecipesRandomResource.php
@@ -9,9 +9,15 @@ class RecipesRandomResource extends JsonResource
     // Transform the resource into an array.
     public function toArray($request)
     {
+		if (locale() === 'ru') {
+			$title = $this->title_ru;
+		} elseif (locale() === 'en') {
+			$title = $this->title_en;
+		}
+
         return [
-			'id'    => $this->id,
-			'title' => $this->title,
+			'id' => $this->id,
+			'title' => $title,
 			'image' => $this->image
 		];
     }
