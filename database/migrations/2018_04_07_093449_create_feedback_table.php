23506bc04c8f544ee0915a79e37f874a82ad58a8
Serhii Cho <1990serzhil@gmail.com>
2018-06-05 11:09:54 +0300

    Deleted name field from contact migration

--- database/migrations/2018_04_07_093449_create_feedback_table.php
+++ database/migrations/2018_04_07_093449_create_feedback_table.php
@@ -11,7 +11,6 @@ class CreateFeedbackTable extends Migration
     {
         Schema::create('feedback', function (Blueprint $table) {
 			$table->increments('id');
-			$table->string('name');
 			$table->string('email');
 			$table->text('message');
             $table->timestamps();
