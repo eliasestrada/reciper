1a37c3a23a35465fcb617338beb0b0148f74e9a6
Serhii Cho <1990serzhil@gmail.com>
2018-06-05 16:15:38 +0300

    Changed recipes migration and factory

--- database/factories/RecipeFactory.php
+++ database/factories/RecipeFactory.php
@@ -9,13 +9,23 @@ $factory->define(Recipe::class, function (Faker $faker) {
 		'user_id' => rand(1, 2),
 		'category_id' => rand(1, Category::count()),
 		'meal_id' => rand(1, 3),
-        'title' => $faker->catchPhrase,
-		'intro' => $faker->text(200),
-		'ingredients' => $faker->text(40),
-		'text' => $faker->text(1000),
 		'time' => rand(10, 160),
-		'ready' => 1,
-		'approved' => 1,
-		'image' => 'default.jpg'
+		'image' => 'default.jpg',
+
+		// Russian language
+        'title_ru' => $faker->catchPhrase,
+		'intro_ru' => $faker->text(200),
+		'ingredients_ru' => $faker->text(40),
+		'text_ru' => $faker->text(1000),
+		'ready_ru' => 1,
+		'approved_ru' => 1,
+
+		// Russian language
+        'title_en' => $faker->catchPhrase,
+		'intro_en' => $faker->text(200),
+		'ingredients_en' => $faker->text(40),
+		'text_en' => $faker->text(1000),
+		'ready_en' => 1,
+		'approved_en' => 1
     ];
 });
--- database/migrations/2018_02_22_115608_create_recipes_table.php
+++ database/migrations/2018_02_22_115608_create_recipes_table.php
@@ -14,17 +14,27 @@ class CreateRecipesTable extends Migration
 			$table->integer('user_id')->unsigned()->default(1);
 			$table->integer('category_id')->unsigned()->default(1);
 			$table->integer('meal_id')->unsigned()->default(1);
-            $table->string('title')->nullable();
-            $table->text('intro')->nullable();
-            $table->text('ingredients')->nullable();
-            $table->text('text')->nullable();
             $table->integer('time')->default(0);
             $table->bigInteger('views')->default(0);
             $table->bigInteger('likes')->default(0);
-            $table->bigInteger('ready')->default(0);
-			$table->boolean('approved')->default(0);
 			$table->string('image')->default('default.jpg');
 			$table->timestamps();
+			
+			// Russian language
+            $table->string('title_ru')->nullable();
+            $table->text('intro_ru')->nullable();
+            $table->text('ingredients_ru')->nullable();
+			$table->text('text_ru')->nullable();
+            $table->bigInteger('ready_ru')->default(0);
+			$table->boolean('approved_ru')->default(0);
+			
+			// English language
+            $table->string('title_en')->nullable();
+            $table->text('intro_en')->nullable();
+            $table->text('ingredients_en')->nullable();
+            $table->text('text_en')->nullable();
+            $table->bigInteger('ready_en')->default(0);
+			$table->boolean('approved_en')->default(0);
         });
     }
 
