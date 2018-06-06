864cee177c415589f86818bfb8177979f6c1686e
Serhii Cho <1990serzhil@gmail.com>
2018-06-06 10:52:48 +0300

    Fixed resources, now its easiert to add new language

--- app/Http/Resources/RecipesRandomResource.php
+++ app/Http/Resources/RecipesRandomResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Recipe;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class RecipesRandomResource extends JsonResource
@@ -9,15 +10,9 @@ class RecipesRandomResource extends JsonResource
     // Transform the resource into an array.
     public function toArray($request)
     {
-		if (locale() === 'ru') {
-			$title = $this->title_ru;
-		} elseif (locale() === 'en') {
-			$title = $this->title_en;
-		}
-
         return [
 			'id' => $this->id,
-			'title' => $title,
+			'title' => $this->resource->toArray()['title_'.locale()],
 			'image' => $this->image
 		];
     }
