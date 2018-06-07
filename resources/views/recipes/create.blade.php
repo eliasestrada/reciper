bd202ea5442a04edb025c6dff1c0b10f76be1184
Serhii Cho <1990serzhil@gmail.com>
2018-06-07 14:46:46 +0300

    In create recipe blade added field for choosing category

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -17,41 +17,30 @@
 			</div>
 		</div>
 	
-		<div class="col-12 col-sm-6 col-md-3">
+		<div class="col-12 col-sm-6 col-md-4">
 			{{-- Title --}}
 			<div class="form-group">
 				<label for="title">@lang('recipes.title')</label>
 				<input type="text" name="title" placeholder="@lang('recipes.title')" id="title">
 			</div>
 		</div>
-		<div class="col-12 col-sm-6 col-md-3">
-			{{-- Category --}}
-			<div class="form-group simple-group">
-				<label for="category_id">@lang('recipes.category')</label>
-				<select name="category_id" id="category_id">
-					@foreach ($categories as $category)
-						<option value="{{ $category->id }}">
-							{{ $category->toArray()['name_'.locale()] }}
-						</option>
-					@endforeach
-				</select>
-			</div>
-		</div>
-		<div class="col-12 col-sm-6 col-md-3">
+		<div class="col-12 col-sm-6 col-md-4">
 			{{-- Time --}}
 			<div class="form-group simple-group">
 				<label for="time">@lang('recipes.time_description')</label>
 				<input name="time" type="number" value="0" id="time">
 			</div>
 		</div>
-		<div class="col-12 col-sm-6 col-md-3">
+		<div class="col-12 col-sm-6 col-md-4">
 			{{-- Meal time --}}
 			<div class="form-group simple-group">
 				<label for="meal">@lang('recipes.meal_description')</label>
 				<select name="meal" id="meal">
-					<option value="1">{{ title_case(trans('header.breakfast')) }}</option>
-					<option value="2">{{ title_case(trans('header.lunch')) }}</option>
-					<option value="3">{{ title_case(trans('header.dinner')) }}</option>
+					@foreach ($meal as $m)
+						<option value="{{ $m['id'] }}">
+							{{ title_case($m['name_'.locale()]) }}
+						</option>
+					@endforeach
 				</select>
 			</div>
 		</div>
@@ -77,6 +66,16 @@
 				<label for="text">@lang('recipes.text_of_recipe')</label>
 				<textarea name="text" placeholder="@lang('recipes.text_description')" id="text"></textarea>
 			</div>
+		</div>
+
+		<div class="form-group col-12">
+			<categories-field
+				locale="{{ locale() }}"
+				label="@lang('recipes.category')"
+				select="@lang('form.select')"
+				deleting="@lang('form.deleting')"
+				add="@lang('form.add')">
+			</categories-field>
 		</div>
 
 		{{-- Image --}}
