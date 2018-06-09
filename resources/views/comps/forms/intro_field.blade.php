6fe3503fa39c988d64bfb61a7e80d07363095f25
Serhii Cho <1990serzhil@gmail.com>
2018-06-09 16:43:23 +0300

    Added intro_field component

---
+++ resources/views/comps/forms/intro_field.blade.php
@@ -0,0 +1,4 @@
+<div class="form-group">
+	<label for="intro">@lang('recipes.intro')</label>
+	<textarea name="intro" id="intro" placeholder="@lang('recipes.short_intro')">{{ ($intro ?? '') }}</textarea>
+</div>
