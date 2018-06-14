83bd9efe5fdd626b15a79c553b20aef0e282d23f
Serhii Cho <1990serzhil@gmail.com>
2018-06-14 16:35:45 +0300

    Added and changed blade icons

--- resources/views/icons/heart.blade.php
+++ resources/views/icons/heart.blade.php
@@ -1,3 +1,3 @@
-<svg style="{{ ($style ?? '') }}" class="{{ ($class ?? '') }}">
+<svg style="{{ ($style ?? '') }}" class="{{ ($class ?? '') }}" viewBox="0 0 {{ ($scale ?? '75') }} {{ ($scale ?? '75') }}">
 	<path d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z"></path>
 </svg>
