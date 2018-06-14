9bc5f01738acbd932adc796f177f96a547f9f687
Serhii Cho <1990serzhil@gmail.com>
2018-06-14 16:37:33 +0300

    Changed blade icon in recipes show page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,12 +5,42 @@
 @section('content')
 
 <div class="profile-header">
-	<h1>{{ $user->name }}</h1>
-	<img src="{{ asset('storage/uploads/'.$user->image) }}" alt="{{ $user->name }}" />
-	<div class="content text-center mt-3">
-		<p>@include('icons.heart') {{ $likes }}</p>
+	<div class="content text-center">
+		<h1>{{ $user->name }}</h1>
 		<p>@lang('users.joined'): {{ facebookTimeAgo($user->created_at) }}</p>
 		<p>@lang('users.online'): {{ facebookTimeAgo($user->updated_at) }}</p>
+	</div>
+
+	<img src="{{ asset('storage/uploads/'.$user->image) }}" alt="{{ $user->name }}" />
+
+	<div class="bubbles">
+		<div class="mb-4 bubbles-block">
+			<div class="bubble">
+				<span class="number">
+					{!! readableNumber($likes) !!}
+				</span>
+				@include('icons.heart', ['scale' => '50'])
+			</div>
+			<span>@lang('users.likes')</span>
+		</div>
+		<div class="mb-4 bubbles-block">
+			<div class="bubble">
+				<span class="number">
+					{!! readableNumber(getRatingNumber($recipes, $likes)) !!}
+				</span>
+				@include('icons.trophy', ['scale' => '50'])
+			</div>
+			<span>@lang('users.rating')</span>
+		</div>
+		<div class="bubbles-block">
+			<div class="bubble">
+				<span class="number">
+					{!! readableNumber(99900) !!}
+				</span>
+				@include('icons.trophy', ['scale' => '50'])
+			</div>
+			<span>some text</span>
+		</div>
 	</div>
 </div>
 
