42adad5e3d5d28880299dd7db6b75b02f0c19b7a
Serhii Cho <1990serzhil@gmail.com>
2018-06-14 10:48:24 +0300

    Added method shortcut to Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,4 +12,9 @@ class Category extends Model
 	public function recipes() {
 		return $this->belongsToMany(Recipe::class);
 	}
+
+	public function getName()
+	{
+		return $this->toArray()['name_' . locale()];
+	}
 }
