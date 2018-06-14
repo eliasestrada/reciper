6883d3cdf919972da4b099e265a1f477c86226a7
Serhii Cho <1990serzhil@gmail.com>
2018-06-14 18:50:41 +0300

    Done with single user page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,7 +8,11 @@
 	<div class="content text-center">
 		<h1>{{ $user->name }}</h1>
 		<p>@lang('users.joined'): {{ facebookTimeAgo($user->created_at) }}</p>
-		<p>@lang('users.online'): {{ facebookTimeAgo($user->updated_at) }}</p>
+		<p>
+			{!! getOnlineIcon(facebookTimeAgo($user->updated_at)) !!}
+			@lang('date.online') 
+			{{ facebookTimeAgo($user->updated_at, 'online') }}
+		</p>
 	</div>
 
 	<img src="{{ asset('storage/uploads/'.$user->image) }}" alt="{{ $user->name }}" />
@@ -35,20 +39,17 @@
 		<div class="bubbles-block">
 			<div class="bubble">
 				<span class="number">
-					{!! readableNumber(99900) !!}
+					{!! readableNumber($recipes->count()) !!}
 				</span>
-				@include('icons.trophy', ['scale' => '50'])
+				@include('icons.book', ['scale' => '1430'])
 			</div>
-			<span>some text</span>
+			<span>@lang('includes.recipes')</span>
 		</div>
 	</div>
 </div>
 
 {{--  All my recipes  --}}
 @component('comps.list_of_recipes', ['recipes' => $recipes])
-	@slot('title')
-		@lang('users.all_recipes')
-	@endslot
 	@slot('no_recipes')
 		@lang('users.this_user_does_not_have_recipes')
 	@endslot
