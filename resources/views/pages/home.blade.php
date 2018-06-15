403c7c15351cc4f86e440813a827181474330c94
Serhii Cho <1990serzhil@gmail.com>
2018-06-15 18:55:01 +0300

    Deleted useless

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -90,12 +90,3 @@
 </section>
 
 @endsection
-
-@section('script')
-	<script>
-		document.addEventListener('DOMContentLoaded', function() {
-			var elems = document.querySelectorAll('.parallax');
-			var instances = M.Parallax.init(elems);
-		});
-	</script>
-@endsection
