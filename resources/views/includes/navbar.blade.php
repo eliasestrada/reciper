61dd7c4a5b12382961afabe14826445dc0ebe8c2
Serhii Cho <1990serzhil@gmail.com>
2018-06-15 14:28:45 +0300

    Added search to navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -11,13 +11,13 @@
 </ul>
 <nav>
 	<div class="nav-wrapper main">
-		<div class="px-3">
+		<div class="px-3 wrapper">
 			<a href="/" title="@lang('includes.home')" class="brand-logo">
 				{{ config('app.name') }}
 			</a>
 			<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+
 			<ul class="right hide-on-med-and-down">
-				
 				<li class="{{ activeIfRouteIs('/') }}">
 					<a href="/" title="На главную">@lang('includes.home')</a>
 				</li>
@@ -27,13 +27,22 @@
 				<li class="{{ activeIfRouteIs('search') }}">
 					<a href="/search" title="Поиск">@lang('includes.search')</a>
 				</li>
-	
+
 				<!-- Dropdown Trigger -->
 				<li>
 					<a class="dropdown-trigger" href="#!" data-target="dropdown1">
 						@lang('includes.categories')
 						<i class="material-icons right">arrow_drop_down</i>
 					</a>
+				</li>
+				<li>
+					<form action="{{ action('PagesController@search') }}" method="get">
+						<div class="input-field">
+							<input id="search" type="search" name="for" placeholder="@lang('pages.search_details')" class="pr-5 fix-input" required>
+							<label class="label-icon" for="search"><i class="material-icons">search</i></label>
+							<i class="material-icons">close</i>
+						</div>
+					</form>
 				</li>
 			</ul>
 		</div>
