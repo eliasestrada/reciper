49d3d4cbb8c4f5325802e0aa29ac178f7158da80
Serhii Cho <1990serzhil@gmail.com>
2018-06-15 18:55:42 +0300

    Chaged hardcoded javascript to blade directives

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,17 +21,10 @@
 	<!-- Javascript -->
 	{!! scriptTimestamp('/js/vue.js') !!}
 	{!! scriptTimestamp('/js/vanilla.js') !!}
+
 	@yield('script')
-	<script>
-		document.addEventListener('DOMContentLoaded', function() {
-			// Dropdown
-			var elems = document.querySelectorAll('.dropdown-trigger');
-			var instances = M.Dropdown.init(elems);
-			// Navigation
-			var elems = document.querySelectorAll('.sidenav');
-			var instances = M.Sidenav.init(elems);
-		});
-	</script>
+	@include('includes.js.dropdown')
+	@include('includes.js.sidenav')
 	@include('includes.messages')
 </body>
 </html>
