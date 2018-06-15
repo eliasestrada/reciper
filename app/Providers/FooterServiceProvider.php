1421d4e79542ac30f2740cab9cfe5e3b33731c69
Serhii Cho <1990serzhil@gmail.com>
2018-06-15 18:20:11 +0300

    Fixed footer provider

--- app/Providers/FooterServiceProvider.php
+++ app/Providers/FooterServiceProvider.php
@@ -41,6 +41,8 @@ class FooterServiceProvider extends ServiceProvider
 					Recipe::select('id', 'title_' . locale())
 						->withCount('likes')
 						->orderBy('likes_count', 'desc')
+						->where('ready_' . locale(), 1)
+						->where('approved_' . locale(), 1)
 						->limit(10)
 						->get()
 				);
