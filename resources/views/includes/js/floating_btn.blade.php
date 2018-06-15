51d4fa21310ce03f214b9bcbbc4a12013ec202d1
Serhii Cho <1990serzhil@gmail.com>
2018-06-15 19:13:32 +0300

    Fixed floationg button blade

--- resources/views/includes/js/floating_btn.blade.php
+++ resources/views/includes/js/floating_btn.blade.php
@@ -2,7 +2,8 @@
 	document.addEventListener('DOMContentLoaded', function() {
 		var elems = document.querySelectorAll('.fixed-action-btn');
 		var instances = M.FloatingActionButton.init(elems, {
-		direction: 'top'
+			direction: 'top',
+			hoverEnabled: false
 		});
 	});
 </script>
