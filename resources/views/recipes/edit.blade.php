868bd74b37a639e9cfc74a57733ab68757944c3d
Serhii Cho <1990serzhil@gmail.com>
2018-06-15 19:28:18 +0300

    Added some bootstrap and fisish btns on edit recipe page

--- resources/views/recipes/edit.blade.php
+++ resources/views/recipes/edit.blade.php
@@ -12,24 +12,35 @@
 	<div class="row">
 		<div class="col s12">
 			<h1 class="headline">@lang('recipes.add_recipe')</h1>
-			<div class="recipe-buttons">
-				{{--  Save button  --}}
-				<input type="submit" id="submit-save-recipe" class="edit-recipe-icon icon-save" title="@lang('recipes.save_recipe')" value="">
-	
-				{{--  View button  --}}
-				<a href="/recipes/{{ $recipe->id }}" class="edit-recipe-icon icon-eye" title="@lang('recipes.view_recipe')"></a>
 
-				{{-- Publish button --}}
-				<a href="#" class="edit-recipe-icon icon-publish" title="@lang('recipes.view_recipe')" id="publish-btn"></a>
-				<input type="checkbox" name="ready" value="1" class="d-none" id="ready-checkbox">
-
-				{{--  Delete button  --}}
-				<delete-recipe-btn
-					recipe-id="{{ $recipe->id }}"
-					deleted-fail="{{ trans('recipes.deleted_fail') }}"
-					deleting="{{ trans('recipes.deleting') }}"
-					confirm="{{ trans('recipes.are_you_sure_to_delete') }}">
-				</delete-recipe-btn>
+			<div class="fixed-action-btn">
+				<a href="#" class="btn-floating main btn-large pulse z-depth-3"><i class="large material-icons">more_vert</i></a>
+				<ul>
+					<li> {{--  Delete button  --}}
+						<delete-recipe-btn
+							recipe-id="{{ $recipe->id }}"
+							deleted-fail="{{ trans('recipes.deleted_fail') }}"
+							deleting="{{ trans('recipes.deleting') }}"
+							confirm="{{ trans('recipes.are_you_sure_to_delete') }}">
+						</delete-recipe-btn>
+					</li>
+					<li> {{--  Publish button  --}}
+						<a href="#" class="btn-floating green btn-large" title="@lang('recipes.press_to_publish')" id="publish-btn">
+							<i class="large material-icons">send</i>
+						</a>
+						<input type="checkbox" name="ready" value="1" class="d-none" id="ready-checkbox">
+					</li>
+					<li> {{--  View button  --}}
+						<a href="/recipes/{{ $recipe->id }}" class="btn-floating green btn-large" title="@lang('recipes.view_recipe')">
+							<i class="large material-icons">remove_red_eye</i>
+						</a>
+					</li>
+					<li> {{--  Save button  --}}
+						<button type="submit" id="submit-save-recipe" title="@lang('recipes.save_recipe')" class="btn-floating green btn-large">
+							<i class="large material-icons">save</i>
+						</button>
+					</li>
+				</ul>
 			</div>
 		</div>
 
@@ -113,4 +124,8 @@
 	</div>
 </form>
 
+@endsection
+
+@section('script')
+	@include('includes.js.floating_btn')
 @endsection
