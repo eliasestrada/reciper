ce36075f57a874cf2bb6cd30c13d8aa7a9194c1b
Serhii Cho <1990serzhil@gmail.com>
2018-06-16 14:16:05 +0300

    Done with notifications in blade

--- resources/views/users/notifications.blade.php
+++ resources/views/users/notifications.blade.php
@@ -4,23 +4,33 @@
 
 @section('content')
 
-<h1 class="headline">
-	@lang('dashboard.notifications') 
-	{{ count($notifications) > 0 ? ': '.count($notifications) : '' }}
-</h1>
-
-<div class="py-5">
-	@forelse ($notifications as $notification)
-		<div class="notification">
-			<h4 class="notification-title">{{ $notification->title }}</h4>
-			<p class="notification-date">{{ facebookTimeAgo($notification->created_at) }}</p>
-			<p class="notification-message">{{ $notification->message }}</p>
-		</div>
-	@empty
-		<p class="content center-align">@lang('users.u_dont_have_notif')</p>
-	@endforelse
+<div class="py-4 px-2">
+	<div class="center-align">
+		<h1 class="headline">
+			@lang('includes.notifications') 
+			{{ count($notifications) > 0 ? ': '.count($notifications) : '' }}
+		</h1>
+	</div>
+	
+	<div class="row py-5">
+		@forelse ($notifications as $notification)
+			<div class="col s12 m6 l4">
+				<div class="card-panel px-3">
+					<span class="white-text">
+						<h5>{{ $notification->title }}</h5>
+						<p>{{ $notification->message }}</p>
+						<span class="grey-text right">{{ facebookTimeAgo($notification->created_at) }}</span>
+					</span>
+				</div>
+			</div>
+		@empty
+			<div class="center-align">
+				<p class="flow-text grey-text">@lang('users.u_dont_have_notif')</p>
+			</div>
+		@endforelse
+	</div>
+		
+	{{ $notifications->links() }}
 </div>
-
-{{ $notifications->links() }}
 
 @endsection
