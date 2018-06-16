811ceeb53ef23876b120b4d75d8a4855c84811ea
Serhii Cho <1990serzhil@gmail.com>
2018-06-16 12:08:34 +0300

    Done with login and register pages Added rows to lang file form.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,22 +7,27 @@
 <div class="container py-5 px-3">
 	<form method="POST" action="{{ route('login') }}" class="form">
 
-		@csrf <h3 class="headline">@lang('form.login')</h3>
+		@csrf <div class="center-align"><h3 class="headline">@lang('form.login')</h3></div>
 	
 		<div class="input-field">
-			<input id="email" type="email" name="email" class="validate">
+			<input id="email" type="email" name="email" placeholder="@lang('form.email')" class="validate">
 			<label for="email">@lang('form.email')</label>
 		</div>
 	
 		<div class="input-field">
-			<input type="password" name="password" id="password" class="validate">
+			<input type="password" name="password" id="password" placeholder="@lang('form.email')" class="pwd">
 			<label for="password">@lang('form.pwd')</label>
 		</div>
 
 		<div class="mt-3">
 			<label>
 				<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }} />
-				<span>@lang('form.remember_me')</span>
+				<span>
+					@lang('form.remember_me') 
+					<a class="tooltipped" data-tooltip="@lang('form.remember_info')">
+						@lang('form.help')
+					</a>
+				</span>
 			</label>
 		</div>
 	
