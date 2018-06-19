95971b884609c7276df40f964203d178d10a2c1a
Serhii Cho <1990serzhil@gmail.com>
2018-06-19 10:24:00 +0300

    Added class .page and rebuild settings general page

--- resources/views/settings/general.blade.php
+++ resources/views/settings/general.blade.php
@@ -4,56 +4,50 @@
 
 @section('content')
 
-<div class="row">
-	<div class="col m6">
-		<form action="{{ action('SettingsController@updateUserPassword') }}" method="post" class="form">
+<div class="row page">
+	<div class="col s12 m6 mb-5">
+		<div class="center-align">
+			<h2 class="headline">@lang('form.change_pwd')</h2>
+		</div>
 
-			@method('put')
-			@csrf
+		<form action="{{ action('SettingsController@updateUserPassword') }}" method="post">
+			@method('put') @csrf
 		
-			<div class="form-group simple-group">
-				<h2 class="form-headline">
-					<i class="title-icon" style="background: url('/css/icons/svg/question.svg')"></i>
-					@lang('form.change_pwd')
-				</h2>
-				
+			<div class="input-field">
 				<label for="old_password">@lang('form.current_pwd')</label>
 				<input type="password" name="old_password" id="old_password" placeholder="@lang('form.current_pwd')">
 			</div>
 		
-			<div class="form-group simple-group">
+			<div class="input-field">
 				<label for="password">@lang('form.new_pwd')</label>
 				<input type="password" name="password" id="password" placeholder="@lang('form.new_pwd')">
 			</div>
 		
-			<div class="form-group simple-group">
+			<div class="input-field">
 				<label for="password_confirmation">@lang('form.repeat_new_pwd')</label>
 				<input type="password" name="password_confirmation" id="password_confirmation" placeholder="@lang('form.repeat_new_pwd')">
 			</div>
 
-			<div class="form-group simple-group mt-4">
+			<div class="input-field mt-4 center-align">
 				<button class="btn" type="submit">@lang('form.save_changes')</button>
 			</div>
-		
 		</form>
 	</div>
-	<div class="col m6">
-		<form action="{{ action('SettingsController@updateUserData') }}" method="post" class="form">
 
+	<div class="col s12 m6">
+		<div class="center-align">
+			<h2 class="headline">@lang('settings.general')</h2>
+		</div>
+		<form action="{{ action('SettingsController@updateUserData') }}" method="post" class="form">
 			@csrf
 			@method('put')
 		
-			<div class="form-group simple-group">
-				<h2 class="form-headline">
-					<i class="title-icon" style="background: url('/css/icons/svg/wrench.svg')"></i>
-					@lang('settings.general')
-				</h2>
-		
+			<div class="input-field">
 				<label for="name">@lang('form.name')</label>
 				<input type="text" value="{{ user()->name }}" placeholder="@lang('form.name')">
 			</div>
 
-			<div class="form-group simple-group mt-4">
+			<div class="input-field mt-4 center-align">
 				<button class="btn" type="submit">@lang('form.save_changes')</button>
 			</div>
 
