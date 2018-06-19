0cb3c8fefa3b9ab7dfe3e0275e8eeb8012541c39
Serhii Cho <1990serzhil@gmail.com>
2018-06-19 11:47:52 +0300

    Fixed general settings page

--- resources/views/settings/general.blade.php
+++ resources/views/settings/general.blade.php
@@ -11,21 +11,19 @@
 		</div>
 
 		<form action="{{ action('SettingsController@updateUserPassword') }}" method="post">
-			@method('put') @csrf
-		
-			<div class="input-field">
+			<div class="input-field"> @method('put') @csrf
 				<label for="old_password">@lang('form.current_pwd')</label>
-				<input type="password" name="old_password" id="old_password" placeholder="@lang('form.current_pwd')">
+				<input type="password" name="old_password" id="old_password">
 			</div>
 		
 			<div class="input-field">
 				<label for="password">@lang('form.new_pwd')</label>
-				<input type="password" name="password" id="password" placeholder="@lang('form.new_pwd')">
+				<input type="password" name="password" id="password">
 			</div>
 		
 			<div class="input-field">
 				<label for="password_confirmation">@lang('form.repeat_new_pwd')</label>
-				<input type="password" name="password_confirmation" id="password_confirmation" placeholder="@lang('form.repeat_new_pwd')">
+				<input type="password" name="password_confirmation" id="password_confirmation">
 			</div>
 
 			<div class="input-field mt-4 center-align">
@@ -38,19 +36,16 @@
 		<div class="center-align">
 			<h2 class="headline">@lang('settings.general')</h2>
 		</div>
+
 		<form action="{{ action('SettingsController@updateUserData') }}" method="post" class="form">
-			@csrf
-			@method('put')
-		
-			<div class="input-field">
+			<div class="input-field"> @csrf @method('put')
 				<label for="name">@lang('form.name')</label>
-				<input type="text" value="{{ user()->name }}" placeholder="@lang('form.name')">
+				<input type="text" name="name" id="name" value="{{ user()->name }}">
 			</div>
 
 			<div class="input-field mt-4 center-align">
 				<button class="btn" type="submit">@lang('form.save_changes')</button>
 			</div>
-
 		</form>
 	</div>
 </div>
