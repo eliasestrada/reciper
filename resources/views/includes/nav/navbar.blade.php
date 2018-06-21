164592bc3d7161603701b3f60703523e26aa9d59
Serhii Cho <1990serzhil@gmail.com>
2018-06-21 18:17:13 +0300

    Changed navbar blade

--- resources/views/includes/nav/navbar.blade.php
+++ resources/views/includes/nav/navbar.blade.php
@@ -1,34 +1,43 @@
- <!-- Categories -->
-<ul id="dropdown1" class="dropdown-content">
+ <!-- Categories Dropdown menu -->
+ <ul id="dropdown1" class="dropdown-content bottom-borders">
 	@include('includes.nav.categories')
 </ul>
+
+<!-- User Dropdown menu -->
+@auth
+	<ul id="dropdown2" class="dropdown-content bottom-borders">
+		@include('includes.nav.user-menu')
+	</ul>
+@endauth
 
 <nav>
 	<div class="nav-wrapper main" style="z-index:15;">
 		<div class="px-3">
-			<a href="/" title="@lang('includes.home')" class="brand-logo">
-				{{ config('app.name') }}
-			</a>
+			<a href="/" title="@lang('includes.home')" class="brand-logo">{{ config('app.name') }}</a>
 			<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 
-			<ul class="right hide-on-med-and-down">
+			<ul class="right hide-on-med-and-down right-borders">
 				@include('includes.nav.menu')
 
-				<li> <!-- Dropdown Trigger -->
+				<li> <!-- Dropdown Trigger 1 Categories -->
 					<a class="dropdown-trigger" href="#!" data-target="dropdown1">
 						@lang('includes.categories')
 						<i class="material-icons right">arrow_drop_down</i>
 					</a>
 				</li>
-				<li>
-					<form action="{{ action('PagesController@search') }}" method="get">
-						<div class="input-field">
-							<input id="search" type="search" name="for" placeholder="@lang('pages.search_details')" class="fix-input" required>
-							<label class="label-icon" for="search"><i class="material-icons">search</i></label>
-							<i class="material-icons">close</i>
-						</div>
-					</form>
-				</li>
+				@auth
+					<li> <!-- Dropdown Trigger 2 User -->
+						<a class="dropdown-trigger" href="#!" data-target="dropdown2">
+							@lang('includes.profile')
+							<i class="right user-icon">
+								<img src="{{ asset('storage/uploads/'.user()->image) }}" alt="user">
+							</i>
+						</a>
+					</li>
+				@else
+					{{--  --}}
+				@endauth
+				@include('includes.nav.search-form')
 			</ul>
 		</div>
 	</div>
@@ -37,7 +46,6 @@
 <ul class="sidenav" id="mobile-demo">
 	@include('includes.nav.menu')
 
-	<div class="divider"></div>
 	<li> <!-- Categories -->
 		<ul class="collapsible">
 			<li>
@@ -45,14 +53,15 @@
 					<i class="material-icons">arrow_drop_down</i>
 					<span>@lang('includes.categories')</span>
 				</div>
-				<div class="collapsible-body">
-					<ul>
-						@include('includes.nav.categories')
-					</ul>
-				</div>
+				<div class="collapsible-body"><ul>@include('includes.nav.categories')</ul></div>
 			</li>
 		</ul>
 	</li>
+
+	@auth <!-- User Menu -->
+		<div class="divider"></div>
+		<li><ul>@include('includes.nav.user-menu')</ul></li>
+	@endauth
 </ul>
 
 
