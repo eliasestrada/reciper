583da59b72fb5291c246b56c0b2ca17838bc8aa4
Serhii Cho <1990serzhil@gmail.com>
2018-06-21 17:45:54 +0300

    Deleted paralax js include file

--- resources/views/includes/js/parallax.blade.php
+++
@@ -1,6 +0,0 @@
-<script>
-	document.addEventListener('DOMContentLoaded', function() {
-		var elems = document.querySelectorAll('.parallax');
-		var instances = M.Parallax.init(elems);
-	});
-</script>
