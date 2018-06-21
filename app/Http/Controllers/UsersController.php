36739fae8b136861ca9c4c496985ad03b0adbdd6
Serhii Cho <1990serzhil@gmail.com>
2018-06-21 14:22:39 +0300

    Deleted everything related to notifications from UsersController

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Like;
 use App\Models\User;
 use App\Models\Recipe;
-use App\Models\Notification;
 
 class UsersController extends Controller
 {
@@ -44,23 +43,5 @@ class UsersController extends Controller
 		$recipes = Recipe::whereUserId(user()->id)->latest()->paginate(20);
 
 		return view('users.my-recipes')->withRecipes($recipes);
-	}
-
-	public function notifications()
-	{
-		$notifications = Notification::whereUserId(user()->id);
-
-		if (user()->isAdmin()) {
-			$notifications->orWhere('for_admins', 1);
-		}
-
-		$query = $notifications->latest()->paginate(10);
-
-		User::whereId(user()->id)->update([
-			'notif_check' => NOW()
-		]);
-
-		return view('users.notifications')
-			->withNotifications($query);
 	}
 }
