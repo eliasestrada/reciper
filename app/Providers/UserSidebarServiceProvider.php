7c167c2913a6c5d2b8e46691be483d9806dabe21
Serhii Cho <1990serzhil@gmail.com>
2018-06-21 14:48:21 +0300

    Super cleaned UserSidebar ServiceProvider

--- app/Providers/UserSidebarServiceProvider.php
+++ app/Providers/UserSidebarServiceProvider.php
@@ -2,24 +2,30 @@
 
 namespace App\Providers;
 
+use Schema;
 use App\Models\User;
 use App\Models\Recipe;
 use App\Models\Feedback;
 use App\Models\Notification;
-use Schema;
 use Illuminate\Support\ServiceProvider;
 
 class UserSidebarServiceProvider extends ServiceProvider
 {
-    public function boot()
+	/**
+	 * Bootstrap services
+     * @return void
+     */
+    public function boot() : void
     {
 		$this->countAndComposeAllNotifications();
 		$this->countAndComposeAllFeedback();
 		$this->countAndComposeAllUnprovedRecipes();
     }
 
-
-    public function countAndComposeAllNotifications()
+	/**
+     * @return void
+     */
+    public function countAndComposeAllNotifications() : void
     {
         if (Schema::hasTable('notifications')) {
 			view()->composer('includes.user-sidebar', function($view) {
@@ -36,48 +42,68 @@ class UserSidebarServiceProvider extends ServiceProvider
 						])->count();
 						$notifications += $notifications_for_admin;
 					}
-
-					$notifications = empty($notifications) ? '' : 'data-notif='.$notifications;
-					$view->with(compact('notifications'));
+					$view->withNotifications($this->getDataNotifMarkup($notifications));
 				}
 			});
 		} else {
-			logger()->emergency("Table notifications wasn't found while trying to count all notifications, name of the method: countAndComposeAllNotifications");
+			$this->log('notifications', 'countAndComposeAllNotifications');
 		}
 	}
 
-
-	public function countAndComposeAllFeedback()
+	/**
+     * @return void
+     */
+	public function countAndComposeAllFeedback() : void
 	{
 		if (Schema::hasTable('feedback')) {
 			view()->composer('includes.user-sidebar', function($view) {
 				if (user()) {
 					$feed = Feedback::where('created_at', '>', user()->contact_check)->count();
-					$all_feedback = !empty($feed) ? 'data-notif='.$feed : '';
-					$view->with(compact('all_feedback'));
+					$view->withAllFeedback($this->getDataNotifMarkup($feed));
 				}
 			});
 		} else {
-			logger()->emergency("Table feedback wasn't found while trying to count all feedback messages, name of the method: countAndComposeAllFeedback");
+			$this->log('feedback', 'countAndComposeAllFeedback');
 		}
 	}
 
-
-	public function countAndComposeAllUnprovedRecipes()
+	/**
+     * @return void
+     */
+	public function countAndComposeAllUnprovedRecipes() : void
 	{
 		if (Schema::hasTable('recipes')) {
 			view()->composer('includes.user-sidebar', function($view) {
 				if (user()) {
-					$recipes = Recipe
-						::where("approved_" . locale(), 0)
+					$recipes = Recipe::where("approved_" . locale(), 0)
 						->where("ready_" . locale(), 1)
 						->count();
-					$all_unapproved = !empty($recipes) ? 'data-notif='.$recipes : '';
+					$all_unapproved = $this->getDataNotifMarkup($recipes);
 					$view->with(compact('all_unapproved'));
 				}
 			});
 		} else {
-			logger()->emergency("Table recipes wasn't found while trying to count all unproved recipes, name of the method: countAndComposeAllUnprovedRecipes");
+			$this->log('recipes', 'countAndComposeAllUnprovedRecipes');
 		}
+	}
+
+	/**
+	 * Helper
+	 * @return string
+	 * @param int $data
+	 */
+	public function getDataNotifMarkup($data) : string
+	{
+		return !empty($data) ? 'data-notif=' . $data : '';
+	}
+
+	/**
+	 * Helper
+	 * @param string $table
+	 * @param string $method
+	 */
+	public function log($table, $method)
+	{
+		return logger()->emergency("Table $table wasn't found while trying to count all unproved recipes, name of the method: $method");
 	}
 }
