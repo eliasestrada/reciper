99f5c7d3522a27a195cefda611e622bacc16f9cd
Serhii Cho <1990serzhil@gmail.com>
2018-06-27 12:19:58 +0300

    Fixed search form in nav bar, css and blade

--- resources/views/includes/nav/search-form.blade.php
+++ resources/views/includes/nav/search-form.blade.php
@@ -3,7 +3,6 @@
 		<div class="input-field">
 			<input id="search" type="search" name="for" placeholder="@lang('pages.search_details')" required>
 			<label class="label-icon" for="search"><i class="material-icons">search</i></label>
-			<i class="material-icons">close</i>
 		</div>
 	</form>
 </li>
