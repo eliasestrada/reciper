981f83dd1b028e5c39449054add393103720eabc
Serhii Cho <1990serzhil@gmail.com>
2018-06-29 12:10:39 +0300

    - construct method in dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,11 +4,6 @@ namespace App\Http\Controllers;
 
 class DashboardController extends Controller
 {
-    public function __construct()
-    {
-		$this->middleware('auth');
-	}
-
     public function index()
     {
 		return redirect('users/' . user()->id);
