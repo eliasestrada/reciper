59544226feb81dbef9f247a9285d3b787552cd4c
Serhii Cho <1990serzhil@gmail.com>
2018-06-30 19:30:01 +0300

    Done for the most part with _master logs template

--- resources/views/vendor/log-viewer/custom-theme/_master.blade.php
+++ resources/views/vendor/log-viewer/custom-theme/_master.blade.php
@@ -1,19 +1,14 @@
 <!doctype html>
 <html lang="{{ locale() }}">
 <head>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	
 	@yield('head')
 	@include('includes.head')
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
 	{!! styleTimestamp('/css/logs.css') !!}
 
-	<title>@yield('title') {{ log_viewer()->version() }}</title>
+	<title>@lang('logs.logs') {{ log_viewer()->version() }}</title>
 </head>
 <body>
-
 	@include('includes.nav.navbar')
 
 	<div id="app" class="wrapper">
@@ -21,20 +16,13 @@
 	</div>
 
 	@include('includes.footer')
-
-    {{-- Scripts --}}
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
-
-    @yield('modals')
-	@yield('scripts')
+</body>
 	{!! scriptTimestamp('/js/vue.js') !!}
 	{!! scriptTimestamp('/js/vanilla.js') !!}
+
 	@include('includes.js.dropdown')
 	@include('includes.js.sidenav')
 	@include('includes.js.tooltip')
 	@include('includes.js.collapsible')
 	@include('includes.messages')
-</body>
 </html>
