f8bec58f5921e5de6895163874d4a6124c879d3b
Serhii Cho <1990serzhil@gmail.com>
2018-06-30 19:28:42 +0300

    Changed logs lang file

--- resources/lang/ru/logs.php
+++ resources/lang/ru/logs.php
@@ -2,9 +2,17 @@
 
 return [
 
+	// Views
 	'home' => 'Главная',
 	'dashboard' => 'Панель',
 	'logs' => 'Журнал',
+	'page' => 'Страница',
+	'of' => 'из',
+	'path_to_file' => 'Путь к файлу',
+	'all_files' => 'Кол-во файлов',
+	'file_size' => 'Размер файла',
+	'created_at' => 'Дата создания',
+	'updated_at' => 'Дата обновления',
 
 	// Controllers
 	'deleted' => 'Запись удаленна',
