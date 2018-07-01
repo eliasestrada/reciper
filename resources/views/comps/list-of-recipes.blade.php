1f384760ad96163bcb29857bfc8324f8e0ac3b8c
Serhii Cho <1990serzhil@gmail.com>
2018-07-01 17:14:15 +0300

    Added ul tags to list-of-items blade

--- resources/views/comps/list-of-recipes.blade.php
+++ resources/views/comps/list-of-recipes.blade.php
@@ -10,23 +10,25 @@
 	<div class="item-list unstyled-list row">
 		@isset($recipes)
 			@forelse ($recipes as $recipe)
-				<li style="border-left-color:#{{ $recipe->done() ? '65b56e' : 'ce7777' }};" class="col s12 m6 l4" title="{{ $recipe->getTitle() }}">
-					<a href="/recipes/{{ $recipe->id }}">
-						<img src="{{ asset('storage/images/'.$recipe->image) }}" alt="{{ $recipe->getTitle() }}" />
-					</a>
-
-					<div class="item-content">
-						<section>{{ $recipe->getTitle() }}</section>
-						<section>
-							@lang('users.date') {{ timeAgo($recipe->updated_at) }}
-						</section>
-						<section>
-							<span class="new badge mt-2 {{ $recipe->done() ? 'green' : 'red' }}">
-								@lang('users.status'): {{ $recipe->getStatus() }}
-							</span>
-						</section>
-					</div>
-				</li>
+				<ul>
+					<li style="border-left-color:#{{ $recipe->done() ? '65b56e' : 'ce7777' }};" class="col s12 m6 l4" title="{{ $recipe->getTitle() }}">
+						<a href="/recipes/{{ $recipe->id }}">
+							<img src="{{ asset('storage/images/'.$recipe->image) }}" alt="{{ $recipe->getTitle() }}" />
+						</a>
+	
+						<div class="item-content">
+							<section>{{ $recipe->getTitle() }}</section>
+							<section>
+								@lang('users.date') {{ timeAgo($recipe->updated_at) }}
+							</section>
+							<section>
+								<span class="new badge mt-2 {{ $recipe->done() ? 'green' : 'red' }}">
+									@lang('users.status'): {{ $recipe->getStatus() }}
+								</span>
+							</section>
+						</div>
+					</li>
+				</ul>
 			@empty
 				@isset($no_recipes)
 					@component('comps.empty')
