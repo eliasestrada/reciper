d97a3b0979224a0df8771b6f84a1929659ccd644
Serhii Cho <1990serzhil@gmail.com>
2018-07-01 15:40:14 +0300

    Added shadows to messages

--- resources/views/includes/messages.blade.php
+++ resources/views/includes/messages.blade.php
@@ -1,29 +1,27 @@
-@isset($errors)
-	@if (count($errors) > 0)
-		@foreach ($errors->all() as $error)
+@if (count($errors) > 0)
+	@foreach ($errors->all() as $error)
+	<script>
+		let message = ''
+	</script>
 		<script>
-			let message = ''
+			message += '{{ $error }} <br />'
 		</script>
-			<script>
-				message += '{{ $error }} <br />'
-			</script>
-		@endforeach
-		<script>
-			M.toast({html: message, displayLength: 10000, classes: 'red'})
-		</script>
-	@endif
-@endisset
+	@endforeach
+	<script>
+		M.toast({html: message, displayLength: 10000, classes: 'red z-depth-2'})
+	</script>
+@endif
 
 @if (session('success'))
 	<script>
 		let message = '{{ @session('success') }}'
-		M.toast({html: message, displayLength: 10000, classes: 'green'})
+		M.toast({html: message, displayLength: 10000, classes: 'green z-depth-2'})
 	</script>
 @endif
 
 @if (session('error'))
 	<script>
 		let message = '{{ @session('error') }}'
-		M.toast({html: message, displayLength: 10000, classes: 'red'})
+		M.toast({html: message, displayLength: 10000, classes: 'red z-depth-2'})
 	</script>
 @endif
