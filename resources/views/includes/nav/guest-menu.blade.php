bf52f59deaf8f2db0f864b8a8ba1f027a2cbca38
Serhii Cho <1990serzhil@gmail.com>
2018-07-03 14:56:21 +0300

    Добавил guest-menu и изменил munu.blade.php

---
+++ resources/views/includes/nav/guest-menu.blade.php
@@ -0,0 +1,12 @@
+<li>
+	<a href="#" data-target="dropdown3" title="@lang('includes.add')">
+		<i class="material-icons left">add</i>
+		@lang('includes.add')
+	</a>
+</li>
+<li>
+	<a href="/login" data-target="dropdown3" title="@lang('includes.enter')">
+		@lang('includes.enter')
+		<i class="material-icons left">keyboard_arrow_left</i>
+	</a>
+</li>
--- resources/views/includes/nav/menu.blade.php
+++ resources/views/includes/nav/menu.blade.php
@@ -1,6 +1,18 @@
 <li class="{{ activeIfRouteIs('/') }}">
-	<a href="/" title="@lang('includes.home')">@lang('includes.home')</a>
+	<a href="/" title="@lang('includes.home')">
+		<i class="material-icons left">home</i>
+		@lang('includes.home')
+	</a>
 </li>
 <li class="{{ activeIfRouteIs('recipes') }}">
-	<a href="/recipes" title="@lang('includes.recipes')">@lang('includes.recipes')</a>
+	<a href="/recipes" title="@lang('includes.recipes')">
+		<i class="material-icons left">restaurant</i>
+		@lang('includes.recipes')
+	</a>
+</li>
+<li>
+	<a href="#" title="@lang('includes.search')">
+		<i class="material-icons left">search</i>
+		@lang('includes.search')
+	</a>
 </li>
