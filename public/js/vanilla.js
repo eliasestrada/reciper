73a640fd4bc400dc8a5e6f5ba3bb35f03ef087e9
Serhii Cho <1990serzhil@gmail.com>
2018-07-03 18:08:38 +0300

    Создал файл в папке vanilla с названием _navSearchForm.js

--- public/js/vanilla.js
+++ public/js/vanilla.js
@@ -10635,3 +10635,14 @@ if ($('search-form')) {
 		});
 	})();
 }
+if ($('nav-btn-for-search-1')) {
+	activeAfterClickBtn('nav-search-form', 'nav-btn-for-search-1');
+}
+
+if ($('nav-btn-for-search-2')) {
+	activeAfterClickBtn('nav-search-form', 'nav-btn-for-search-2');
+}
+
+if ($('nav-btn-for-search-3')) {
+	activeAfterClickBtn('nav-search-form', 'nav-btn-for-search-3');
+}
---
+++ resources/assets/js/vanilla/components/_navSearchForm.js
@@ -0,0 +1,11 @@
+if ($('nav-btn-for-search-1')) {
+	activeAfterClickBtn('nav-search-form', 'nav-btn-for-search-1')
+}
+
+if ($('nav-btn-for-search-2')) {
+	activeAfterClickBtn('nav-search-form', 'nav-btn-for-search-2')
+}
+
+if ($('nav-btn-for-search-3')) {
+	activeAfterClickBtn('nav-search-form', 'nav-btn-for-search-3')
+}
