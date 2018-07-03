4eb3bb97a97859a545036f6977f7483340dc358b
Serhii Cho <1990serzhil@gmail.com>
2018-07-03 18:12:01 +0300

    Удалил search-form файл с includes он уже не нужен

--- resources/views/includes/nav/search-form.blade.php
+++
@@ -1,8 +0,0 @@
-<li>
-	<form action="{{ action('PagesController@search') }}" method="get">
-		<div class="input-field">
-			<input id="search" type="search" name="for" placeholder="@lang('pages.search_details')" required>
-			<label class="label-icon" for="search"><i class="material-icons">search</i></label>
-		</div>
-	</form>
-</li>
