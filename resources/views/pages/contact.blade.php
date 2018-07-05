153ff75e880e826fbd55745689f64ea1ab092de6
Serhii Cho <1990serzhil@gmail.com>
2018-07-05 18:14:54 +0300

    Added descriptions to contact page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -6,6 +6,7 @@
 
 <div class="page container">
 	<div class="center"><h1 class="headline">@lang('pages.feedback')</h1></div>
+	<p>@lang('contact.intro')</p>
 
 	@if (session('success'))
 		<div class="center mt-4">
@@ -16,16 +17,21 @@
 		<form action="{{ action('ContactController@store') }}" method="post">
 			<div class="input-field"> @csrf
 				<i class="material-icons prefix">email</i>
-				<label for="email">@lang('form.email')</label>
 				<input type="email" name="email" id="email">
+				<label for="email">@lang('form.email')</label>
+				<span class="helper-text">@lang('form.email_desc')</span>
 			</div>
 			<div class="input-field">
-				<i class="material-icons prefix">message</i>
+				<i class="material-icons prefix">comment</i>
 				<textarea name="message" id="message" class="materialize-textarea counter" data-length="{{ config('validation.contact_message') }}"></textarea>
 				<label for="message">@lang('form.message')</label>
+				<span class="helper-text">@lang('form.message_desc')</span>
 			</div>
 			<div class="input-field">
-				<button type="submit" class="btn btn-main">@lang('form.send')</button>
+				<button type="submit" class="btn btn-main">
+					<i class="material-icons right">send</i>
+					@lang('form.send')
+				</button>
 			</div>
 		</form>
 	@endif
