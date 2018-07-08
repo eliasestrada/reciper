fc58d114535d73923b1ded973d3719734283b524
Serhii Cho <1990serzhil@gmail.com>
2018-07-08 14:52:38 +0300

    Исправил ошибки в папки Request и контроллере рецептов

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -7,8 +7,8 @@ use App\Models\Meal;
 use App\Models\Recipe;
 use App\Helpers\Traits\RecipesControllerHelpers;
 use App\Helpers\Contracts\SaveRecipeDataContract;
-use App\Http\Requests\Settings\RecipeSaveRequest;
-use App\Http\Requests\Settings\RecipePublichRequest;
+use App\Http\Requests\Recipes\RecipeSaveRequest;
+use App\Http\Requests\Recipes\RecipePublichRequest;
 use App\Http\Responses\Controllers\RecipeUpdateResponse;
 
 class RecipesController extends Controller
--- app/Http/Requests/Recipes/RecipePublichRequest.php
+++ app/Http/Requests/Recipes/RecipePublichRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Settings;
+namespace App\Http\Requests\Recipes;
 
 use App\Models\Category;
 use App\Rules\UniqueCategory;
