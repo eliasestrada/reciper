69eef2f9a349c7246b6aa74dc31ab3a6a3ef7398
Serhii Cho <1990serzhil@gmail.com>
2018-07-08 19:14:12 +0300

    Добавил old input к login странице

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
 		@csrf <div class="center"><h3 class="headline">@lang('form.login')</h3></div>
 	
 		<div class="input-field">
-			<input id="email" type="email" name="email" class="validate">
+			<input id="email" value="{{ old('email') }}" type="email" name="email" class="validate">
 			<label for="email">@lang('form.email')</label>
 		</div>
 	
