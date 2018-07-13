007f1dc493abf53795c7c7fb25c62b936db924f3
Serhii Cho <1990serzhil@gmail.com>
2018-07-13 15:21:33 +0300

    Added helpers to Title model

--- app/Models/Title.php
+++ app/Models/Title.php
@@ -8,4 +8,20 @@ class Title extends Model
 {
 	protected $guarded = ['id'];
 	public $timestamps  = false;
+
+	/**
+	 * @return string
+	 */
+	public function getTitle() : ? string
+	{
+		return $this->toArray()['title_' . locale()];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getText() : ? string
+	{
+		return $this->toArray()['text_' . locale()];
+	}
 }
