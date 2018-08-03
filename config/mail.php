005c6289b40c9a1b52f0298a5789e1ab60837aad
Serhii Cho <1990serzhil@gmail.com>
2018-08-03 10:41:33 +0300

    Cleaned

--- config/mail.php
+++ config/mail.php
@@ -7,10 +7,6 @@ return [
     | Mail Driver
     |--------------------------------------------------------------------------
     |
-    | Laravel supports both SMTP and PHP's "mail" function as drivers for the
-    | sending of e-mail. You may specify which one you're using throughout
-    | your application here. By default, Laravel is setup for SMTP mail.
-    |
     | Supported: "smtp", "sendmail", "mailgun", "mandrill", "ses",
     |            "sparkpost", "log", "array"
     |
