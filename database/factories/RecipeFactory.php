4dbac4a4fbccf29f00f0bcbf0068200cb0bb9537
Serhii Cho <1990serzhil@gmail.com>
2018-08-04 13:11:39 +0300

    Fixed RecipeFactory

--- database/factories/RecipeFactory.php
+++ database/factories/RecipeFactory.php
@@ -1,31 +1,29 @@
 <?php
 
-use App\Models\Meal;
-use App\Models\User;
 use App\Models\Recipe;
 use Faker\Generator as Faker;
 
 $factory->define(Recipe::class, function (Faker $faker) {
     return [
-		'user_id' => rand(1, User::count()),
-		'meal_id' => rand(1, 3),
-		'time' => rand(10, 160),
-		'image' => 'default.jpg',
+        'user_id' => 1,
+        'meal_id' => rand(1, 3),
+        'time' => rand(10, 160),
+        'image' => 'default.jpg',
 
-		// Russian language
+        // Russian language
         'title_ru' => $faker->sentence,
-		'intro_ru' => $faker->paragraph,
-		'ingredients_ru' => $faker->paragraph,
-		'text_ru' => $faker->paragraph,
-		'ready_ru' => 1,
-		'approved_ru' => 1,
+        'intro_ru' => $faker->paragraph,
+        'ingredients_ru' => $faker->paragraph,
+        'text_ru' => $faker->paragraph,
+        'ready_ru' => 1,
+        'approved_ru' => 1,
 
-		// English language
+        // English language
         'title_en' => $faker->sentence,
-		'intro_en' => $faker->paragraph,
-		'ingredients_en' => $faker->paragraph,
-		'text_en' => $faker->paragraph,
-		'ready_en' => 1,
-		'approved_en' => 1
+        'intro_en' => $faker->paragraph,
+        'ingredients_en' => $faker->paragraph,
+        'text_en' => $faker->paragraph,
+        'ready_en' => 1,
+        'approved_en' => 1,
     ];
 });
