4c82452ecfe02d2a593bc1afa6cc44bbc0cbc581
Serhii Cho <1990serzhil@gmail.com>
2018-08-09 13:58:38 +0300

    Moved js and sass folders from assets to resources

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,10 @@
 let mix = require('laravel-mix');
 
 var vanillaFilesToCompile = [
-	'resources/assets/js/vanilla/modules.js',
-	'resources/assets/js/vanilla/materialize.min.js',
-	'resources/assets/js/vanilla/functions/_*.js',
-	'resources/assets/js/vanilla/components/_*.js',
+	'resources/js/vanilla/modules.js',
+	'resources/js/vanilla/materialize.min.js',
+	'resources/js/vanilla/functions/_*.js',
+	'resources/js/vanilla/components/_*.js',
 ];
 
 var combineJsFrom = [
@@ -17,13 +17,13 @@ var combineJsTo = 'public/js/app.js'
 var browserSyncOptions = {
 	proxy: 'localhost:8000',
 	browser: 'firefox',
-	files: ['resources/assets/sass/**/*']
+	files: ['resources/sass/**/*']
 }
 
-mix.js('resources/assets/js/vue/vue.js', 'public/js/vendor')
+mix.js('resources/js/vue/vue.js', 'public/js/vendor')
 	.sourceMaps()
 	.babel(vanillaFilesToCompile, 'public/js/vendor/vanilla.js')
 	.combine(combineJsFrom, combineJsTo)
-	.sass('resources/assets/sass/app.scss', 'public/css/app.css')
+	.sass('resources/sass/app.scss', 'public/css/app.css')
 	.browserSync(browserSyncOptions)
 	.options({ processCssUrls: false })
