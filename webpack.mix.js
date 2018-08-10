014696a47d45fced980f083d901b4a5cee473866
Serhii Cho <1990serzhil@gmail.com>
2018-08-10 14:42:49 +0300

    Fixed webpack and js files structure

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,29 +1,23 @@
 let mix = require('laravel-mix');
 
-var vanillaFilesToCompile = [
-	'resources/js/vanilla/modules.js',
-	'resources/js/vanilla/materialize.min.js',
-	'resources/js/vanilla/functions/_*.js',
-	'resources/js/vanilla/components/_*.js',
-];
-
-var combineJsFrom = [
-	'public/js/vendor/vanilla.js',
-	'public/js/vendor/vue.js'
-]
-
-var combineJsTo = 'public/js/app.js'
-
-var browserSyncOptions = {
-	proxy: 'localhost:8000',
-	browser: 'firefox',
-	files: ['resources/sass/**/*']
-}
-
 mix.js('resources/js/vue/vue.js', 'public/js/vendor')
 	.sourceMaps()
-	.babel(vanillaFilesToCompile, 'public/js/vendor/vanilla.js')
-	.combine(combineJsFrom, combineJsTo)
+	.babel([
+		'resources/js/vanilla/modules.js',
+		'resources/js/vanilla/materialize.min.js',
+		'resources/js/vanilla/functions/_*.js',
+		'resources/js/vanilla/components/_*.js',
+	], 'public/js/vendor/vanilla.js')
+	.combine([
+		'public/js/vendor/vue.js',
+		'public/js/vendor/vanilla.js'
+	], 'public/js/app.js')
 	.sass('resources/sass/app.scss', 'public/css/app.css')
-	.browserSync(browserSyncOptions)
-	.options({ processCssUrls: false })
+	.browserSync({
+		proxy: 'localhost:8000',
+		browser: 'firefox',
+		files: ['resources/sass/**/*', 'resources/js/**/*']
+	})
+	.options({
+		processCssUrls: false
+	})
