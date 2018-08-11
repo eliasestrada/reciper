e18f24aced49d752682b516c74524e72d6f9b533
Serhii Cho <1990serzhil@gmail.com>
2018-08-11 14:16:47 +0300

    Refactored Unit test HelperFunctionsTest

--- tests/Unit/HelperFunctionsTest.php
+++ tests/Unit/HelperFunctionsTest.php
@@ -26,28 +26,16 @@ class HelperFunctionsTest extends TestCase
     public function check_get_online_icon_helper(): void
     {
         // Icon should be on ===================
-        $expected = '<span class="online-icon-on"></span>';
-
-        $current = getOnlineIcon('30 ' . trans('date.second') . ' ' . trans("date.ago"));
-        $this->assertEquals($expected, $current);
-
-        $current = getOnlineIcon('30 ' . trans('date.seconds') . ' ' . trans("date.ago"));
-        $this->assertEquals($expected, $current);
-
-        $current = getOnlineIcon('30 ' . trans('date.seconds2') . ' ' . trans("date.ago"));
-        $this->assertEquals($expected, $current);
+        array_map(function ($second) {
+            $current = getOnlineIcon("30 $second " . trans("date.ago"));
+            $this->assertEquals('<span class="online-icon-on"></span>', $current);
+        }, [trans('date.second'), trans('date.seconds'), trans('date.seconds2')]);
 
         // Icon should be off ===================
-        $expected = '<span class="online-icon-off"></span>';
-
-        $current = getOnlineIcon('30 ' . trans('date.minute') . ' ' . trans("date.ago"));
-        $this->assertEquals($expected, $current);
-
-        $current = getOnlineIcon('30 ' . trans('date.minutes') . ' ' . trans("date.ago"));
-        $this->assertEquals($expected, $current);
-
-        $current = getOnlineIcon('30 ' . trans('date.minutes2') . ' ' . trans("date.ago"));
-        $this->assertEquals($expected, $current);
+        array_map(function ($minute) {
+            $current = getOnlineIcon("30 $minute " . trans("date.ago"));
+            $this->assertEquals('<span class="online-icon-off"></span>', $current);
+        }, [trans('date.minute'), trans('date.minutes'), trans('date.minutes2')]);
     }
 
     /**
@@ -56,14 +44,12 @@ class HelperFunctionsTest extends TestCase
      */
     public function check_readable_number_helper(): void
     {
-        $this->assertEquals(readableNumber(999),
-            999);
-        $this->assertEquals(readableNumber(1000),
-            '1<br /><small>' . trans('users.thousand') . '</small>');
-        $this->assertEquals(readableNumber(99999),
-            '99<br /><small>' . trans('users.thousand') . '</small>');
-        $this->assertEquals(readableNumber(1000000),
-            '1<br /><small>' . trans('users.million') . '</small>');
+        $thousand = trans('users.thousand');
+        $million = trans('users.million');
+
+        $this->assertEquals(readableNumber(999), 999);
+        $this->assertEquals(readableNumber(1000), "1<br /><small>$thousand</small>");
+        $this->assertEquals(readableNumber(1000000), "1<br /><small>$million</small>");
     }
 
     /**
@@ -72,11 +58,8 @@ class HelperFunctionsTest extends TestCase
      */
     public function check_get_rating_number_helper(): void
     {
-        $recipes = [
-            factory(Recipe::class)->make([
-                'user_id' => factory(User::class)->make()->id,
-            ]),
-        ];
+        $user = factory(User::class)->make();
+        $recipes = [factory(Recipe::class)->make(['user_id' => $user->id])];
 
         $this->assertEquals(getRatingNumber($recipes, $likes = 11), 2.1);
         $this->assertEquals(getRatingNumber($recipes, $likes = -11), 1);
