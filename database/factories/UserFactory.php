4898c6ba4e6fbf907c3c33c166f8467fb079401a
Serhii Cho <1990serzhil@gmail.com>
2018-08-11 18:20:12 +0300

    Fixed UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,17 +3,6 @@
 use App\Models\User;
 use Faker\Generator as Faker;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
- */
-
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
@@ -23,8 +12,8 @@ $factory->define(User::class, function (Faker $faker) {
         'admin' => 0,
         'master' => 0,
         'image' => 'default.jpg',
-        "notif_check" => date('Y-m-d H:i:s'),
-        "contact_check" => date('Y-m-d H:i:s'),
-        "last_visit_at" => date('Y-m-d H:i:s'),
+        "notif_check" => now(),
+        "contact_check" => now(),
+        "last_visit_at" => now(),
     ];
 });
