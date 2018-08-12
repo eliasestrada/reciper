5173dd3be19a1c3dcd04de119642bb28edfa3504
Serhii Cho <1990serzhil@gmail.com>
2018-08-12 16:59:28 +0300

    Fixed notification blade file

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -8,7 +8,7 @@
 	<div class="center">
 		<h1 class="headline">
 			@lang('includes.notifications') 
-			{{ count($notifications) > 0 ? ': '.count($notifications) : '' }}
+			{{ $notifications->count() > 0 ? ": $notifications" : '' }}
 		</h1>
 	</div>
 	
@@ -21,10 +21,15 @@
 						<p>{{ trans($notif->message) }}</p>
 						<hr /><p>{{ $notif->data }}</p>
 						<span class="grey-text right">{{ timeAgo($notif->created_at) }}</span>
+
 						@if ($notif->for_admins === 0)
 							<form action="{{ action('NotificationController@destroy', ['notification' => $notif->id]) }}" method="post" onsubmit='return confirm("@lang('notifications.sure_to_delete')")'>
+
 								@csrf @method('delete')
-								<button class="btn" title="@lang('form.deleting')">@lang('form.deleting')</button>
+
+								<button class="btn" title="@lang('form.deleting')">
+									@lang('form.deleting')
+								</button>
 							</form>
 						@endif
 					</span>
