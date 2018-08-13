d86583ebcc1f4db4cbbe1e2b31898acd353ecfc5
Serhii Cho <1990serzhil@gmail.com>
2018-08-13 21:12:54 +0300

    Changed recipe title max length to 85

--- config/validation.php
+++ config/validation.php
@@ -2,17 +2,17 @@
 
 return [
 
-	// Recipes
-	'title_max' => 190,
-	'intro_max' => 2000,
-	'ingredients_max' => 5000,
-	'text_max' => 10000,
-	
-	// Contact form
-	'contact_message' => 5000,
-	
-	// Documents
-	'docs_title_max' => 190,
-	'docs_text_max' => 10000,
+    // Recipes
+    'title_max' => 85,
+    'intro_max' => 2000,
+    'ingredients_max' => 5000,
+    'text_max' => 10000,
+
+    // Contact form
+    'contact_message' => 5000,
+
+    // Documents
+    'docs_title_max' => 190,
+    'docs_text_max' => 10000,
 
 ];
