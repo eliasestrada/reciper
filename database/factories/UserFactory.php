3970b94bcd01c5c7fe7966acbdb7a2ef1a11593b
Serhii Cho <1990serzhil@gmail.com>
2018-08-13 21:50:30 +0300

    Added image for user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,20 @@ use App\Models\User;
 use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
+    $path_slug = date('Y') . '/' . date('n');
+    $path_to = storage_path("app/public/users/$path_slug");
+    $path_from = base_path('../tools/people');
+
+    if (!\File::exists($path_to)) {
+        \File::makeDirectory($path_to, 0777, true);
+    }
+
+    if (app()->env === 'testing') {
+        $img = 'default.jpg';
+    } else {
+        $img = $path_slug . '/' . $faker->file($path_from, $path_to, false);
+    }
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
@@ -11,7 +25,8 @@ $factory->define(User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
         'admin' => 0,
         'master' => 0,
-        'image' => 'default.jpg',
+        //'image' => 'default.jpg',
+        'image' => $img,
         "notif_check" => now(),
         "contact_check" => now(),
         "last_visit_at" => now(),
