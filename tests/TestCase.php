fea267fa032d3395475da587f5a8116d7dedc2fb
Serhii Cho <1990serzhil@gmail.com>
2018-08-18 18:40:15 +0300

    Added data to Unit Test Case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,21 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $migrate = false;
+    protected static $migrationsRun = false;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->app['config']->set('database.connections.mysql', [
+            'database' => 'reciper_testing',
+        ]);
+
+        if ($this->migrate === true && !static::$migrationsRun) {
+            \Artisan::call('wipe');
+            static::$migrationsRun = true;
+        }
+    }
 }
