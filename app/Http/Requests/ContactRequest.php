c11f36b553d549a024e8197da275f99597df9501
Serhii Cho <1990serzhil@gmail.com>
2018-08-30 16:13:25 +0300

    Added comment to ContactRequest

--- app/Http/Requests/ContactRequest.php
+++ app/Http/Requests/ContactRequest.php
@@ -6,29 +6,38 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ContactRequest extends FormRequest
 {
-    // Determine if the user is authorized to make this request.
+    /**
+     * Determine if the user is authorized to make this request.
+     * @return bool
+     */
     public function authorize()
     {
         return true;
     }
 
-    // Get the validation rules that apply to the request.
+    /**
+     * Get the validation rules that apply to the request.
+     * @return array
+     */
     public function rules()
     {
         return [
-			'email' => 'required|email',
-			'message' => 'required|min:20|max:' . config('validation.contact_message')
+            'email' => 'required|email',
+            'message' => 'required|min:20|max:' . config('validation.contact_message'),
         ];
-	}
-	
-	public function messages()
+    }
+
+    /**
+     * @return array
+     */
+    public function messages()
     {
         return [
-			'email.required' => trans('contact.contact_email_required'),
-			'message.required' => trans('contact.contact_message_required'),
-			'email.email' => trans('contact.contact_email_email'),
-			'message.min' => trans('contact.contact_message_min'),
-			'message.max' => trans('contact.contact_message_max')
-		];
+            'email.required' => trans('contact.contact_email_required'),
+            'message.required' => trans('contact.contact_message_required'),
+            'email.email' => trans('contact.contact_email_email'),
+            'message.min' => trans('contact.contact_message_min'),
+            'message.max' => trans('contact.contact_message_max'),
+        ];
     }
 }
