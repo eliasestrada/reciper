c92b2c782d3258cfaad86248d46b2dfbeb6cbb8c
Serhii Cho <1990serzhil@gmail.com>
2018-08-30 16:17:27 +0300

    Changed some notification messages in lang file

--- resources/lang/ru/notifications.php
+++ resources/lang/ru/notifications.php
@@ -2,16 +2,20 @@
 
 return [
 
-	// Controllers
-	'recipe_published' => 'Рецепт опубликован',
-	'recipe_with_title_published' => 'Ваш рецепт был опубликован.',
-	'recipe_not_published' => 'Рецепт не опубликован',
-	'recipe_with_title_not_published' => 'Ваш рецепт не был опубликован так как администрация венула его вам на переработку.',
-	'title_script_attack' => 'Скрипт аттака',
-	'message_script_attack' => 'Один из пользователь использовал Javascript тэг <script> в одном из своих рецептов. Срочно проверьте журнал',
-	'deleted' => 'Сообщение было успешно удалено',
-	
-	// View
-	'sure_to_delete' => 'Вы точно хотите удалить это сообщение?'
+    // Controllers
+    'recipe_published' => 'Рецепт опубликован!',
+    'recipe_not_published' => 'Рецепт не опубликован!',
+    'title_script_attack' => 'Скрипт аттака',
+    'message_script_attack' => 'Один из пользователь использовал Javascript тэг <script> в одном из своих рецептов. Срочно проверьте журнал',
+    'deleted' => 'Сообщение было успешно удалено',
+
+    // Validations
+    'message_required' => 'Поле для ввода сообщения обязательно к заполнению',
+    'message_min' => 'Сообщение должно быть не менее :min символов',
+    'message_max' => 'Сообщение должно быть не более :max символов',
+
+    // View
+    'sure_to_delete' => 'Вы точно хотите удалить это сообщение?',
+    'set_message' => 'Введите сообщение автору рецепта',
 
 ];
