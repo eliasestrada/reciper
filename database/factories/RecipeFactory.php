7dac85bd624e5411009bf179af0fcbb0b0bbfdbc
Serhii Cho <1990serzhil@gmail.com>
2018-09-01 12:36:47 +0300

    Changed 2 factories

--- database/factories/RecipeFactory.php
+++ database/factories/RecipeFactory.php
@@ -5,21 +5,21 @@ use App\Models\User;
 use Faker\Generator as Faker;
 
 $factory->define(Recipe::class, function (Faker $faker) {
-    if (app()->env === 'testing') {
-        $img = 'default.jpg';
-    } else {
-        $img = $faker->file(base_path('../tools/food'),
-            storage_path('/app/public/images'), false);
+    // if (app()->env === 'testing') {
+    //     $img = 'default.jpg';
+    // } else {
+    //     $img = $faker->file(base_path('../tools/food'),
+    //         storage_path('/app/public/images'), false);
 
-        copy(storage_path("/app/public/images/$img"),
-            storage_path("/app/public/images/small/$img"));
-    }
+    //     copy(storage_path("/app/public/images/$img"),
+    //         storage_path("/app/public/images/small/$img"));
+    // }
 
     return [
         'user_id' => factory(User::class)->create()->id,
         'meal_id' => rand(1, 3),
         'time' => rand(10, 160),
-        'image' => $img,
+        'image' => 'default.jpg',
 
         // Russian language
         'title_ru' => $faker->sentence,
