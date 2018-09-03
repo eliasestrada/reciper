bbb80cdb07d0b3a2c753c50e91ccdacdf8a61dce
SerhiiCho <serhiicho@inbox.ru>
2018-09-03 15:51:33 +0300

    Changed recipe migration added simple column

--- database/migrations/2018_02_22_115608_create_recipes_table.php
+++ database/migrations/2018_02_22_115608_create_recipes_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateRecipesTable extends Migration
 {
@@ -10,33 +10,33 @@ class CreateRecipesTable extends Migration
     public function up()
     {
         Schema::create('recipes', function (Blueprint $table) {
-			$table->increments('id');
-			$table->unsignedInteger('user_id')->default(1);
-			$table->unsignedInteger('meal_id')->default(1);
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->default(1);
+            $table->unsignedInteger('meal_id')->default(1);
 
-			// Russian language
+            // Russian language
             $table->string('title_ru')->nullable();
             $table->text('intro_ru')->nullable();
             $table->text('ingredients_ru')->nullable();
-			$table->text('text_ru')->nullable();
+            $table->text('text_ru')->nullable();
             $table->boolean('ready_ru')->default(0);
-			$table->boolean('approved_ru')->default(0);
-			
-			// English language
+            $table->boolean('approved_ru')->default(0);
+
+            // English language
             $table->string('title_en')->nullable();
             $table->text('intro_en')->nullable();
             $table->text('ingredients_en')->nullable();
             $table->text('text_en')->nullable();
             $table->boolean('ready_en')->default(0);
-			$table->boolean('approved_en')->default(0);
+            $table->boolean('approved_en')->default(0);
 
-			// Other
+            // Other
             $table->unsignedSmallInteger('time')->default(0);
-			$table->string('image')->default('default.jpg');
-			$table->timestamps();
+            $table->string('image')->default('default.jpg');
+            $table->boolean('simple')->default(0);
+            $table->timestamps();
         });
     }
-
 
     public function down()
     {
