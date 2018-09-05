392e76b7d0d1a3acab505e5256eaf2dfa6b0334a
SerhiiCho <serhiicho@inbox.ru>
2018-09-05 22:16:58 +0300

    Changed webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,23 +1,25 @@
-let mix = require('laravel-mix');
+let mix = require("laravel-mix");
 
-mix.js('resources/js/vue/vue.js', 'public/js/vendor')
-	.sourceMaps()
-	.babel([
-		'resources/js/vanilla/modules.js',
-		'resources/js/vanilla/materialize.min.js',
-		'resources/js/vanilla/functions/_*.js',
-		'resources/js/vanilla/components/_*.js',
-	], 'public/js/vendor/vanilla.js')
-	.combine([
-		'public/js/vendor/vue.js',
-		'public/js/vendor/vanilla.js'
-	], 'public/js/app.js')
-	.sass('resources/sass/app.scss', 'public/css/app.css')
-	.browserSync({
-		proxy: 'localhost:8000',
-		browser: 'firefox',
-		files: ['resources/sass/**/*', 'resources/js/**/*']
-	})
-	.options({
-		processCssUrls: false
-	})
+mix.js("resources/js/vue/vue.js", "public/js/vendor")
+    .sourceMaps()
+    .babel(
+        [
+            "resources/js/vanilla/modules.js",
+            "resources/js/vanilla/materialize.min.js",
+            "resources/js/vanilla/functions/_*.js",
+            "resources/js/vanilla/components/_*.js"
+        ],
+        "public/js/vendor/vanilla.js"
+    )
+    .combine(
+        ["public/js/vendor/vue.js", "public/js/vendor/vanilla.js"],
+        "public/js/app.js"
+    )
+    .sass("resources/sass/app.scss", "public/css/app.css")
+    .browserSync({
+        proxy: "localhost:8000",
+        files: ["resources/sass/**/*", "resources/js/**/*"]
+    })
+    .options({
+        processCssUrls: false
+    });
