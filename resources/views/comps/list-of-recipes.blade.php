eb8dbd94c852c820a3411a792f15e22c5ea59c23
SerhiiCho <serhiicho@inbox.ru>
2018-09-08 19:10:35 +0300

    Refactored llist-of-recipes view, replaced methods for getting status

--- resources/views/comps/list-of-recipes.blade.php
+++ resources/views/comps/list-of-recipes.blade.php
@@ -2,7 +2,7 @@
 	@isset($recipes)
 		@forelse ($recipes as $recipe)
 			<ul>
-				<li style="border-left-color:#{{ $recipe->isDone() ? '65b56e' : 'ce7777' }};" class="col s12 m6 l4 row">
+				<li style="border-left-color:{{ $recipe->getStatusColor() }};" class="col s12 m6 l4 row">
 					<a href="/recipes/{{ $recipe->id }}" style="width:11em">
 						<img src="{{ asset('storage/images/small/'.$recipe->image) }}" alt="{{ $recipe->getTitle() }}" />
 					</a>
@@ -13,8 +13,8 @@
 					</div>
 
 					<div class="mt-3" style="width:35px">
-						<span class="new badge tooltipped {{ $recipe->isDone() ? 'green' : 'red' }}" style="min-width:auto; height:28px; padding:3px;" data-tooltip="@lang('users.status'): {{ $recipe->getStatus() }}">
-						<i class="material-icons" style="font-size:23px">{{ $recipe->getStatus('icon') }}</i>
+						<span class="new badge tooltipped" style="min-width:auto; height:28px; padding:3px; background-color:{{ $recipe->getStatusColor() }}" data-tooltip="@lang('users.status'): {{ $recipe->getStatusText() }}">
+							<i class="material-icons" style="font-size:23px">{{ $recipe->getStatusIcon() }}</i>
 						</span>
 					</div>
 				</li>
