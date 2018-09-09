b2297981d7016530ad8d1214911b3535cc9b9816
SerhiiCho <serhiicho@inbox.ru>
2018-09-09 19:51:36 +0300

    Added eye svg icon to includes/icons folder Changed other icon files, added translatable messages (tips) for icons

--- resources/views/includes/icons/heart.blade.php
+++ resources/views/includes/icons/heart.blade.php
@@ -1,3 +1,5 @@
-<svg viewBox="0 0 20 20" height="30" width="30">
-	<path d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z"></path>
-</svg>
+<span class="tooltipped" data-tooltip="@lang('tips.likes_tip', ['value' => $likes])" data-position="top">
+	<svg viewBox="0 0 20 20" height="30" width="30">
+		<path d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z"></path>
+	</svg>
+</span>
