d5e61cfd09d5b995361e15237ab7072baba4315e
SerhiiCho <serhiicho@inbox.ru>
2018-09-12 16:04:39 +0300

    Added DocumentPolicy but havent filled it out yet

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,12 +8,14 @@ class AuthServiceProvider extends ServiceProvider
 {
     // The policy mappings for the application
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Models\Document' => 'App\Policies\DocumentPolicy',
     ];
 
     // register any authentication / authorization services
     public function boot()
     {
         $this->registerPolicies();
+
+        \Gate::resource('documents', 'App\Policies\DocumentPolicy');
     }
 }
