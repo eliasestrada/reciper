9f680da425484af4e0b817a56e4ff5b8e8248bbe
SerhiiCho <serhiicho@inbox.ru>
2018-09-16 16:56:24 +0300

    Deleted ReportsController and moved stuff to FeedbackController

--- app/Http/Controllers/Admin/FeedbackController.php
+++ app/Http/Controllers/Admin/FeedbackController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ContactRequest;
 use App\Models\Feedback;
 use App\Models\User;
 
 class FeedbackController extends Controller
 {
     /**
+     * Show all reports and feedback
      * Mark user as he saw these messages
      *
      * @return \Illuminate\Http\Response
@@ -22,12 +24,45 @@ class FeedbackController extends Controller
         ]);
 
         return view('admin.feedback.index', [
-            'feedback' => Feedback::paginate(40)->onEachSide(1),
+            'feedback_ru' => Feedback::whereLang('ru')->paginate(20)->onEachSide(1),
+            'feedback_en' => Feedback::whereLang('en')->paginate(20)->onEachSide(1),
         ]);
     }
 
     /**
-     * Remove the specified resource from storage
+     * Store a newly created report in storage.
+     *
+     * @param ContactRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ContactRequest $request)
+    {
+        cache()->forget('all_feedback');
+
+        Feedback::create([
+            'is_report' => is_null($request->recipe) ? 0 : 1,
+            'lang' => lang(),
+            'visitor_id' => visitor_id(),
+            'recipe_id' => $request->recipe,
+            'message' => $request->message,
+        ]);
+
+        return back()->withSuccess(trans('feedback.success_message'));
+    }
+
+    /**
+     * Display single message
+     *
+     * @param  Feedback  $feedback
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Feedback $feedback)
+    {
+        return view('admin.feedback.show', compact('feedback'));
+    }
+
+    /**
+     * Remove message from storage
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
