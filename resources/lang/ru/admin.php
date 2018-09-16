0b95961f6694b69c97279d89c6f6aff1f5f3bcdf
SerhiiCho <serhiicho@inbox.ru>
2018-09-16 21:59:50 +0300

    Messages in lang files

--- resources/lang/ru/admin.php
+++ resources/lang/ru/admin.php
@@ -3,7 +3,6 @@
 return [
 
     // Controller
-    'only_admin_can_delete' => 'Только админ может удалять эти сообщения!',
     'feedback_has_been_deleted' => 'Отзыв успешно удален',
 
     // Views
