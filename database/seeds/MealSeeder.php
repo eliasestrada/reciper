e34c9fb3db1258cff766203bc4393da92cdb62b9
SerhiiCho <serhiicho@inbox.ru>
2018-09-20 21:03:46 +0300

    Cleaned 2 seeders

--- database/seeds/MealSeeder.php
+++ database/seeds/MealSeeder.php
@@ -5,16 +5,16 @@ use Illuminate\Database\Seeder;
 
 class MealSeeder extends Seeder
 {
-	/**
+    /**
      * Run the database seeds
      * @return void
      */
     public function run()
     {
         Meal::insert([
-			[ 'name_en' => 'breakfast', 'name_ru' => 'завтрак' ],
-			[ 'name_en' => 'lunch', 'name_ru' => 'обед' ],
-			[ 'name_en' => 'dinner', 'name_ru' => 'ужин' ],
-		]);
+            ['name_en' => 'breakfast', 'name_ru' => 'завтрак'],
+            ['name_en' => 'lunch', 'name_ru' => 'обед'],
+            ['name_en' => 'dinner', 'name_ru' => 'ужин'],
+        ]);
     }
 }
