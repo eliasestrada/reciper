cacc464f8e528b63bab5df1c0bad11338ff8290f
SerhiiCho <serhiicho@inbox.ru>
2018-09-20 19:06:54 +0300

    Added Help model and test to it

---
+++ app/Models/Help.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Help extends Model
+{
+    protected $table = 'help';
+    protected $guarded = ['id'];
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->toArray()['title_' . lang()];
+    }
+
+    /**
+     * @return string
+     */
+    public function getText(): string
+    {
+        return $this->toArray()['text_' . lang()];
+    }
+}
