31d3947621bad184b1b33e05fed4743d4f3ce33e
SerhiiCho <serhiicho@inbox.ru>
2018-09-20 19:12:16 +0300

    Added show.blade and test to it

---
+++ resources/views/help/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', trans('help.help'))
+
+@section('content')
+
+<div class="page">
+    <p><b>{{ $help->getTitle() }}</b></p>
+    <div class="divider"></div>
+    <p>{{ $help->getText() }}</p>
+</div>
+
+@endsection
---
+++ tests/Feature/Views/Help/HekpShowPageTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature\Views\Help;
+
+use App\Models\Help;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class HelpShowPageTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public $help;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->help = create(Help::class);
+    }
+
+    /** @test */
+    public function page_accessible_and_has_data(): void
+    {
+        $this->get("/help/{$this->help->id}")
+            ->assertOk()
+            ->assertViewIs('help.show')
+            ->assertViewHas('help', Help::find($this->help->id));
+    }
+}
