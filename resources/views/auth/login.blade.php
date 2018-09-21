59f66b2e2b6deeb8b4801ca589a5314671167ce9
SerhiiCho <serhiicho@inbox.ru>
2018-09-21 22:56:48 +0300

    Changed login and register blade pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,41 +4,44 @@
 
 @section('content')
 
-<div class="page container">
-    <form method="POST" action="{{ route('login') }}" class="form">
+<div class="page row">
+    <div class="center"><h3 class="headline">@lang('form.login')</h3></div>
 
-        @csrf <div class="center"><h3 class="headline">@lang('form.login')</h3></div>
-    
-        <div class="input-field">
-            <input id="email" value="{{ old('email') }}" type="email" name="email" class="validate">
-            <label for="email">@lang('form.email')</label>
-        </div>
-    
-        <visibility name-attr="password" id-attr="password" class-attr="pwd">
-            <label for="password" slot="content">@lang('form.pwd')</label>
-            @include('includes.preloader2')
-        </visibility>
+    <div class="col s12 m8 offset-m2 l6 offset-l3">
+        <form method="POST" action="{{ route('login') }}" class="form">
+            @csrf
+            <div class="input-field">
+                <input id="email" value="{{ old('email') }}" type="email" name="email" class="validate">
+                <label for="email">@lang('form.email')</label>
+            </div>
+        
+            <visibility name-attr="password" id-attr="password" class-attr="pwd">
+                <label for="password" slot="content">@lang('form.pwd')</label>
+                @include('includes.preloader2')
+            </visibility>
 
-        <div class="mt-3">
-            <label>
-                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }} />
-                <span>
-                    @lang('form.remember_me') 
-                    @include('includes.tip', ['tip' => trans('form.remember_info')])
-                </span>
-            </label>
-        </div>
-    
-        <div class="input-field">
-            <button type="submit" id="go-to-account" class="waves-effect waves-light btn">
-                <i class="material-icons right">exit_to_app</i>
-                @lang('form.login')
-            </button>
-        </div>
+            <div class="mt-3">
+                <label>
+                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }} />
+                    <span>
+                        @lang('form.remember_me') 
+                        @include('includes.tip', ['tip' => trans('form.remember_info')])
+                    </span>
+                </label>
+            </div>
+        
+            <div class="input-field">
+                <button type="submit" id="go-to-account" class="waves-effect waves-light btn">
+                    <i class="material-icons right">exit_to_app</i>
+                    @lang('form.login')
+                </button>
+            </div>
 
-        {{-- <a href="{{ route('password.request') }}">@lang('form.forgot_pwd')</a> --}}
-    </form>
-    <div class="pt-4">
+            {{-- <a href="{{ route('password.request') }}">@lang('form.forgot_pwd')</a> --}}
+        </form>
+    </div>
+
+    <div class="col s12 m8 offset-m2 pt-4">
         <p>
             @lang('messages.dont_have_account'):
             <a href="/register">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,67 +4,69 @@
 
 @section('content')
 
-<div class="page container">
-    <form method="POST" action="{{ route('register') }}" class="form">
+<div class="page row">
+    <div class="center"><h2 class="headline">@lang('form.register')</h2></div>
 
-        @csrf <div class="center"><h2 class="headline">@lang('form.register')</h2></div>
-    
-        <div class="input-field">
-            <input type="text" id="name" name="name" value="{{ old('name') }}" class="validate" required>
-            <label for="name">
-                @lang('form.name') 
-                @include('includes.tip', ['tip' => trans('form.name_desc')])
-            </label>
-        </div>
-
-        <div class="input-field">
-            <input type="email" id="email" name="email" value="{{ old('email') }}" class="validate" required>
-            <label for="email">
-                @lang('form.email')
-                @include('includes.tip', ['tip' => trans('form.email_desc')])
-            </label>
-        </div>
-
-        <visibility name-attr="password" id-attr="password">
-            <label for="password" slot="content">
-                @lang('form.pwd')
-                @include('includes.tip', ['tip' => trans('form.pwd_desc')])
-            </label>
-            @include('includes.preloader2')
-        </visibility>
-
-        <visibility name-attr="password_confirmation" id-attr="password_confirmation">
-            <label for="password_confirmation" slot="content">
-                @lang('form.pwd_confirm')
-                @include('includes.tip', ['tip' => trans('form.pwd2_desc')])
-            </label>
-            @include('includes.preloader2')
-        </visibility>
-
-        @if(!empty($document))
-            <blockquote class="mt-5">
-                {!! trans('form.agree_to_terms', ['btn' => trans('form.register')]) !!}
-            </blockquote>
-
-            <!-- show-document-modal structure -->
-            <div id="show-document-modal" class="modal">
-                <div class="modal-content reset">
-                    <h4>{{ $document->getTitle() }}</h4>
-                    {!! $document->text !!}
-                </div>
-                <div class="modal-footer">
-                    <a href="#!" class="modal-close waves-effect waves-green btn-flat left">
-                        @lang('messages.agree')
-                    </a>
-                </div>
+    <div class="col s12 m8 offset-m2 l6 offset-l3">
+        <form method="POST" action="{{ route('register') }}" class="form">
+            @csrf
+            <div class="input-field">
+                <input type="text" id="name" name="name" value="{{ old('name') }}" class="validate" required>
+                <label for="name">
+                    @lang('settings.reciper_name') 
+                    @include('includes.tip', ['tip' => trans('form.name_desc')])
+                </label>
             </div>
-        @endif
 
-        <button type="submit" id="register-btn" class="waves-effect waves-light btn mt-3">
-            <i class="material-icons left">assignment_turned_in</i>
-            @lang('form.register')
-        </button>
-    </form>
+            <div class="input-field">
+                <input type="email" id="email" name="email" value="{{ old('email') }}" class="validate" required>
+                <label for="email">
+                    @lang('form.email')
+                    @include('includes.tip', ['tip' => trans('form.email_desc')])
+                </label>
+            </div>
+
+            <visibility name-attr="password" id-attr="password">
+                <label for="password" slot="content">
+                    @lang('form.pwd')
+                    @include('includes.tip', ['tip' => trans('form.pwd_desc')])
+                </label>
+                @include('includes.preloader2')
+            </visibility>
+
+            <visibility name-attr="password_confirmation" id-attr="password_confirmation">
+                <label for="password_confirmation" slot="content">
+                    @lang('form.pwd_confirm')
+                    @include('includes.tip', ['tip' => trans('form.pwd2_desc')])
+                </label>
+                @include('includes.preloader2')
+            </visibility>
+
+            @if(!empty($document))
+                <blockquote class="mt-5">
+                    {!! trans('form.agree_to_terms', ['btn' => trans('form.register')]) !!}
+                </blockquote>
+
+                <!-- show-document-modal structure -->
+                <div id="show-document-modal" class="modal">
+                    <div class="modal-content reset">
+                        <h4>{{ $document->getTitle() }}</h4>
+                        {!! $document->text !!}
+                    </div>
+                    <div class="modal-footer">
+                        <a href="#!" class="modal-close waves-effect waves-green btn-flat left">
+                            @lang('messages.agree')
+                        </a>
+                    </div>
+                </div>
+            @endif
+
+            <button type="submit" id="register-btn" class="waves-effect waves-light btn mt-3">
+                <i class="material-icons left">assignment_turned_in</i>
+                @lang('form.register')
+            </button>
+        </form>
+    </div>
 </div>
 
 @endsection
