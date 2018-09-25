c1a22c166620db771b9ff036a8467fbae8862419
SerhiiCho <serhiicho@inbox.ru>
2018-09-26 02:03:15 +0300

    Fixed seach view page

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -13,7 +13,7 @@
     <div class="container">
         <form action="{{ action('PagesController@search') }}" method="get" id="search-form">
             <div class="input-field">
-                <button type="submit" class="prefix btn-floating"><i class="material-icons">search</i></button>
+                <button type="submit" class="prefix btn-floating"><i class="fas fa-search"></i></button>
                 <input type="text" name="for" id="autocomplete-input" class="autocomplete" style="margin-left:4em" autocomplete="off">
                 <label for="autocomplete-input" style="margin-left:4em">@lang('pages.search_details')</label>
             </div>
@@ -31,18 +31,19 @@
                         </a>
                     </div>
                     <div class="card-content min-h">
-                        <span class="card-title activator">
+                        <span style="height:75%" class="card-title activator">
                             {{ $recipe->getTitle() }}
                         </span>
+                        <div style="height:25%">
+                            <i class="fas fa-ellipsis-h fa-15x right red-text activator"></i>
+                        </div>
                     </div>
                     <div class="card-reveal">
-                        <span class="card-title ">
-                            {{ $recipe->getTitle() }}
-                            <i class="material-icons right">close</i>
-                        </span>
-                        <p>
-                            <a href="/recipes/{{ $recipe->id }}" title="{{ $recipe->getTitle() }}">@lang('recipes.go')</a>
-                        </p>
+                        <span class="card-title">{{ $recipe->getTitle() }}</span>
+                        <div><i class="fas fa-times right red-text card-title"></i></div>
+                        <a class="btn-small mt-3" href="/recipes/{{ $recipe->id }}">
+                            @lang('recipes.go')
+                        </a>
                         <p>{{ $recipe->getIntro() }}</p>
                     </div>
                 </div>
