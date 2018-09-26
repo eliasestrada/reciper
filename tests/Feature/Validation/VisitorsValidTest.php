4f6d9ce5bc16767916890fe1c32825f9fb4888f8
SerhiiCho <serhiicho@inbox.ru>
2018-09-27 01:57:43 +0300

    Cleaned VisitorsValidTest

--- tests/Feature/Validation/VisitorsValidTest.php
+++ tests/Feature/Validation/VisitorsValidTest.php
@@ -27,50 +27,45 @@ class VisitorsValidTest extends TestCase
     /** @test */
     public function ban_days_field_is_required(): void
     {
-        $data = ['days' => '', 'message' => str_random(40)];
-
-        $this->request
-            ->put(action('Master\VisitorsController@update', ['id' => $this->visitor->id]), $data)
-            ->assertSeeText(trans('visitors.days_required'));
+        $this->request->put(action('Master\VisitorsController@update', ['id' => $this->visitor->id]), [
+            'days' => '',
+            'message' => str_random(40),
+        ])->assertSeeText(trans('visitors.days_required'));
     }
 
     /** @test */
     public function ban_message_fiels_is_required(): void
     {
-        $data = ['days' => 2, 'message' => ''];
-
-        $this->request
-            ->put(action('Master\VisitorsController@update', ['id' => $this->visitor->id]), $data)
-            ->assertSeeText(trans('visitors.message_required'));
+        $this->request->put(action('Master\VisitorsController@update', ['id' => $this->visitor->id]), [
+            'days' => 2,
+            'message' => '',
+        ])->assertSeeText(trans('visitors.message_required'));
     }
 
     /** @test */
     public function ban_days_field_must_be_numeric(): void
     {
-        $data = ['days' => 'gg', 'message' => str_random(40)];
-
-        $this->request
-            ->put(action('Master\VisitorsController@update', ['id' => $this->visitor->id]), $data)
-            ->assertSeeText(trans('visitors.days_numeric'));
+        $this->request->put(action('Master\VisitorsController@update', ['id' => $this->visitor->id]), [
+            'days' => 'gg',
+            'message' => str_random(40),
+        ])->assertSeeText(trans('visitors.days_numeric'));
     }
 
     /** @test */
     public function ban_message_must_be_not_short(): void
     {
-        $data = ['days' => 5, 'message' => str_random($this->msg_min - 1)];
-
-        $this->request
-            ->put(action('Master\VisitorsController@update', ['id' => $this->visitor->id]), $data)
-            ->assertSeeText(preg_replace('/:min/', $this->msg_min, trans('visitors.message_min')));
+        $this->request->put(action('Master\VisitorsController@update', ['id' => $this->visitor->id]), [
+            'days' => 5,
+            'message' => str_random($this->msg_min - 1),
+        ])->assertSeeText(preg_replace('/:min/', $this->msg_min, trans('visitors.message_min')));
     }
 
     /** @test */
     public function ban_message_must_be_not_long(): void
     {
-        $data = ['days' => 5, 'message' => str_random($this->msg_max + 1)];
-
-        $this->request
-            ->put(action('Master\VisitorsController@update', ['id' => $this->visitor->id]), $data)
-            ->assertSeeText(preg_replace('/:max/', $this->msg_max, trans('visitors.message_max')));
+        $this->request->put(action('Master\VisitorsController@update', ['id' => $this->visitor->id]), [
+            'days' => 5,
+            'message' => str_random($this->msg_max + 1),
+        ])->assertSeeText(preg_replace('/:max/', $this->msg_max, trans('visitors.message_max')));
     }
 }
