58bab33e8478f33fd2d11c02cff40476def7f6bd
SerhiiCho <serhiicho@inbox.ru>
2018-09-27 19:48:21 +0300

    Added GeneralRequestTest

--- tests/Feature/Requests/Settings/GeneralRequestTest.php
+++ tests/Feature/Requests/Settings/GeneralRequestTest.php
@@ -9,13 +9,40 @@ class GeneralRequestTest extends TestCase
 {
     use DatabaseTransactions;
 
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    private $name_min;
+    private $name_max;
+    private $about_me_max;
+    private $request;
+
+    public function setUp()
     {
-        $this->assertTrue(true);
+        parent::setUp();
+        $this->name_min = config('validation.settings.name.min');
+        $this->name_max = config('validation.settings.name.max');
+        $this->about_me_max = config('validation.settings.about_me.max');
+        $this->request = $this->actingAs(create_user())->followingRedirects();
+    }
+
+    /** @test */
+    public function name_is_required(): void
+    {
+        $this->request->put(action('Settings\GeneralController@update'), ['name' => ''])
+            ->assertSeeText(trans('settings.name_required'));
+    }
+
+    /** @test */
+    public function name_must_be_not_short(): void
+    {
+        $this->request->put(action('Settings\GeneralController@update'), [
+            'name' => str_random($this->name_min - 1),
+        ])->assertSeeText(preg_replace('/:min/', $this->name_min, trans('settings.name_min')));
+    }
+
+    /** @test */
+    public function name_must_be_not_long(): void
+    {
+        $this->request->put(action('Settings\GeneralController@update'), [
+            'name' => str_random($this->name_max + 1),
+        ])->assertSeeText(preg_replace('/:max/', $this->name_max, trans('settings.name_max')));
     }
 }
