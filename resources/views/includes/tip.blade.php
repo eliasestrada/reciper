055cca60317cbea1cb697b0f2f30e5fdb6153025
SerhiiCho <serhiicho@inbox.ru>
2018-09-30 13:18:25 +0300

    Changed tips on register page and added tip function that returns nice looking text

--- resources/views/includes/tip.blade.php
+++ resources/views/includes/tip.blade.php
@@ -1,5 +1,5 @@
 @isset($tip)
     <a class="tooltipped" style="cursor:pointer" data-tooltip="{{ $tip }}" data-position="top">
-        <i class="fas fa-question-circle red-text"></i>
+        <i class="fas fa-question-circle hoverable z-depth-1 red-text" style="border-radius:50%"></i>
     </a>
 @endisset
