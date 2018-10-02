62687bf393b5ce0061a5dcc178cc022d2cd55b06
SerhiiCho <serhiicho@inbox.ru>
2018-10-02 20:20:20 +0300

    Added favs table with model and test to model

---
+++ database/migrations/2018_10_02_185330_create_favs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favs', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->unsignedInteger('recipe_id');
+            $table->foreign('recipe_id')->references('id')->on('recipes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favs');
+    }
+}
