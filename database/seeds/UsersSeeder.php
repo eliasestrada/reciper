a788dd8de3c0dbe8fff74fc10f780f8f85ec43d8
SerhiiCho <serhiicho@inbox.ru>
2018-10-09 00:08:32 +0300

    Changed seeder

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -16,14 +16,15 @@ class UsersSeeder extends Seeder
             'visitor_id' => factory(Visitor::class)->create(['ip' => '127.0.0.1'])->id,
             'email' => '11@11.com',
             'name' => 'Серый 😎',
-            'xp' => 57,
+            'xp' => 577,
             'popularity' => 7,
             'about_me' => 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.',
+            'streak_days' => 253,
             'password' => bcrypt('111111'),
             'notif_check' => date("Y-m-d H:i:s"),
             'contact_check' => date("Y-m-d H:i:s"),
             'created_at' => '2018-03-08 12:08:59',
-            'image' => '1539031832-user1.jpg',
+            'image' => '2018/10/1539031832-user1.jpg',
         ]);
         $user->addRole('admin');
         $user->addRole('master');
@@ -32,13 +33,14 @@ class UsersSeeder extends Seeder
             'visitor_id' => factory(Visitor::class)->create(['ip' => '127.0.0.2'])->id,
             'email' => '22@22.com',
             'name' => 'Кристина 👑',
-            'xp' => 27,
+            'xp' => 177,
             'popularity' => 5,
             'about_me' => 'Люблю Никитку',
+            'streak_days' => 24,
             'password' => bcrypt('111111'),
             'contact_check' => date("Y-m-d H:i:s"),
             'created_at' => '2018-03-08 12:08:59',
-            'image' => '1539024731-user2.jpg',
+            'image' => '2018/10/1539024731-user2.jpg',
         ]);
     }
 }
