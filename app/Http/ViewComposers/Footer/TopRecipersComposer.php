5e03f9e337dd5d543bda410bcd09ccb3e8bdcf31
SerhiiCho <serhiicho@inbox.ru>
2018-10-10 15:29:32 +0300

    TopResipersComposer now returns data from cache

--- app/Http/ViewComposers/Footer/TopRecipersComposer.php
+++ app/Http/ViewComposers/Footer/TopRecipersComposer.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\ViewComposers\Footer;
 
-use App\Models\Like;
-use App\Models\User;
 use Illuminate\View\View;
 
 class TopRecipersComposer
@@ -15,26 +13,6 @@ class TopRecipersComposer
      */
     public function compose(View $view): void
     {
-        $top_recipers = cache()->remember('top_recipers', config('cache.timing.top_recipers'), function () {
-            return Like::whereDay('created_at', date('j'))->get();
-        });
-
-        $users = $top_recipers->map(function ($like) {
-            return $like->recipe->user->id . '<split>' . $like->recipe->user->name;
-        })->toArray();
-
-        $users = array_slice(array_reverse(array_sort(array_count_values($users))), 0, 10);
-
-        $top_recipers = [];
-
-        foreach ($users as $name => $value) {
-            $explode = explode('<split>', $name);
-            array_push($top_recipers, [
-                'id' => $explode[0],
-                'name' => $explode[1],
-            ]);
-        }
-
-        $view->with(compact('top_recipers'));
+        $view->with('top_recipers', cache()->get('top_recipers', []));
     }
 }
