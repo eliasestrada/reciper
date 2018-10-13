2bb4b0c570c66eb1c98d8a8430bb3cac0afb95a8
SerhiiCho <serhiicho@inbox.ru>
2018-10-13 19:59:44 +0300

    fixed gitignore

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,28 +1,36 @@
 let mix = require("laravel-mix");
 
-// mix.js("resources/js/vue/vue.js", "public/js/vendor")
-//     .sourceMaps()
-//     .babel(
-//         [
-//             "resources/js/vanilla/modules.js",
-//             "resources/js/vanilla/materialize.js",
-//             "resources/js/vanilla/functions/_*.js",
-//             "resources/js/vanilla/components/_*.js"
-//         ],
-//         "public/js/vendor/vanilla.js"
-//     )
-//     .combine(
-//         ["public/js/vendor/vue.js", "public/js/vendor/vanilla.js"],
-//         "public/js/app.js"
-//     );
+let css = 1;
+let js = 0;
+let server = 0;
 
-mix.sass("resources/sass/app.scss", "public/css/app.css")
-    .disableNotifications()
-    .options({
+if (css == 1) {
+    mix.sass("resources/sass/app.scss", "public/css/app.css").options({
         processCssUrls: false
-    })
-    .browserSync({
+    });
+}
+
+if (js == 1) {
+    mix.js("resources/js/vue/vue.js", "public/js/vendor")
+        .babel(
+            [
+                "resources/js/vanilla/modules.js",
+                "resources/js/vanilla/materialize.js",
+                "resources/js/vanilla/functions/_*.js",
+                "resources/js/vanilla/components/_*.js"
+            ],
+            "public/js/vendor/vanilla.js"
+        )
+        .combine(
+            ["public/js/vendor/vue.js", "public/js/vendor/vanilla.js"],
+            "public/js/app.js"
+        );
+}
+
+if (server == 1) {
+    mix.browserSync({
         proxy: "localhost:8000",
-        files: ["resources/sass/**/*"],
+        files: ["public/css/*.css", "public/js/*.js"],
         notify: false
     });
+}
