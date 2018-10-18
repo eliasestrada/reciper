d84a6dc11a27df63b187a98303ba5d7f890f81e0
SerhiiCho <serhiicho@inbox.ru>
2018-10-19 00:25:43 +0300

    Refactored StatisticsController I divided 1 giant method to parts to make it more testible

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Recipe;
 use App\Models\User;
+use Illuminate\Support\Collection;
 
 class StatisticsController extends Controller
 {
@@ -76,10 +77,18 @@ class StatisticsController extends Controller
             throw new \Exception('getDataFromUserScript 1 parameter can only have one of three values. Given value does not match any of them');
         }
 
-        $user = $user ? $user : user();
-        $recipes = $user->recipes()->where('created_at', '>=', now()->subYear())->get();
+        $rules = $this->makeArrayOfRules();
+        $rules_filled = $this->populateWithSumOfLikes($rules, $column, ($user ? $user : user()));
 
-        $rules = array_map(function ($i) {
+        return collect($this->convertMonthNumberToName($rules_filled));
+    }
+
+    /**
+     * @return array
+     */
+    public function makeArrayOfRules(): array
+    {
+        return array_map(function ($i) {
             return [
                 'month' => now()->subMonths($i - 1)->month,
                 'from' => now()->subMonths($i),
@@ -87,6 +96,17 @@ class StatisticsController extends Controller
                 'sum' => 0,
             ];
         }, [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
+    }
+
+    /**
+     * @param array $rules
+     * @param string $column
+     * @param User $user
+     * @return array
+     */
+    public function populateWithSumOfLikes(array $rules, string $column, User $user): array
+    {
+        $recipes = $user->recipes()->where('created_at', '>=', now()->subYear())->get();
 
         foreach ($rules as $key => $rule) {
             $rules[$key]['sum'] += $recipes->map(function ($recipe) use ($rule, $column) {
@@ -96,13 +116,19 @@ class StatisticsController extends Controller
                     ->count();
             })->sum();
         }
+        return $rules;
+    }
 
-        // Convert month number to month name
-        $rules_with_month_name = collect($rules)->map(function ($rule) {
+    /**
+     * Convert month number to month name
+     * @param array $rules
+     * @return Collection
+     */
+    public function convertMonthNumberToName(array $rules): Collection
+    {
+        return collect($rules)->map(function ($rule) {
             $rule['month'] = trans("date.month_{$rule['month']}");
             return $rule;
         });
-
-        return collect($rules_with_month_name);
     }
 }
