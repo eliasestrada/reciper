c2704548bf65423c4d1e479ade4882f2b2d9f9a7
SerhiiCho <serhiicho@inbox.ru>
2018-10-18 14:30:33 +0300

    Added tests to NotificationIndeX page

--- tests/Feature/Views/Notifications/NotificationsIndexPageTest.php
+++ tests/Feature/Views/Notifications/NotificationsIndexPageTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Feature\Views\Notifications;
 
+use App\Models\Recipe;
 use App\Models\User;
+use App\Notifications\RecipeApprovedNotification;
+use App\Notifications\ScriptAttackNotification;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
@@ -11,14 +14,37 @@ class NotificationsIndexPageTest extends TestCase
     use DatabaseTransactions;
 
     /** @test */
-    public function user_can_see_the_page(): void
+    public function userCanSee_ThePage(): void
     {
         $this->actingAs(make(User::class))->get('/notifications')->assertViewIs('notifications.index')->assertOk();
     }
 
     /** @test */
-    public function guest_cant_see_the_page(): void
+    public function guestCantSee_ThePage(): void
     {
         $this->get('/notifications')->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function userCanRecive_RecipeApprovedNotification(): void
+    {
+        $user = create_user();
+        $recipe = make(Recipe::class, ['user_id' => $user->id]);
+        $user->notify(new RecipeApprovedNotification($recipe));
+
+        $this->actingAs($user)
+            ->get('/notifications')
+            ->assertSeeText(trans('approves.recipe_published'));
+    }
+
+    /** @test */
+    public function firstUserCanRecive_ScriptAttackNotification(): void
+    {
+        $first = User::first();
+        $first->notify(new ScriptAttackNotification(create_user()->username));
+
+        $this->actingAs($first)
+            ->get('/notifications')
+            ->assertSeeText(trans('notifications.title_script_attack'));
     }
 }
