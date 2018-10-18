ae434e51ca07f291978e72205515275db2c0d2e8
SerhiiCho <serhiicho@inbox.ru>
2018-10-18 23:52:33 +0300

    Moved Scripts folder in Feature to Unit tests

--- tests/Unit/Scripts/StatisticsPopularityChartScriptTest.php
+++ tests/Unit/Scripts/StatisticsPopularityChartScriptTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Scrips;
+namespace Tests\Unit\Scripts;
 
 use App\Http\Controllers\StatisticsController;
 use App\Models\Recipe;
--- tests/Unit/Scripts/TopRecipersJobScriptTest.php
+++ tests/Unit/Scripts/TopRecipersJobScriptTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Scrips;
+namespace Tests\Unit\Scripts;
 
 use App\Jobs\TopRecipersJob;
 use Carbon\Carbon;
