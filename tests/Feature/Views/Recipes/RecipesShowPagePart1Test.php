6bfe8a5556bab4391ad6705fb14e55c0472c1cb1
Serhii Cho <1990serzhil@gmail.com>
2018-10-20 16:11:32 +0300

    Divided test to 2 parts

--- tests/Feature/Views/Recipes/RecipesShowPagePart1Test.php
+++ tests/Feature/Views/Recipes/RecipesShowPagePart1Test.php
@@ -2,15 +2,13 @@
 
 namespace Tests\Feature\Views\Recipes;
 
-use App\Models\Fav;
 use App\Models\Feedback;
 use App\Models\Recipe;
 use App\Models\User;
-use App\Models\Visitor;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
-class RecipesShowPageTest extends TestCase
+class RecipesShowPagePart1Test extends TestCase
 {
     use DatabaseTransactions;
 
@@ -197,71 +195,5 @@ class RecipesShowPageTest extends TestCase
             ->post(action('Admin\FeedbackController@store'), $data2)
             ->assertSee(lang('feedback.success_message'));
         $this->assertDatabaseHas('feedback', $data2);
-    }
-
-    /** @test */
-    public function owner_of_the_recipe_sees_report_button_disabled(): void
-    {
-        $user = create_user();
-        $recipe = create(Recipe::class, ['user_id' => $user->id]);
-
-        $this->actingAs($user)
-            ->get("/recipes/$recipe->id")
-            ->assertSee('<a href="#report-recipe-modal" class="btn waves-effect waves-light modal-trigger min-w" disabled>');
-    }
-
-    /** @test */
-    public function auth_user_can_add_recipe_to_favs(): void
-    {
-        $user = create_user();
-        $recipe = create(Recipe::class);
-
-        $this->actingAs($user)
-            ->post(action('FavsController@store', ['id' => $recipe->id]))
-            ->assertOk()
-            ->assertSeeText('active');
-
-        $this->assertDatabaseHas('favs', [
-            'recipe_id' => $recipe->id,
-            'user_id' => $user->id,
-        ]);
-    }
-
-    /** @test */
-    public function auth_user_can_delete_recipe_from_favs(): void
-    {
-        $user = create_user();
-        $recipe = create(Recipe::class);
-        Fav::create(['user_id' => $user->id, 'recipe_id' => $recipe->id]);
-
-        $this->actingAs($user)
-            ->post(action('FavsController@store', ['id' => $recipe->id]))
-            ->assertOk()
-            ->assertDontSeeText('active');
-
-        $this->assertDatabaseMissing('favs', [
-            'recipe_id' => $recipe->id,
-            'user_id' => $user->id,
-        ]);
-    }
-
-    /** @test */
-    public function visitor_can_like_the_recipe(): void
-    {
-        $visitor = create(Visitor::class);
-        $recipe = create(Recipe::class);
-
-        $this->post("/api/like/like/$recipe->id")
-            ->assertExactJson(['liked' => 1]);
-    }
-
-    /** @test */
-    public function visitor_can_dislike_the_recipe(): void
-    {
-        $visitor = create(Visitor::class);
-        $recipe = create(Recipe::class);
-
-        $this->post("/api/like/dislike/$recipe->id")
-            ->assertExactJson(['liked' => 0]);
     }
 }
--- tests/Feature/Views/Statistics/StatisticsIndexPageTest.php
+++ tests/Feature/Views/Statistics/StatisticsIndexPageTest.php
@@ -2,33 +2,18 @@
 
 namespace Tests\Feature\Views\Statistics;
 
-use App\Models\Recipe;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Models\User;
 use Tests\TestCase;
 
 class StatisticsPageTest extends TestCase
 {
-    use DatabaseTransactions;
-
     /** @test */
     public function view_has_data(): void
     {
-        $user = create_user();
-
-        $recipes = Recipe::whereUserId($user->id)
-            ->select('id', 'title_' . lang())
-            ->withCount('likes')
-            ->withCount('views')
-            ->get();
-
-        $most_viewed = $recipes->where('views_count', $recipes->max('views_count'))->first();
-        $most_liked = $recipes->where('likes_count', $recipes->max('likes_count'))->first();
-        $most_favs = $recipes->where('favs_count', $recipes->max('favs_count'))->first();
-
-        $this->actingAs($user)
+        $this->actingAs(make(User::class))
             ->get('/statistics')
             ->assertViewIs('statistics.index')
-            ->assertViewHasAll(compact('recipes', 'most_viewed', 'most_liked', 'most_favs'));
+            ->assertViewHasAll(['recipes', 'most_viewed', 'most_liked', 'most_favs']);
     }
 
     /** @test */
