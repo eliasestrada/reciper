3ec6b6037db4ba81efdda4da21e11c00fb9fce87
SerhiiCho <serhiicho@inbox.ru>
2018-10-22 16:55:08 +0300

    Cleaned HelpShowPageTest

--- tests/Feature/Views/Help/HelpShowPageTest.php
+++ tests/Feature/Views/Help/HelpShowPageTest.php
@@ -3,27 +3,16 @@
 namespace Tests\Feature\Views\Help;
 
 use App\Models\Help;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class HelpShowPageTest extends TestCase
 {
-    use DatabaseTransactions;
-
-    private $help;
-
-    public function setUp()
-    {
-        parent::setUp();
-        $this->help = create(Help::class);
-    }
-
     /** @test */
     public function page_accessible_and_has_data(): void
     {
-        $this->get("/help/{$this->help->id}")
+        $this->get('/help/1')
             ->assertOk()
             ->assertViewIs('help.show')
-            ->assertViewHas('help', Help::find($this->help->id));
+            ->assertViewHas('help');
     }
 }
