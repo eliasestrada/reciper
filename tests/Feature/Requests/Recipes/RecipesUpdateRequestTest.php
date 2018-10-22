a0a09c7673f90eb3e748e5e5839f56c8adb9cdeb
SerhiiCho <serhiicho@inbox.ru>
2018-10-22 13:55:23 +0300

    RecipesUpdateRequest joined from 2 to 1 files

--- tests/Feature/Requests/Recipes/RecipesUpdateRequestTest.php
+++ tests/Feature/Requests/Recipes/RecipesUpdateRequestTest.php
@@ -2,12 +2,14 @@
 
 namespace Tests\Feature\Requests\Recipes;
 
+use App\Models\Category;
+use App\Models\Meal;
 use App\Models\Recipe;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
-class RecipesUpdateRequestPart1Test extends TestCase
+class RecipesUpdateRequestTest extends TestCase
 {
     use DatabaseTransactions;
 
@@ -91,6 +93,68 @@ class RecipesUpdateRequestPart1Test extends TestCase
         $text_max = config('valid.recipes.text.max');
         $this->data['text'] = str_random($text_max + 1);
         $this->response()->assertSeeText($this->replace(':max', $text_max, trans('recipes.text_max')));
+    }
+
+    /** @test */
+    public function meal_must_be_numeric(): void
+    {
+        $this->data['meal'] = 'n';
+        $this->response()->assertSeeText(trans('recipes.meal_numeric'));
+    }
+
+    /** @test */
+    public function meal_must_be_between_numbers(): void
+    {
+        $meal_max = Meal::count();
+        $this->data['meal'] = $meal_max + 1;
+        $expect = $this->replace(':min', 1, trans('recipes.meal_between'));
+
+        $this->response()->assertSeeText($this->replace(':max', $meal_max, $expect));
+    }
+
+    /** @test */
+    public function categories_are_required(): void
+    {
+        $this->data['categories'] = '';
+        $this->response()->assertSeeText(trans('recipes.categories_required'));
+    }
+
+    /** @test */
+    public function categories_must_be_distinct(): void
+    {
+        $this->data['categories'] = [0 => 2, 1 => 2];
+        $this->response()->assertSeeText(trans('recipes.categories_distinct'));
+    }
+
+    /** @test */
+    public function categories_must_be_numeric(): void
+    {
+        $this->data['categories'] = [0 => 'string'];
+        $this->response()->assertSeeText(trans('recipes.categories_numeric'));
+    }
+
+    /** @test */
+    public function categories_must_not_have_id_of_one(): void
+    {
+        $this->data['categories'] = [0 => 1];
+        $this->response()->assertSeeText(trans('recipes.categories_between'));
+    }
+
+    /** @test */
+    public function categories_must_be_between_numbers(): void
+    {
+        $this->data['categories'] = [0 => 1, 1 => 2];
+        $this->response()->assertSeeText(trans('recipes.categories_between'));
+
+        $this->data['categories'] = [0 => 3, 1 => Category::count() + 1];
+        $this->response()->assertSeeText(trans('recipes.categories_between'));
+    }
+
+    /** @test */
+    public function categories_correct_data(): void
+    {
+        $this->data['categories'] = [0 => 2, 1 => Category::count()];
+        $this->response()->assertSeeText(trans('recipes.added_to_approving'));
     }
 
     /**
