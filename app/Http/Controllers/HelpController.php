1112892507e516ee57ab964a890ed3e1b598b137
SerhiiCho <serhiicho@inbox.ru>
2018-10-24 00:11:33 +0300

    Added try catch block to helpController to index method only

--- app/Http/Controllers/HelpController.php
+++ app/Http/Controllers/HelpController.php
@@ -4,20 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Help;
 use App\Models\HelpCategory;
+use Illuminate\Database\QueryException;
 
 class HelpController extends Controller
 {
     public function index()
     {
-        $help = cache()->remember('help', 10, function () {
-            return Help::selectBasic()->orderBy('title')->get()->toArray();
-        });
+        try {
+            $help = cache()->remember('help', 10, function () {
+                return Help::selectBasic()->orderBy('title')->get()->toArray();
+            });
 
-        $help_categories = cache()->remember('help_categories', 10, function () {
-            return HelpCategory::selectBasic()->get()->toArray();
-        });
+            $help_categories = cache()->remember('help_categories', 10, function () {
+                return HelpCategory::selectBasic()->get()->toArray();
+            });
 
-        return view('help.index', compact('help', 'help_categories'));
+            return view('help.index', compact('help', 'help_categories'));
+        } catch (QueryException $e) {
+            no_connection_error($e, __CLASS__);
+            return view('help.index', ['help' => [], 'help_categories' => []]);
+        }
     }
 
     /**
