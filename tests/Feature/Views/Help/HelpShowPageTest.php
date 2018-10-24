c2d8d4d2f1088348699b8106654fa36d70f405ef
SerhiiCho <serhiicho@inbox.ru>
2018-10-24 12:54:12 +0300

    In help/show page test deleted DatabaseTransaction trait

--- tests/Feature/Views/Help/HelpShowPageTest.php
+++ tests/Feature/Views/Help/HelpShowPageTest.php
@@ -3,17 +3,13 @@
 namespace Tests\Feature\Views\Help;
 
 use App\Models\Help;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class HelpShowPageTest extends TestCase
 {
-    use DatabaseTransactions;
-
     /** @test */
     public function page_accessible(): void
     {
-        $help_page_id = create(Help::class)->id;
-        $this->get("/help/$help_page_id")->assertOk()->assertViewIs('help.show');
+        $this->get('/help/1')->assertOk()->assertViewIs('help.show');
     }
 }
