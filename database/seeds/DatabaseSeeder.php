e87956876e5dc7ef0907e994da65d1b9948b9b04
SerhiiCho <serhiicho@inbox.ru>
2018-10-27 16:16:45 +0300

    Changed DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,17 +11,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(RolesSeeder::class);
-
-        if (app()->env !== 'testing') {
-            $this->call(HelpCategoriesSeeder::class);
-            $this->call(HelpSeeder::class);
-        }
-
         $this->call(CategoriesSeeder::class);
         $this->call(VisitorSeeder::class);
         $this->call(RecipeSeeder::class);
         $this->call(UsersSeeder::class);
         $this->call(DocumentsSeeder::class);
         $this->call(MealSeeder::class);
+
+        if (app()->env !== 'testing') {
+            $this->call(HelpCategoriesSeeder::class);
+            $this->call(HelpSeeder::class);
+        }
     }
 }
