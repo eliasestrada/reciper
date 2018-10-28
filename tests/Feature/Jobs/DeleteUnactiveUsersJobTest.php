a3a02f81b153fdf93b1f13705fede8736d7b9069
SerhiiCho <serhiicho@inbox.ru>
2018-10-28 10:04:36 +0200

    Added test for DeleteUnactiveUsers

---
+++ tests/Feature/Jobs/DeleteUnactiveUsersJobTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Tests\Feature\Jobs;
+
+use App\Jobs\DeleteUnactiveUsersJob;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class Test extends TestCase
+{
+    use DatabaseTransactions;
+
+    private $job;
+
+    /**
+     * @author Cho
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->job = new DeleteUnactiveUsersJob;
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function handle_method_deletes_all_unactive_users_that_have_not_been_updated_30_days_or_more(): void
+    {
+        $unactive_users = [
+            'first' => create_user('', ['active' => 0, 'updated_at' => now()->subDays(31)]),
+            'second' => create_user('', ['active' => 0, 'updated_at' => now()->subDays(30)]),
+        ];
+
+        $this->withoutJobs();
+        $this->job->handle();
+
+        $this->assertDatabaseMissing('users', ['id' => $unactive_users['first']->id]);
+        $this->assertDatabaseMissing('users', ['id' => $unactive_users['second']->id]);
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function handle_method_doesnt_delete_unactive_users_that_have_not_been_updated_less_than_30_days(): void
+    {
+        $unactive_users = [
+            'first' => create_user('', ['active' => 0, 'updated_at' => now()->subDays(29)]),
+            'second' => create_user('', ['active' => 0, 'updated_at' => now()->subDays(28)]),
+        ];
+
+        $this->withoutJobs();
+        $this->job->handle();
+
+        $this->assertDatabaseHas('users', ['id' => $unactive_users['first']->id]);
+        $this->assertDatabaseHas('users', ['id' => $unactive_users['second']->id]);
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function handle_method_doesnt_delete_active_users_that_have_not_been_updated(): void
+    {
+        $active_users = [
+            'first' => create_user('', ['updated_at' => now()->subMonth()]),
+            'second' => create_user('', ['updated_at' => now()->subYear()]),
+        ];
+
+        $this->withoutJobs();
+        $this->job->handle();
+
+        $this->assertDatabaseHas('users', ['id' => $active_users['first']->id]);
+        $this->assertDatabaseHas('users', ['id' => $active_users['second']->id]);
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function handle_method_dispaches_DeletePhotoJob_when_deletes_user(): void
+    {
+        $this->expectsJobs(\App\Jobs\DeletePhotoJob::class);
+
+        $unactive_user = create_user('', [
+            'active' => 0,
+            'photo' => 'some.jpg',
+            'updated_at' => now()->subMonths(2),
+        ]);
+        $this->job->handle();
+    }
+}
