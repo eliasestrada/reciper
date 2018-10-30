0eb15ac97616a9a9e7cda73baf99c587fbaabeed
SerhiiCho <serhiicho@inbox.ru>
2018-10-30 23:00:47 +0200

    Keep adding dark-theme Refactored sidenav

--- resources/views/includes/nav/categories.blade.php
+++ resources/views/includes/nav/categories.blade.php
@@ -3,6 +3,7 @@
         @if ($category['id'] != 1)
             <li>
                 <a href="/recipes#category={{ $category['id'] }}" title="{{ $category['name'] }}">
+                    <i class="fas fa-angle-right fa-15x grey-text left"></i> 
                     {{ $category['name'] }}
                 </a>
             </li>
--- resources/views/includes/nav/sidenav.blade.php
+++ resources/views/includes/nav/sidenav.blade.php
@@ -1,43 +1,65 @@
 <ul class="sidenav" id="mobile-demo">
-    @guest
-        <div class="sidebar-hat-guest center">
-            <a href="/login" class="btn waves-effect waves-light min-w" title="@lang('auth.login')">
-                @lang('auth.login')
-            </a><br>
-            <a href="/register" class="btn waves-effect waves-light min-w" title="@lang('auth.register')">
-                @lang('auth.register')
-            </a>
-        </div>
-    @else
-        <div class="sidebar-hat-auth center"
-            style="background-image:url({{ asset('storage/users/' . user()->photo) }})">
-        </div>
-    @endguest
+        <li>
+            <div class="user-view">
+                <div class="background">
+                    <img src="{{ asset('storage/other/img-small.jpg') }}">
+                </div>
+
+                @guest
+                    <a href="/login" class="btn waves-effect waves-light min-w" title="@lang('auth.login')">
+                        @lang('auth.login')
+                    </a>
+                    <br>
+                    <a href="/register" class="btn waves-effect waves-light min-w mb-2" title="@lang('auth.register')">
+                        @lang('auth.register')
+                    </a>
+                @else
+                    <a href="/users/{{ user()->username }}">
+                        <img class="circle z-depth-1 hoverable" src="{{ asset('storage/users/' . user()->photo) }}">
+                    </a>
+                    <a href="/users/{{ user()->username }}">
+                        <span class="white-text name">{{ user()->getName() }}</span>
+                    </a>
+                    <a href="#email">
+                        <span class="white-text email">
+                            @lang('users.popularity') 
+                            <i class="fas fa-crown" style="font-size:0.8em;color:orange"></i> 
+                            {{ user()->popularity }}
+                        </span>
+                    </a>
+                @endguest
+            </div>
+        </li>
 
     <li class="{{ active_if_route_is(['/']) }}">
         <a href="/">
+            <i class="fas fa-home fa-15x grey-text left"></i>
             @lang('home.home')
         </a>
     </li>
     <li class="{{ active_if_route_is(['recipes']) }}">
         <a href="/recipes">
+            <i class="fas fa-book-open fa-15x grey-text left"></i>
             @lang('recipes.recipes')
         </a>
     </li>
     <li class="{{ active_if_route_is(['search']) }}">
         <a href="/search">
+            <i class="fas fa-search fa-15x grey-text left"></i>
             @lang('pages.search')
         </a>
     </li>
     <li class="{{ active_if_route_is(['help']) }}">
         <a href="/help" title="@lang('messages.help')">
+            <i class="fas fa-question-circle fa-15x grey-text left"></i>
             @lang('messages.help')
         </a>
+        <div class="divider"></div>
     </li>
 
-
-
-    <div class="divider"></div>
+    <li>
+        <a class="subheader grey-text">@lang('pages.categories')</a>
+    </li>
 
     @include('includes.nav.categories')
 </ul>
