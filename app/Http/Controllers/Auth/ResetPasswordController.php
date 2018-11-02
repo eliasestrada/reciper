3b77d485c0e1f3cb07b0ae6c47557869b3fd377c
SerhiiCho <serhiicho@inbox.ru>
2018-11-02 16:12:18 +0200

    Добавил сообщения ошибки валидации в формах сброса пароля

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -7,17 +7,6 @@ use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-     */
-
     use ResetsPasswords;
 
     // Where to redirect users after resetting their password.
@@ -27,5 +16,40 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    /**
+     * Get the password reset validation rules.
+     *
+     * @return array
+     */
+    protected function rules()
+    {
+        $pwd_min = config('valid.settings.password.min');
+        $pwd_max = config('valid.settings.password.max');
+
+        return [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => "required|min:$pwd_min|max:$pwd_max|confirmed",
+        ];
+    }
+
+    /**
+     * Get the password reset validation error messages.
+     *
+     * @return array
+     */
+    protected function validationErrorMessages()
+    {
+        return [
+            'token.required' => trans('settings.token_required'),
+            'email.required' => trans('settings.email_required'),
+            'email.email' => trans('settings.email_email'),
+            'password.required' => trans('settings.pwd_required'),
+            'password.min' => trans('settings.pwd_min'),
+            'password.max' => trans('settings.pwd_max'),
+            'password.confirmed' => trans('settings.pwd_confirmed'),
+        ];
     }
 }
