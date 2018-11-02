af83eb1bd97813f9cab1f1d90a78257a7d466f18
SerhiiCho <serhiicho@inbox.ru>
2018-11-02 16:43:29 +0200

    Added queue for ResetPasswordNotification

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -3,12 +3,12 @@
 namespace App\Notifications;
 
 use App\Models\User;
-//use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ResetPasswordNotification extends Notification
+class ResetPasswordNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
