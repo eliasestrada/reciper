06fb697b530397b0e38529b4dce62f1069935cb8
SerhiiCho <serhiicho@inbox.ru>
2018-11-04 10:04:37 +0200

    Добавил описание сайта в тег head

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,10 +1,11 @@
 <!-- Meta -->
 <meta charset="utf-8">
+<meta name="author" content="Serhii Cho">
+<meta name="description" content="Пошаговые кулинарные рецепты вкусных блюд, с удобным и понятным интерфейсом. Опубликуйте свои рецепты и наблюдайте за ростом их популярности. Зарабатывайте очки опыта и популярности. Опережайте других Ресипёров.">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<!-- Styles -->
 {!! style_timestamp('/css/app.css') !!}
 
 <!-- Links -->
