ed11db25cd36b564a979e5c881662e132391517f
SerhiiCho <serhiicho@inbox.ru>
2018-11-04 11:29:19 +0200

    Добавил пару категорий к CategoriesSeeder

--- database/seeds/CategoriesSeeder.php
+++ database/seeds/CategoriesSeeder.php
@@ -21,12 +21,14 @@ class CategoriesSeeder extends Seeder
             ['name_ru' => 'Детские блюда', 'name_en' => 'Baby dishes'],
             ['name_ru' => 'Крупы', 'name_en' => 'Cereals'],
             ['name_ru' => 'Мультиварка', 'name_en' => 'Multicooker'],
+            ['name_ru' => 'Напитки', 'name_en' => 'Drinks'],
             ['name_ru' => 'Мясо', 'name_en' => 'Meat'],
             ['name_ru' => 'Овощи', 'name_en' => 'Vegetables'],
             ['name_ru' => 'Рыба', 'name_en' => 'Fish'],
             ['name_ru' => 'Салаты', 'name_en' => 'Salads'],
             ['name_ru' => 'Соусы', 'name_en' => 'Sauces'],
             ['name_ru' => 'Суши', 'name_en' => 'Sushi'],
+            ['name_ru' => 'Фрукты', 'name_en' => 'Fruits'],
             ['name_ru' => 'Разное', 'name_en' => 'Other'],
         ]);
     }
