481fce163ed674b8f9a403f33e8e155362316f6e
SerhiiCho <serhiicho@inbox.ru>
2018-11-05 12:39:08 +0200

    Добавил Javascript функцию которая увеличивает шрифт на recipes/show странице.

--- resources/js/vanilla/components/_recipes.js
+++ resources/js/vanilla/components/_recipes.js
@@ -27,3 +27,31 @@
         activeAfterClickBtn("popup-window", "popup-window-trigger");
     }
 })();
+
+(function IncreaseFontSize() {
+    let incFontSizeBtn = $('inc-font-size');
+    let dicFontSizeBtn = $('dic-font-size');
+    let elements = document.querySelectorAll('.font-scalable');
+
+    incFontSizeBtn.addEventListener('click', () => {
+        elements.forEach(el => {
+            let currentSize = parseFloat(el.style.fontSize);
+
+            if (el.style.fontSize == "") el.style.fontSize = "1.05em";
+
+            if (currentSize <= 1.5 && currentSize >= 0.9) {
+                el.style.fontSize = currentSize + 0.1 + "em"
+            }
+        });
+    });
+
+    dicFontSizeBtn.addEventListener('click', () => {
+        elements.forEach(el => {
+            let currentSize = parseFloat(el.style.fontSize);
+
+            if (currentSize <= 1.6 && currentSize >= 1) {
+                el.style.fontSize = currentSize - 0.1 + "em"
+            }
+        });
+    });
+})();
