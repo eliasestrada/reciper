68aab594ae320d7b0c9176856577a50084b299c9
SerhiiCho <serhiicho@inbox.ru>
2018-11-08 11:29:47 +0200

    Veird staff

---
+++ resources/js/vue/vanilla/components/_recipes.js
@@ -0,0 +1,63 @@
+(function ReadyCheckbox() {
+    let checkbox = $("ready-checkbox");
+    let publishBtn = $("publish-btn");
+
+    if (checkbox) {
+        publishBtn.addEventListener("click", () => {
+            if (confirm(publishBtn.getAttribute("data-alert"))) {
+                if ((checkbox.checked = true)) {
+                    checkbox.closest("form").submit();
+                }
+            }
+        });
+    }
+})();
+
+(function ImageUploader() {
+    if (imageUploader.src && imageUploader.target) {
+        imageUploader.showImage();
+    }
+})();
+
+(function PopupWindowShowMore() {
+    let popupTrigger = $("popup-window-trigger");
+    let popupWindow = $("popup-window");
+
+    if (popupTrigger && popupWindow) {
+        activeAfterClickBtn("popup-window", "popup-window-trigger");
+    }
+})();
+
+(function IncreaseFontSize() {
+    let incFontSizeBtn = $('inc-font-size');
+    let dicFontSizeBtn = $('dic-font-size');
+    let elements = document.querySelectorAll('.font-scalable');
+
+    if (incFontSizeBtn && dicFontSizeBtn) {
+        incFontSizeBtn.addEventListener('click', () => {
+            elements.forEach(el => {
+                let currentSize = parseFloat(el.style.fontSize);
+                let newFontSize = currentSize + 0.1
+
+                if (currentSize <= 1.5 && currentSize >= 0.9) {
+                    el.style.fontSize = newFontSize + "em"
+                    fetch(`/invokes/font-size-switcher/${newFontSize}`)
+                        .catch(err => console.error(err));
+                }
+            });
+        });
+
+        dicFontSizeBtn.addEventListener('click', () => {
+            elements.forEach(el => {
+                let currentSize = parseFloat(el.style.fontSize);
+                let newFontSize = currentSize - 0.1
+
+                if (currentSize <= 1.6 && currentSize >= 1) {
+                    el.style.fontSize = newFontSize + "em"
+                    fetch(`/invokes/font-size-switcher/${newFontSize}`)
+                        .catch(err => console.error(err));
+                }
+            });
+        });
+    }
+})();
---
+++ resources/js/vue/vanilla/components/_settings.js
@@ -0,0 +1,11 @@
+(function CollapsibleNavigationMenu() {
+    let li = $("collapsible-li-tag");
+    let body = document.querySelector("#collapsible-li-tag .collapsible-body");
+
+    if (li && window.innerWidth > 600) {
+        setTimeout(() => {
+            body.setAttribute("style", "display:block");
+            li.classList.add("active");
+        }, 1);
+    }
+})();
