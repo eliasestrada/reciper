1915551e966255de38f069c6b9eaf85d8a43db6c
SerhiiCho <serhiicho@inbox.ru>
2018-11-10 18:46:35 +0200

    Added email form field to general/index page

--- resources/views/settings/general/index.blade.php
+++ resources/views/settings/general/index.blade.php
@@ -13,13 +13,13 @@
             <div class="col s12">
                 <form action="{{ action('Settings\GeneralController@updateGeneral') }}" method="post">
                     @csrf @method('put')
-
-                    {{-- Reciper's name --}}
+                    {{-- Name --}}
                     <h2 class="header">@lang('settings.reciper_name')</h2>
                     <div class="input-field">
                         <label for="name">@lang('forms.name')</label>
                         <input type="text" name="name" id="name" value="{{ user()->name }}" data-length="{{ config('valid.settings.general.name.max') }}" class="counter" maxlength="{{ config('valid.settings.general.name.max') }}" minlength="{{ config('valid.settings.general.name.min') }}">
                     </div>
+                    {{-- Status --}}
                     <div class="input-field">
                         <textarea id="status" class="materialize-textarea counter" data-length="{{ config('valid.settings.general.status.max') }}" name="status" maxlength="{{ config('valid.settings.general.status.max') }}">{{ (user()->status ?? old('status')) }}</textarea>
                         <label for="status">@lang('settings.status')</label>
@@ -29,7 +29,22 @@
                     </div>
                 </form>
             </div>
-            <div class="col s12">
+            {{-- Email --}}
+            <div class="col s12 mt-5">
+                <form action="{{ action('Settings\GeneralController@updateEmail') }}" method="post">
+                    @csrf @method('put')
+                    <h2 class="header">@lang('forms.email')</h2>
+                    <div class="input-field">
+                        <label for="email">@lang('forms.email_desc')</label>
+                        <input type="text" name="email" id="email" value="{{ user()->email }}" data-length="{{ config('valid.settings.email.max') }}" class="counter" maxlength="{{ config('valid.settings.email.max') }}">
+                    </div>
+                    <div class="input-field mt-4">
+                        <button class="btn" type="submit">@lang('forms.save')</button>
+                    </div>
+                </form>
+            </div>
+            {{-- Password --}}
+            <div class="col s12 mt-5">
                 <h2 class="header">@lang('forms.change_pwd')</h2>
                 <form action="{{ action('Settings\GeneralController@updatePassword') }}" method="post">
                     @method('put') @csrf
