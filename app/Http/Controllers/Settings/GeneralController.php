5af4f44d94e8cc1bfb07ccfac387a43367ddfb3f
SerhiiCho <serhiicho@inbox.ru>
2018-11-11 11:34:38 +0200

    General controller in updateEmail method changed the name of the cache key

--- app/Http/Controllers/Settings/GeneralController.php
+++ app/Http/Controllers/Settings/GeneralController.php
@@ -59,13 +59,13 @@ class GeneralController extends Controller
             return back()->withSuccess(trans('settings.email_is_empty'));
         }
 
-        if (cache()->has("user_{$user->id}_changed_email")) {
+        if (cache()->has("user:{$user->id}:changed_email")) {
             return back()->withError(trans('settings.email_change_once_per_week'));
         }
 
         $user->update(['email' => request('email'), 'token' => str_random(30)]);
         $user->notify(new VerifyEmailNotification($user));
-        cache()->put("user_{$user->id}_changed_email", 1, 10080);
+        cache()->put("user:{$user->id}:changed_email", 1, 10080);
 
         return back()->withSuccess(trans('settings.saved_now_verify_email'));
     }
