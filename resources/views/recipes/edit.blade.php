945bb038d7ae1a845e925076cf69c7420bbd4c43
SerhiiCho <serhiicho@inbox.ru>
2018-12-02 20:40:05 +0200

    Cleaned recipes/edit page

--- resources/views/recipes/edit.blade.php
+++ resources/views/recipes/edit.blade.php
@@ -4,7 +4,12 @@
 
 @section('content')
 
-<form action="{{ action('RecipesController@update', ['recipe' => $recipe->id]) }}" method="post" class="page" enctype="multipart/form-data" id="form-update-recipe">
+<form action="{{ action('RecipesController@update', ['recipe' => $recipe->id]) }}"
+    method="post"
+    class="page"
+    enctype="multipart/form-data"
+    id="form-update-recipe"
+>
 
     @method('put')
     @csrf
@@ -16,10 +21,16 @@
 
         <div class="center pb-4">
             {{--  View button  --}}
-            <input type="submit" value="&#xf06e" name="view" class="fas btn-floating green tooltipped" data-tooltip="@lang('tips.view')">
+            <input type="submit" value="&#xf06e" name="view"
+                class="fas btn-floating green tooltipped"
+                data-tooltip="@lang('tips.view')"
+            >
 
             {{--  Save button  --}}
-            <button type="submit" id="submit-save-recipe" data-tooltip="@lang('tips.save')" class="btn-floating green tooltipped waves-effect waves-light">
+            <button type="submit" id="submit-save-recipe"
+                data-tooltip="@lang('tips.save')"
+                class="btn-floating green tooltipped waves-effect waves-light"
+            >
                 <i class="fas fa-save"></i>
             </button>
 
@@ -30,11 +41,11 @@
                 confirm="{{ trans('recipes.are_you_sure_to_delete') }}"
             >
                 <span v-if="!error">
-                    <a href="#" class="btn-floating red tooltipped waves-effect waves-light"
-                        data-position="top"
+                    <a href="#" id="_delete-recipe"
+                        class="btn-floating red tooltipped waves-effect waves-light"
                         data-tooltip="@lang('forms.deleting')"
                         v-on:click="deleteRecipe"
-                        id="_delete-recipe"
+                        data-position="top"
                     >
                         <i class="fas fa-trash"></i>
                     </a>
@@ -42,7 +53,11 @@
             </delete-recipe-btn>
 
             {{--  Publish button  --}}
-            <a href="#" class="btn-floating green tooltipped waves-effect waves-light" id="publish-btn" data-tooltip="@lang('tips.publish')" data-alert="@lang('recipes.are_you_sure_to_publish')">
+            <a href="#" id="publish-btn"
+                class="btn-floating green tooltipped waves-effect waves-light"
+                data-alert="@lang('recipes.are_you_sure_to_publish')"
+                data-tooltip="@lang('tips.publish')"
+            >
                 <i class="fas fa-clipboard-check"></i>
             </a>
             <input type="checkbox" name="ready" value="1" class="hide" id="ready-checkbox">
@@ -52,7 +67,12 @@
             {{-- Title --}}
             <div class="col s12 m6 l4">
                 <div class="input-field">
-                    <input type="text" name="title" id="title" value="{{ old('title') ?? $recipe->getTitle() }}" class="counter" data-length="{{ config('valid.recipes.title.max') }}">
+                    <input type="text" name="title"
+                        value="{{ old('title') ?? $recipe->getTitle() }}"
+                        data-length="{{ config('valid.recipes.title.max') }}"
+                        class="counter"
+                        id="title"
+                    >
                     <label for="title">@lang('recipes.title')</label>
                     @include('includes.input-error', ['field' => 'title'])
                 </div>
@@ -78,7 +98,9 @@
                 </label>
                 <select name="meal" id="meal">
                     @foreach ($meal as $m)
-                        <option value="{{ $m['id'] }}" {{ set_as_selected_if_equal($m['id'], ($recipe->meal->id ?? '')) }}>
+                        <option value="{{ $m['id'] }}" {{ set_as_selected_if_equal(
+                            $m['id'], ($recipe->meal->id ?? '')
+                        ) }}>
                             {{ title_case($m['name']) }}
                         </option>
                     @endforeach
@@ -91,7 +113,11 @@
             {{-- Ingredients --}}
             <div class="col s12 l6">
                 <div class="input-field">
-                    <textarea name="ingredients" id="ingredients" class="materialize-textarea counter" data-length="{{ config('valid.recipes.ingredients.max') }}">{{ old('ingredients') ?? $recipe->getIngredients() }}</textarea>
+                    <textarea name="ingredients" id="ingredients"
+                        class="materialize-textarea counter"
+                        data-length="{{ config('valid.recipes.ingredients.max') }}"
+                    >{{ old('ingredients') ?? $recipe->getIngredients() }}
+                    </textarea>
 
                     <label for="ingredients">
                         @lang('recipes.ingredients') 
@@ -104,7 +130,11 @@
             {{-- Intro --}}
             <div class="col s12 l6">
                 <div class="input-field">
-                    <textarea name="intro" id="intro" class="materialize-textarea counter" data-length="{{ config('valid.recipes.intro.max') }}">{{ old('intro') ?? $recipe->getIntro() }}</textarea>
+                    <textarea name="intro" id="intro"
+                        class="materialize-textarea counter"
+                        data-length="{{ config('valid.recipes.intro.max') }}"
+                    >{{ old('intro') ?? $recipe->getIntro() }}
+                    </textarea>
 
                     <label for="intro">
                         @lang('recipes.short_intro') 
@@ -118,7 +148,11 @@
         {{-- Text --}}
         <div class="col s12">
             <div class="input-field">
-                <textarea name="text" id="text" class="materialize-textarea counter" data-length="{{ config('valid.recipes.text.max') }}">{{ old('text') ?? $recipe->getText() }}</textarea>
+                <textarea name="text" id="text"
+                    class="materialize-textarea counter"
+                    data-length="{{ config('valid.recipes.text.max') }}"
+                >{{ old('text') ?? $recipe->getText() }}
+                </textarea>
 
                 <label for="text">
                     @lang('recipes.text_of_recipe') 
@@ -138,9 +172,11 @@
                     select="@lang('forms.select')"
                     categories-title="@lang('recipes.categories_title')"
                     deleting="@lang('forms.deleting')"
-                    add="@lang('forms.add')">
+                    add="@lang('forms.add')"
+                >
                     @include('includes.preloader')
                 </categories-field>
+
                 @include('includes.input-error', ['field' => 'categories.*'])
             </div>
 
@@ -148,9 +184,14 @@
             <div class="col s12 m6">
                 <div class="center pb-5">
                     <h5 class="col s12 mb-3">@lang('recipes.image')</h5>
+
                     @include('includes.input-error', ['field' => 'image'])
+
                     <div class="preview-image preview-image-recipe position-relative">
-                        <img src="{{ asset("storage/big/recipes/$recipe->image") }}" alt="{{ $recipe->title }}" id="target-image">
+                        <img src="{{ asset("storage/big/recipes/$recipe->image") }}"
+                            alt="{{ $recipe->title }}"
+                            id="target-image"
+                        >
                         <input type="file" name="image" id="src-image" class="hide" style="overflow:hidden">
                         <label for="src-image" class="btn waves-effect waves-light min-w">
                             <i class="fas fa-upload left"></i>
