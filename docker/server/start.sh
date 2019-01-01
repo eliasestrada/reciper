43f37c52ee37497080da42505156680572cb52ee
SerhiiCho <serhiicho@inbox.ru>
2019-01-01 18:48:38 +0200

    -1- Changed start.sh script

--- docker/server/start.sh
+++ docker/server/start.sh
@@ -4,8 +4,8 @@ if [ ! -f /var/www/.env ] && [ -f /var/www/vendor/autoload.php ]; then
     cd /var/www
     cp .env.example .env
     php artisan key:generate
+    php artisan storage:link
     php artisan wipe
-    ./vendor/bin/phpunit
 else
     echo 'Try again, because composer is still installing dependencies'
 fi
