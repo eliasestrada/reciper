b52cf8c6d0c603aef8a4c65a5b5bbe659573d973
SerhiiCho <serhiicho@inbox.ru>
2019-01-11 17:25:11 +0200

    -1- Deleted one line from favs/index page

--- resources/views/favs/index.blade.php
+++ resources/views/favs/index.blade.php
@@ -2,16 +2,20 @@
 
 @section('title', trans('messages.favorites'))
 
-
 @section('content')
 
 <div class="page">
     <div class="center mb-4">
-        <h1 class="header"><i class="fas fa-star" style="color:#d49d10"></i> @lang('messages.favorites')</h1>
+        <h1 class="header">
+            <i class="fas fa-star" style="color:#d49d10"></i> 
+            @lang('messages.favorites')
+        </h1>
     </div>
     <div class="pb-3">
         @foreach ($categories as $category)
-            <a href="/favs/{{ $category['id'] }}" class="btn btn-sort {{ active_if_route_is(['/favs/' . $category['id']]) }} {{ $category['id'] == 1 ? active_if_route_is(['/favs']) : '' }}">
+            <a href="/favs/{{ $category['id'] }}"
+                class="btn btn-sort {{ active_if_route_is(["/favs/{$category['id']}"]) }}"
+            >
                 <span class="pl-1">{{ $category['name'] }}</span>
             </a>
         @endforeach
