a34b1a9428bf7a9e0c96367b6b582ac8fdbfca52
SerhiiCho <serhiicho@inbox.ru>
2019-01-20 12:20:08 +0200

    -0- Fixed comment

--- tests/Feature/Views/Settings/Photo/SettingsPhotoIndexPageTest.php
+++ tests/Feature/Views/Settings/Photo/SettingsPhotoIndexPageTest.php
@@ -92,7 +92,8 @@ class SettingsPhotoIndexPageTest extends TestCase
 
     /**
      * Helper function
-     * @auhor Cho
+     *
+     * @author Cho
      * @param string $photo_path
      * @return void
      */
