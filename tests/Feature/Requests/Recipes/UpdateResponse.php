cd6a4898f0c768fe7e3aa34d12f272289876a9f2
SerhiiCho <serhiicho@inbox.ru>
2019-02-17 00:02:11 +0200

    -1- Added Recipes/UpdateResponse

---
+++ tests/Feature/Requests/Recipes/UpdateResponse.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Responses\Controllers\Recipes;
+
+use App\Helpers\Traits\RecipeControllerHelpers;
+use App\Models\Recipe;
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Http\RedirectResponse;
+
+class UpdateResponse implements Responsable
+{
+    use RecipeControllerHelpers;
+
+    protected $recipe;
+
+    /**
+     * @param \App\Models\Recipe $recipe
+     * @return void
+     */
+    public function __construct(Recipe $recipe)
+    {
+        $this->recipe = $recipe;
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function toResponse($request): RedirectResponse
+    {
+        if (!user()->hasRecipe($this->recipe->id)) {
+            return back()->withError(trans('recipes.cant_draft'));
+        }
+
+        if ($this->recipe->isReady()) {
+            return $this->moveToDraftsAndRedirectWithSuccess();
+        }
+
+        if ($this->checkForScriptTags($request)) {
+            return back()->withError(trans('notifications.cant_use_script_tags'));
+        }
+
+        if ($request->file('image')) {
+            $this->dispatchDeleteFileJob($this->recipe->image);
+        }
+
+        $filename = $this->saveImageIfExist($request->file('image'), $this->recipe->slug);
+        $this->updateRecipe($request, $filename, $this->recipe);
+
+        if ($this->recipe->isReady() && user()->hasRole('admin')) {
+            return $this->fireEventAndRedirectWithSuccess();
+        }
+
+        if ($this->recipe->isReady()) {
+            return $this->clearCacheAndRedirectWithSuccess();
+        }
+
+        if (request()->has('view')) {
+            return redirect("/recipes/{$this->recipe->slug}");
+        }
+
+        return redirect("/recipes/{$this->recipe->slug}/edit")
+            ->withSuccess(trans('recipes.saved'));
+    }
+
+    /**
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function fireEventAndRedirectWithSuccess(): RedirectResponse
+    {
+        event(new \App\Events\RecipeGotApproved($this->recipe));
+
+        return redirect('/users/other/my-recipes')
+            ->withSuccess(trans('recipes.recipe_published'));
+    }
+
+    /**
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function clearCacheAndRedirectWithSuccess(): RedirectResponse
+    {
+        cache()->forget('unapproved_notif');
+
+        return redirect('/users/other/my-recipes')
+            ->withSuccess(trans('recipes.added_to_approving'));
+    }
+
+    /**
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function moveToDraftsAndRedirectWithSuccess(): RedirectResponse
+    {
+        $this->recipe->moveToDrafts();
+        $this->clearCache();
+
+        return redirect("/recipes/{$this->recipe->slug}/edit")
+            ->withSuccess(trans('recipes.saved'));
+    }
+}
