0b25b2d66e76fc1710e790e68beaa5c1f543a012
SerhiiCho <serhiicho@inbox.ru>
2019-02-24 21:15:58 +0200

    -1- Added tests for Controllers/RecipeHelpers@checkForScriptTags method

--- tests/Unit/Controllers/RecipesControllerHelpersTest.php
+++ tests/Unit/Controllers/RecipesControllerHelpersTest.php
@@ -3,15 +3,46 @@
 namespace Tests\Unit\Controllers;
 
 use Tests\TestCase;
+use App\Models\User;
+use App\Helpers\Controllers\RecipeHelpers;
 
 class RecipeControllerHelpersTest extends TestCase
 {
     /**
+     * @var \App\Helpers\Controllers\RecipeHelpers $class
+     */
+    private $class;
+
+    /**
+     * @author Cho
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->class = new class { use RecipeHelpers; };
+    }
+
+    /**
      * @author Cho
      * @test
      */
-    public function here_is_next_test(): void
+    public function method_checkForScriptTags_returns_true_if_title_has_script_tag(): void
     {
-        $this->markTestIncomplete();
+        $this->withoutNotifications();
+        $fields = ['title' => 'Lorem <script ipsum>'];
+        $result = $this->class->checkForScriptTags($fields, make(User::class));
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function method_checkForScriptTags_returns_false_if_title_has_no_script_tag(): void
+    {
+        $this->withoutNotifications();
+        $fields = ['title' => 'Lorem ipsum title'];
+        $result = $this->class->checkForScriptTags($fields, make(User::class));
+        $this->assertFalse($result);
     }
 }
