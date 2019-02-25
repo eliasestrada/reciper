1862c6915ea9084715f78bc80c19c2d0d15902f5
SerhiiCho <serhiicho@inbox.ru>
2019-02-25 16:06:30 +0200

    -2- Refactored response Recipes/EditResponse... Changed construct completely and added private properties

--- app/Http/Responses/Controllers/Recipes/EditResponse.php
+++ app/Http/Responses/Controllers/Recipes/EditResponse.php
@@ -2,23 +2,39 @@
 
 namespace App\Http\Responses\Controllers\Recipes;
 
-use App\Models\Recipe;
-use App\Repos\MealRepo;
 use Illuminate\Contracts\Support\Responsable;
+use App\Repos\RecipeRepo;
+use App\Repos\MealRepo;
+use App\Models\User;
 
 class EditResponse implements Responsable
 {
-    protected $recipe;
-    protected $meal_repo;
+    /**
+     * @var \App\Repos\MealRepo $meal_repo
+     */
+    private $meal_repo;
+
+    /**
+     * @var \App\Models\Recipe $recipe
+     */
+    private $recipe;
+
+    /**
+     * @var \App\Models\User $user
+     */
+    private $user;
 
     /**
      * @param string $slug
+     * @param \App\Repos\RecipeRepo $recipe_repo
      * @param \App\Repos\MealRepo $meal_repo
+     * @param \App\Models\User|null $user
      * @return void
      */
-    public function __construct(string $slug, MealRepo $meal_repo)
+    public function __construct(string $slug, RecipeRepo $recipe_repo, MealRepo $meal_repo, ?User $user = null)
     {
-        $this->recipe = Recipe::whereSlug($slug)->first();
+        $this->user = $user ?? user();
+        $this->recipe = $recipe_repo->find($slug);
         $this->meal_repo = $meal_repo;
     }
 
@@ -45,6 +61,6 @@ class EditResponse implements Responsable
      */
     protected function recipeIsReadyOrDoesntBelongToThisUser(): bool
     {
-        return !user()->hasRecipe($this->recipe->id) || $this->recipe->isReady();
+        return !$this->user->hasRecipe($this->recipe->id) || $this->recipe->isReady();
     }
 }
