688e171cb715c6e54f460b98a92778f0665ceed6
SerhiiCho <serhiicho@inbox.ru>
2019-03-01 18:54:10 +0200

    -1- Added comment and return type to resources documents

--- app/Http/Resources/RecipesRandomResource.php
+++ app/Http/Resources/RecipesRandomResource.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class RecipesRandomResource extends JsonResource
 {
-    // Transform the resource into an array.
-    public function toArray($request)
+    /**
+     * Transform the resource into an array
+     *
+     * @param $request
+     * @return array
+     */
+    public function toArray($request): array
     {
         return [
             'slug' => $this->slug,
--- app/Http/Resources/RecipesResource.php
+++ app/Http/Resources/RecipesResource.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class RecipesResource extends JsonResource
 {
-    // Transform the resource into an array.
-    public function toArray($request)
+    /**
+     * Transform the resource into an array
+     *
+     * @param $request
+     * @return array
+     */
+    public function toArray($request): array
     {
         return [
             'id' => $this->id,
