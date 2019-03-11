006dec95238eee1c12e6f65cafd647032ff977eb
SerhiiCho <serhiicho@inbox.ru>
2019-03-11 15:46:32 +0200

    Change getCache method in CategoryRepo
    Because it wasn't right.

--- app/Repos/CategoryRepo.php
+++ app/Repos/CategoryRepo.php
@@ -13,16 +13,16 @@ class CategoryRepo
      */
     public function getCache(): array
     {
-        $categories = cache()->rememberForever('categories', function () {
-            try {
+        try {
+            $categories = cache()->rememberForever('categories', function () {
                 return Category::get()->toArray();
-            } catch (QueryException $e) {
-                return report_error($e, []);
-            }
-        });
+            });
 
-        return array_map(function ($cat) {
-            return ['id' => $cat['id'], 'name' => $cat[_('name')]];
-        }, $categories);
+            return array_map(function ($cat) {
+                return ['id' => $cat['id'], 'name' => $cat[_('name')]];
+            }, $categories);
+        } catch (QueryException $e) {
+            return report_error($e, []);
+        }
     }
 }
