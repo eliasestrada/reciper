030e78f22530c71fa0522f73e6bb7819a69fe93c
SerhiiCho <serhiicho@inbox.ru>
2019-03-11 15:45:35 +0200

    Change redis database for cache from 1 to 0
    Because in order to take cache from redis using redis facade in needed to tell Redis::select(1) to select first database. Now I don't need to do it, coz it uses 0 database by default.

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => 1,
+            'database' => 0,
         ],
 
         'session' => [
