fb68292f8ac1311227e778c279d8ce2ccc7819f8
SerhiiCho <serhiicho@inbox.ru>
2019-03-11 15:51:05 +0200

    Move all from StatisticController to response class
    * Refactored response class.
    
    Because the controller was full of logic, now it is moved to a response class. The code is more readable and easier to test.

--- app/Http/Controllers/Api/StatisticController.php
+++ app/Http/Controllers/Api/StatisticController.php
@@ -2,18 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
-use Exception;
 use App\Repos\RecipeRepo;
 use App\Http\Controllers\Controller;
 use App\Http\Responses\Controllers\Api\StatisticsPopularityChartResponse;
 
 class StatisticController extends Controller
 {
-    /**
-     * @var \Illuminate\Support\Collection
-     */
-    private $recipes;
-
     /**
      * @return void
      */
@@ -31,72 +25,8 @@ class StatisticController extends Controller
      */
     public function popularityChart(RecipeRepo $recipe_repo, ?int $user_id = null): StatisticsPopularityChartResponse
     {
-        /**
-         * @todo This query should bee cached
-         * Also there should be a timer that shows when stats will update
-         */
-        $this->recipes = $recipe_repo->getCachedUserRecipesForTheLastYear(
-            $user_id ?? user()->id
+        return new StatisticsPopularityChartResponse(
+            $recipe_repo->getUserRecipesForTheLastYear($user_id ?? user()->id)
         );
-
-        $rules = $this->generateRulesArray();
-
-        return new StatisticsPopularityChartResponse([
-            'views' => $this->generateArrayWithChartData('views', $rules),
-            'likes' => $this->generateArrayWithChartData('likes', $rules),
-            'favs' => $this->generateArrayWithChartData('favs', $rules),
-        ]);
-    }
-
-    /**
-     * @return array
-     */
-    public function generateRulesArray(): array
-    {
-        return array_map(function ($num) {
-            return [
-                'month' => now()->startOfMonth()->subMonths($num - 1)->month,
-                'from' => now()->startOfMonth()->subMonths($num - 1)->toDateString(),
-                'to' => now()->startOfMonth()->subMonths($num - 2)->toDateString(),
-                'sum' => 0,
-            ];
-        }, [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
-    }
-
-    /**
-     * @param string $column
-     * @param array $rules
-     * @return array
-     */
-    public function generateArrayWithChartData(string $column, array $rules): array
-    {
-        if (!in_array($column, ['likes', 'views', 'favs'])) {
-            $msg = 'getDataFromUser 1 parameter can only have one of three values. Given value does not match any of them';
-            throw new Exception($msg);
-        }
-
-        // Takes rules array and fills out the 'sum' column with data from database
-        foreach ($rules as $key => $rule) {
-            $rules[$key]['sum'] += $this->recipes->map(function ($recipe) use ($rule, $column) {
-                return $recipe[$column]
-                    ->where('created_at', '>=', $rule['from'])
-                    ->where('created_at', '<=', $rule['to'])
-                    ->count();
-            })->sum();
-        }
-
-        return $this->convertMonthNumberToName($rules);
-    }
-
-    /**
-     * @param array $rules
-     * @return array
-     */
-    public function convertMonthNumberToName(array $rules): array
-    {
-        return array_map(function ($rule) {
-            $rule['month'] = trans("date.month_{$rule['month']}");
-            return $rule;
-        }, $rules);
     }
 }
--- app/Http/Responses/Controllers/Api/StatisticsPopularityChartResponse.php
+++ app/Http/Responses/Controllers/Api/StatisticsPopularityChartResponse.php
@@ -2,22 +2,31 @@
 
 namespace App\Http\Responses\Controllers\Api;
 
+use Redis;
 use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Support\Collection;
 
 class StatisticsPopularityChartResponse implements Responsable
 {
     /**
-     * @var array
+     * @var \Illuminate\Support\Collection
      */
-    private $chart_data = [];
+    private $recipes;
+
+    /**
+     * @var int
+     */
+    private $user_id;
 
     /**
      * @param array $chart_data
+     * @param int|null $user_id
      * @return void
      */
-    public function __construct(array $chart_data)
+    public function __construct(Collection $recipes, ?int $user_id = null)
     {
-        $this->chart_data = $chart_data;
+        $this->recipes = $recipes;
+        $this->user_id = $user_id ?? user()->id;
     }
 
     /**
@@ -26,8 +35,31 @@ class StatisticsPopularityChartResponse implements Responsable
      */
     public function toResponse($request): array
     {
+        $ttl = config('cache.timing.user_statistics_data');
+        $key = "user:{$this->user_id}:chart_data";
+
+        // dump(Redis::ttl($key));
+
+        return cache()->remember($key, $ttl, function () {
+            return $this->reponseChartArray();
+        });
+    }
+
+    /**
+     * @return array
+     */
+    public function reponseChartArray(): array
+    {
+        $rules = $this->generateRulesArray();
+
+        $chart_data = [
+            'views' => $this->generateArrayWithChartData('views', $rules),
+            'likes' => $this->generateArrayWithChartData('likes', $rules),
+            'favs' => $this->generateArrayWithChartData('favs', $rules),
+        ];
+
         return [
-            'labels' => array_column($this->chart_data['views'], 'month'),
+            'labels' => array_column($chart_data['views'], 'month'),
             'options' => [],
             'datasets' => [
                 [
@@ -35,23 +67,75 @@ class StatisticsPopularityChartResponse implements Responsable
                     'fill' => false,
                     'backgroundColor' => '#484074',
                     'borderColor' => '#484074',
-                    'data' => array_column($this->chart_data['views'], 'sum'),
+                    'data' => array_column($chart_data['views'], 'sum'),
                 ],
                 [
                     'label' => trans('users.likes'),
                     'fill' => false,
                     'backgroundColor' => '#cf4545',
                     'borderColor' => '#cf4545',
-                    'data' => array_column($this->chart_data['likes'], 'sum'),
+                    'data' => array_column($chart_data['likes'], 'sum'),
                 ],
                 [
                     'label' => trans('messages.favorites'),
                     'fill' => false,
                     'backgroundColor' => '#d49d10',
                     'borderColor' => '#d49d10',
-                    'data' => array_column($this->chart_data['favs'], 'sum'),
+                    'data' => array_column($chart_data['favs'], 'sum'),
                 ],
             ],
         ];
+    }
+
+    /**
+     * @return array
+     */
+    public function generateRulesArray(): array
+    {
+        return array_map(function ($num) {
+            return [
+                'month' => now()->startOfMonth()->subMonths($num - 1)->month,
+                'from' => now()->startOfMonth()->subMonths($num - 1)->toDateString(),
+                'to' => now()->startOfMonth()->subMonths($num - 2)->toDateString(),
+                'sum' => 0,
+            ];
+        }, [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
+    }
+
+    /**
+     * @param string $column
+     * @param array $rules
+     * @return array
+     */
+    public function generateArrayWithChartData(string $column, array $rules): array
+    {
+        if (!in_array($column, ['likes', 'views', 'favs'])) {
+            $msg = 'getDataFromUser 1 parameter can only have one of three values. Given value does not match any of them';
+            throw new \Exception($msg);
+        }
+
+        // Takes rules array and fills out the 'sum' column with data from database
+        foreach ($rules as $key => $rule) {
+            $rules[$key]['sum'] += $this->recipes->map(function ($recipe) use ($rule, $column) {
+                return $recipe[$column]
+                    ->where('created_at', '>=', $rule['from'])
+                    ->where('created_at', '<=', $rule['to'])
+                    ->count();
+            })->sum();
+        }
+
+        return $this->convertMonthNumberToName($rules);
+    }
+
+    /**
+     * @param array $rules
+     * @return array
+     */
+    public function convertMonthNumberToName(array $rules): array
+    {
+        return array_map(function ($rule) {
+            $rule['month'] = trans("date.month_{$rule['month']}");
+            return $rule;
+        }, $rules);
     }
 }
