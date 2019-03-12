e5b734caf3cb82b9cf0f3ffc622dcdcafba6a107
SerhiiCho <serhiicho@inbox.ru>
2019-03-12 17:35:45 +0200

    Format html markup in BtnLike.vue file
    Because it was unreadable.

--- resources/js/vue/components/BtnLike.vue
+++ resources/js/vue/components/BtnLike.vue
@@ -1,13 +1,19 @@
 <template>
     <section class="ml-2 d-inline-block">
         <a v-if="userId" class="p-0 _like-button" @click="fetchItems">
-            
             <i class="fas fa-heart fa-15x heart" :class="iconClass"></i> 
-            <span v-text="amount" style="transform:translate(-2px, 5px);color:#6b6b6b" class="d-inline-block"></span>
+            <span v-text="amount"
+                style="transform:translate(-2px, 5px);color:#6b6b6b"
+                class="d-inline-block"
+            ></span>
         </a>
+
         <a v-else class="p-0 tooltipped" :data-tooltip="tooltip" data-position="right">
             <i class="fas fa-heart fa-15x heart"></i> 
-            <span v-text="amount" style="transform:translate(-4px, 5px);color:#6b6b6b" class="d-inline-block"></span>
+            <span v-text="amount"
+                style="transform:translate(-4px, 5px);color:#6b6b6b"
+                class="d-inline-block"
+            ></span>
         </a>
     </section>
 </template>
